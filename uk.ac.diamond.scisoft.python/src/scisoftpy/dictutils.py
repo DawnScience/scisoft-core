710c2de156e1800e09bff357ec5692965b758504
Peter Chang <Peter.Chang@diamond.ac.uk>
2011-12-21 15:07:15 +0000

    SCISOFT-CORE: fix coding owing to new copyright

--- uk.ac.diamond.scisoft.python/src/scisoftpy/dictutils.py
+++ uk.ac.diamond.scisoft.python/src/scisoftpy/dictutils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ###
 # Copyright 2011 Diamond Light Source Ltd.
 # 
@@ -13,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###
-
-# coding=utf-8
 
 def _maketranslator():
     '''

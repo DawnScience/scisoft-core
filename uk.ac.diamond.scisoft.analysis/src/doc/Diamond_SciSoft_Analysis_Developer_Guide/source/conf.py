0f01cf6971ce9fc3e6dbe6c050f86604d26f414c
Mark Basham <mark.basham@diamond.ac.uk>
2011-12-21 10:04:27 +0000

    SCISOFT - move utf-8 coding back to top of appropriate python scripts

--- uk.ac.diamond.scisoft.analysis/src/doc/Diamond_SciSoft_Analysis_Developer_Guide/source/conf.py
+++ uk.ac.diamond.scisoft.analysis/src/doc/Diamond_SciSoft_Analysis_Developer_Guide/source/conf.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ###
 # Copyright 2011 Diamond Light Source Ltd.
 # 
@@ -13,8 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###
-
-# -*- coding: utf-8 -*-
 
 # get standard configurations settings
 import os
--- uk.ac.diamond.scisoft.python/doc/Diamond_SciSoft_Python_Guide/source/conf.py
+++ uk.ac.diamond.scisoft.python/doc/Diamond_SciSoft_Python_Guide/source/conf.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ###
 # Copyright 2011 Diamond Light Source Ltd.
 # 
@@ -13,21 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###
-
-# This is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License version 3 as published by the Free
-# Software Foundation.
-# 
-# This software is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-# Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along
-# with this software. If not, see <http://www.gnu.org/licenses/>.
-###
-
-# -*- coding: utf-8 -*-
 
 # get standard configurations settings
 import os

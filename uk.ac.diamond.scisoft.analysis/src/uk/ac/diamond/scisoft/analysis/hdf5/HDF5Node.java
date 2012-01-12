14ad263097dfd6f8fc6d17536216ee24de7f9147
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-01-12 09:10:41 +0000

    SCISOFT-CORE: fix cherry pick wrongness

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/hdf5/HDF5Node.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/hdf5/HDF5Node.java
@@ -89,7 +89,7 @@ public class HDF5Node implements Serializable {
 	/**
 	 * @return iterator over attribute names in node
 	 */
-	public Iterator<String> attributeNameIterator() {
+	public Iterator<String> getAttributeNameIterator() {
 		return attributes.keySet().iterator();
 	}
 

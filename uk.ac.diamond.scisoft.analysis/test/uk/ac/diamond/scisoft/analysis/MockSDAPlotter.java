1e5b45c8e8fc5dbcaf26c789eaf69f4d589684c7
Jonah Graham <jonah@kichwacoders.com>
2012-02-09 16:10:55 +0000

    SCISOFT-CORE: Add new method in MockSDAPlotter to ease breakpoint insertion

--- uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/MockSDAPlotter.java
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/MockSDAPlotter.java
@@ -26,391 +26,398 @@ import uk.ac.diamond.scisoft.analysis.plotserver.GuiPlotMode;
 
 public class MockSDAPlotter implements ISDAPlotter {
 
+	/**
+	 * Common creator of exception so a single breakpoint can be placed to catch any calls.
+	 */
+	private AssertionFailedError newShouldNotBeCalled() {
+		return new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+	}
+
 	@Override
 	public void plot(String plotName, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, String title, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, IDataset xAxis, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, IDataset xAxis, IDataset xAxis2, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, String title, IDataset xAxis, IDataset[] yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, String title, IDataset xAxis, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void plot(String plotName, IDataset xAxis, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plot(String plotName, IDataset[] xAxes, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plot(String plotName, String title, IDataset[] xAxis, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updatePlot(String plotName, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updatePlot(String plotName, IDataset xAxis, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updatePlot(String plotName, IDataset xAxis, IDataset xAxis2, IDataset yAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updatePlot(String plotName, IDataset xAxis, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updatePlot(String plotName, IDataset[] xAxes, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void imagePlot(String plotName, String imageFileName) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void imagePlot(String plotName, IDataset image) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void imagesPlot(String plotName, IDataset[] images) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void imagePlot(String plotName, IDataset xAxis, IDataset yAxis, IDataset image) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void imagesPlot(String plotName, IDataset xAxis, IDataset yAxis, IDataset[] images) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlot(String plotName, IDataset xCoords, IDataset yCoords, int size) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlot(String plotName, AbstractCompoundDataset[] coordPairs, int[] sizes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlot(String plotName, AbstractCompoundDataset[] coordPairs, IDataset[] sizes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlot(String plotName, IDataset xCoords, IDataset yCoords, IDataset sizes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlotOver(String plotName, IDataset xCoords, IDataset yCoords, IDataset sizes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter2DPlotOver(String plotName, IDataset xCoords, IDataset yCoords, int size) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter3DPlot(String plotName, IDataset xCoords, IDataset yCoords, IDataset zCoords, int size)
 			throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter3DPlot(String plotName, IDataset xCoords, IDataset yCoords, IDataset zCoords, IDataset sizes)
 			throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter3DPlotOver(String plotName, IDataset xCoords, IDataset yCoords, IDataset zCoords, int size)
 			throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void scatter3DPlotOver(String plotName, IDataset xCoords, IDataset yCoords, IDataset zCoords, IDataset sizes)
 			throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void surfacePlot(String plotName, IDataset data) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void surfacePlot(String plotName, IDataset xAxis, IDataset data) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void surfacePlot(String plotName, IDataset xAxis, IDataset yAxis, IDataset data) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void stackPlot(String plotName, IDataset xAxis, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void stackPlot(String plotName, IDataset xAxis, IDataset[] yAxes, IDataset zAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void stackPlot(String plotName, IDataset[] xAxes, IDataset[] yAxes) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void stackPlot(String plotName, IDataset[] xAxes, IDataset[] yAxes, IDataset zAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void updateStackPlot(String plotName, IDataset[] xAxes, IDataset[] yAxes, IDataset zAxis) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int maxFiles, int nthFile) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int order) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int order, String[] suffices, int gridColumns,
 			boolean rowMajor) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int order, String regex, String[] suffices,
 			int gridColumns, boolean rowMajor) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int order, String[] suffices, int gridColumns,
 			boolean rowMajor, int maxFiles, int jumpBetween) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public int scanForImages(String viewName, String pathname, int order, String nameregex, String[] suffices,
 			int gridColumns, boolean rowMajor, int maxFiles, int jumpBetween) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void volumePlot(String viewName, String rawvolume, int headerSize, int voxelType, int xdim, int ydim,
 			int zdim) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void volumePlot(String viewName, IDataset volume) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void volumePlot(String viewName, String dsrvolume) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void clearPlot(String viewName) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void setupNewImageGrid(String viewName, int gridRows, int gridColumns) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void setupNewImageGrid(String viewName, int images) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset[] datasets) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset[] datasets, boolean store) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, String filename, int gridX, int gridY) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, String filename) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset dataset) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset dataset, boolean store) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset dataset, int gridX, int gridY) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void plotImageToGrid(String viewName, IDataset dataset, int gridX, int gridY, boolean store)
 			throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public void setGuiBean(String plotName, GuiBean bean) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public GuiBean getGuiBean(String plotName) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void setDataBean(String plotName, DataBean bean) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 
 	}
 
 	@Override
 	public DataBean getDataBean(String plotName) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public GuiBean getGuiStateForPlotMode(String plotName, GuiPlotMode plotMode) {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public String[] getGuiNames() throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void viewNexusTree(String viewer, HDF5File tree) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 	@Override
 	public void viewHDF5Tree(String viewer, HDF5File tree) throws Exception {
-		throw new AssertionFailedError("Methods in MockSDAPlotter should not be called");
+		throw newShouldNotBeCalled();
 	}
 
 }

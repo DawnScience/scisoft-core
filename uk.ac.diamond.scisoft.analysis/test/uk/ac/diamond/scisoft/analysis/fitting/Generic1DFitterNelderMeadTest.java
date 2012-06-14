a0594a3672f9f7e649929d86e5ec9ae656fff65d
matthew.gerring <matthew.gerring@gmail.com>
2012-06-14 09:29:16 +0100

    Change to ensure    List<APeak> Generic1DFitter.fitPeaks(...) is still available in API.

--- uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/fitting/Generic1DFitterNelderMeadTest.java
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/fitting/Generic1DFitterNelderMeadTest.java
@@ -182,7 +182,7 @@ public class Generic1DFitterNelderMeadTest {
 	}
 	
 	private void FittingTest(int[] peakPos, DoubleDataset data, APeak peakFunction) {
-		List<CompositeFunction> fittedPeakList = Generic1DFitter.fitPeaks(xAxis, data, peakFunction, new NelderMead(0.00001),
+		List<CompositeFunction> fittedPeakList = Generic1DFitter.fitPeakFunctions(xAxis, data, peakFunction, new NelderMead(0.00001),
 				smoothing, numPeaks, threashold, autoStopping, backgroundDominated);
 		double[] fittedPeakPos = new double[fittedPeakList.size()];
 		int i = 0;

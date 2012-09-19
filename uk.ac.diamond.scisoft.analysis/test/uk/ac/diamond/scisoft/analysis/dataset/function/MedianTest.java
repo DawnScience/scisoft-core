1308498f916f4658106b8f422518c70aea59118e
Jacob Filik <jacob.filik@diamond.ac.uk>
2012-09-19 15:17:46 +0100

    Add a simple unit test for median filter

---
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/dataset/function/MedianTest.java
@@ -0,0 +1,43 @@
+/*-
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.analysis.dataset.function;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.DoubleDataset;
+
+public class MedianTest extends TestCase {
+
+	@Test
+	public void testExecute() {
+		double[] x = {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.};
+		
+		AbstractDataset d = new DoubleDataset(x);
+		//Test for single dataset
+		Median m = new Median(5);
+		AbstractDataset filterResults = m.value(d).get(0);
+		//assuming edge cases use smaller, asymmetric window
+		assertEquals(filterResults.getDouble(0),2, 1e-8);
+		//clear of edge effects
+		assertEquals(filterResults.getDouble(5),6, 1e-8);
+	}
+
+	
+}

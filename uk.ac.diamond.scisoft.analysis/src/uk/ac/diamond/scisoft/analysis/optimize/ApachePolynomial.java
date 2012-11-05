d825b78d96116018d40a9bb13c485545863f8bb3
Mark Basham <mark.basham@diamond.ac.uk>
2012-11-05 13:09:49 +0000

    Added jakes polynomial smoothing to the central tools, However not replaced it in XAFS as it is backported here to apache 2.2

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/optimize/ApachePolynomial.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/optimize/ApachePolynomial.java
@@ -16,6 +16,7 @@
 
 package uk.ac.diamond.scisoft.analysis.optimize;
 
+import org.apache.commons.math.optimization.OptimizationException;
 import org.apache.commons.math.optimization.fitting.PolynomialFitter;
 import org.apache.commons.math.optimization.general.LevenbergMarquardtOptimizer;
 import org.apache.commons.math.analysis.polynomials.PolynomialFunction;
@@ -34,6 +35,53 @@ public class ApachePolynomial {
 		
 		PolynomialFunction fitted = fitter.fit();
 		return fitted.getCoefficients();
+		
+	}
+	
+	/**
+	 * Calculate a polynomial filtered dataset using a specified window size and polynomial order.
+	 * 
+	 * @param x The abscissa (generally energy/wavelength etc)
+	 * @param y The ordinate to be smoothed
+	 * @param windowSize The size of window used in the smoothing
+	 * @param polyOrder The order of the polynomial fitted to the window
+	 * @return result The smoothed data set
+	 * @throws OptimizationException 
+	 */
+	public AbstractDataset getPolynomialSmoothed(final AbstractDataset x, final AbstractDataset y,int windowSize, int polyOrder) throws OptimizationException{
+		//Could probably do with more sanity check on relative size of window vs polynomial but doesnt seem to trip up
+		// So we'll see how it goes...
+		
+		//integer divide window size so window is symmetric around point
+		int window = windowSize/2;
+		
+		PolynomialFitter fitter = new PolynomialFitter(polyOrder, new LevenbergMarquardtOptimizer());
+		double dx = x.getDouble(1) - x.getDouble(0); //change in x for edge extrapolation
+		int xs =  x.getSize();
+		AbstractDataset result = AbstractDataset.zeros(y);
+		
+		for (int idx = 0; idx < xs; idx++){
+			
+			fitter.clearObservations();
+			
+			//Deal with edge cases:
+			//In both cases extend x edge by dx required for window size
+			//Pad y with first or last value
+			for (int idw = -window; idw < window+1; idw++) {
+				if (idx+idw < 0)
+					fitter.addObservedPoint(1,x.getDouble(0)+(dx*(idx+idw)), y.getDouble(0));
+				else if ((idx + idw) > (x.getSize()-1))
+					fitter.addObservedPoint(1,x.getDouble(xs-1) + dx*(idx + idw -(xs-1)), y.getDouble(xs-1));
+				else
+					fitter.addObservedPoint(1,x.getDouble(idx+idw), y.getDouble(idx+idw));
+				
+			}
+			
+			PolynomialFunction fitted = new PolynomialFunction(fitter.fit().getCoefficients());
+			result.set(fitted.value(x.getDouble(idx)),idx);	
+		}
+		
+		return result;
 		
 	}
 }

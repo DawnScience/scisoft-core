7411d6340f7ab0ea62f2513e56422ab03572b362
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-12-07 11:38:18 +0000

    Removed workaround for import now Jython has been updated

--- uk.ac.diamond.scisoft.python/src/scisoftpy/nexus/__init__.py
+++ uk.ac.diamond.scisoft.python/src/scisoftpy/nexus/__init__.py
@@ -16,8 +16,7 @@
 
 import os
 if os.name == 'java':
-    # jython 2.5.1 import bug (fixed in 2.5.2)
-    from scisoftpy.jython.jyhdf5io import SDS
+    from ..jython.jyhdf5io import SDS
 else:
     from ..python.pynxio import SDS #@Reimport
 

38e1c59743f624fc0ebb80ec9d19781ba2f8cba9
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-12-11 10:16:30 +0000

    Tidied up two loaders

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/PilatusEdfLoader.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/PilatusEdfLoader.java
@@ -23,9 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 

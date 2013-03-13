286485dab3a66e82d0853a1f417133bd906e307c
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-03-13 18:26:09 +0000

    Added initial version of fit rings to q-space with unit tests and added monitoring to other methods

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/diffraction/PowderRingsUtils.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/diffraction/PowderRingsUtils.java
@@ -22,10 +22,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
 
+import javax.measure.unit.SI;
+
+import org.apache.commons.math.ConvergenceException;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.analysis.DifferentiableMultivariateVectorialFunction;
+import org.apache.commons.math.analysis.MultivariateMatrixFunction;
+import org.apache.commons.math.optimization.VectorialPointValuePair;
+import org.apache.commons.math.optimization.general.LevenbergMarquardtOptimizer;
 import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.uncommons.maths.combinatorics.CombinationGenerator;
 
+import uk.ac.diamond.scisoft.analysis.crystallography.HKL;
 import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.BooleanDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.BooleanIterator;
@@ -37,6 +47,7 @@ import uk.ac.diamond.scisoft.analysis.fitting.Generic1DFitter;
 import uk.ac.diamond.scisoft.analysis.fitting.IConicSectionFitFunction;
 import uk.ac.diamond.scisoft.analysis.fitting.IConicSectionFitter;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.IdentifiedPeak;
+import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
 import uk.ac.diamond.scisoft.analysis.roi.CircularROI;
 import uk.ac.diamond.scisoft.analysis.roi.EllipticalFitROI;
 import uk.ac.diamond.scisoft.analysis.roi.EllipticalROI;
@@ -83,17 +94,17 @@ public class PowderRingsUtils {
 	private static final double MAX_FWHM_FACTOR = 2;
 	private static final double RING_SEPARATION = 4;
 
-	public static PolylineROI findPOIsNearCircle(AbstractDataset image, BooleanDataset mask, CircularROI circle) {
-		return findPOIsNearCircle(image, mask, circle, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
+	public static PolylineROI findPOIsNearCircle(IMonitor mon, AbstractDataset image, BooleanDataset mask, CircularROI circle) {
+		return findPOIsNearCircle(mon, image, mask, circle, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
 	}
 
-	public static PolylineROI findPOIsNearCircle(AbstractDataset image, BooleanDataset mask, CircularROI circle,
+	public static PolylineROI findPOIsNearCircle(IMonitor mon, AbstractDataset image, BooleanDataset mask, CircularROI circle,
 			double arcLength, double radialDelta, int maxPoints) {
-		return findPOIsNearEllipse(image, mask, new EllipticalROI(circle), arcLength, radialDelta, maxPoints);
+		return findPOIsNearEllipse(mon, image, mask, new EllipticalROI(circle), arcLength, radialDelta, maxPoints);
 	}
 
-	public static PolylineROI findPOIsNearEllipse(AbstractDataset image, BooleanDataset mask, EllipticalROI ellipse) {
-		return findPOIsNearEllipse(image, mask, ellipse, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
+	public static PolylineROI findPOIsNearEllipse(IMonitor mon, AbstractDataset image, BooleanDataset mask, EllipticalROI ellipse) {
+		return findPOIsNearEllipse(mon, image, mask, ellipse, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
 	}
 
 	/**
@@ -101,6 +112,7 @@ public class PowderRingsUtils {
 	 * <p>
 	 * The ellipse is divided into sub-areas and these POIs are considered to
 	 * be the locations of maximum pixel intensities found within those sub-areas.
+	 * @param mon
 	 * @param image
 	 * @param mask (can be null)
 	 * @param ellipse
@@ -109,7 +121,7 @@ public class PowderRingsUtils {
 	 * @param maxPoints maximum number of points to return ( 
 	 * @return polyline ROI
 	 */
-	public static PolylineROI findPOIsNearEllipse(AbstractDataset image, BooleanDataset mask, EllipticalROI ellipse,
+	public static PolylineROI findPOIsNearEllipse(IMonitor mon, AbstractDataset image, BooleanDataset mask, EllipticalROI ellipse,
 			double arcLength, double radialDelta, int maxPoints) {
 		if (image.getRank() != 2) {
 			logger.error("Dataset must have two dimensions");
@@ -211,6 +223,9 @@ public class PowderRingsUtils {
 //			System.err.printf("Slice: %s, %s has max at %s\n", Arrays.toString(start), Arrays.toString(stop), Arrays.toString(pos));
 				pointSet.add(new PointROI(pos[1]+start[1], pos[0]+start[0]));
 			}
+
+			if (mon != null)
+				mon.worked(1);
 		}
 
 		// analyse pixel values
@@ -255,13 +270,16 @@ public class PowderRingsUtils {
 				polyline.insertPoint(p);
 			}
 		}
+		if (mon != null)
+			mon.worked(polyline.getNumberOfPoints());
+
 		logger.debug("Used {} of {} pixels", polyline.getNumberOfPoints(), pointSet.size());
 
 		return polyline;
 	}
 
-	public static EllipticalFitROI fitAndTrimOutliers(PolylineROI points, boolean circleOnly) {
-		return fitAndTrimOutliers(points, RADIAL_DELTA, circleOnly);
+	public static EllipticalFitROI fitAndTrimOutliers(IMonitor mon, PolylineROI points, boolean circleOnly) {
+		return fitAndTrimOutliers(mon, points, RADIAL_DELTA, circleOnly);
 	}
 
 	/**
@@ -272,7 +290,7 @@ public class PowderRingsUtils {
 	 * @param circleOnly if true, then fit a circle
 	 * @return fitted ellipse
 	 */
-	public static EllipticalFitROI fitAndTrimOutliers(PolylineROI points, double trimDelta, boolean circleOnly) {
+	public static EllipticalFitROI fitAndTrimOutliers(IMonitor mon, PolylineROI points, double trimDelta, boolean circleOnly) {
 		try {
 			EllipticalFitROI efroi = new EllipticalFitROI(points, circleOnly);
 
@@ -300,6 +318,8 @@ public class PowderRingsUtils {
 			if (m < n) {
 				logger.debug("Found some outliers: {}/{}", n - m, n);
 				efroi.setPoints(npts);
+				if (mon != null)
+					mon.worked(m);
 			}
 
 			return efroi;
@@ -320,8 +340,8 @@ public class PowderRingsUtils {
 	 * @param roi initial ellipse
 	 * @return list of ellipses
 	 */
-	public static List<EllipticalROI> findOtherEllipses(AbstractDataset image, BooleanDataset mask, EllipticalROI roi) {
-		return findOtherEllipses(image, mask, roi, RADIAL_MIN, RADIAL_DELTA, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
+	public static List<EllipticalROI> findOtherEllipses(IMonitor mon, AbstractDataset image, BooleanDataset mask, EllipticalROI roi) {
+		return findOtherEllipses(mon, image, mask, roi, RADIAL_MIN, RADIAL_DELTA, ARC_LENGTH, RADIAL_DELTA, MAX_POINTS);
 	}
 
 	/**
@@ -340,7 +360,7 @@ public class PowderRingsUtils {
 	 * @param maxPoints
 	 * @return list of ellipses
 	 */
-	public static List<EllipticalROI> findOtherEllipses(AbstractDataset image, BooleanDataset mask, EllipticalROI roi,
+	public static List<EllipticalROI> findOtherEllipses(IMonitor mon, AbstractDataset image, BooleanDataset mask, EllipticalROI roi,
 			double radialMin, double radialDelta, double arcLength, double trimDelta, int maxPoints) {
 		if (image.getRank() != 2) {
 			logger.error("Dataset must have two dimensions");
@@ -358,10 +378,11 @@ public class PowderRingsUtils {
 		double[] ec = roi.getPoint();
 		TreeSet<Double> majors = new TreeSet<Double>();
 
-		findMajorAxes(majors, image, mask, roi, radialMin, radialDelta, ec, 0 - ec[0], 0 - ec[1]); // TL
-		findMajorAxes(majors, image, mask, roi, radialMin, radialDelta, ec, w - ec[0], 0 - ec[1]); // TR
-		findMajorAxes(majors, image, mask, roi, radialMin, radialDelta, ec, w - ec[0], h - ec[1]); // BR
-		findMajorAxes(majors, image, mask, roi, radialMin, radialDelta, ec, 0 - ec[0], h - ec[1]); // BL
+		// TODO farm this out across several threads
+		findMajorAxes(mon, majors, image, mask, roi, radialMin, radialDelta, ec, 0 - ec[0], 0 - ec[1]); // TL
+		findMajorAxes(mon, majors, image, mask, roi, radialMin, radialDelta, ec, w - ec[0], 0 - ec[1]); // TR
+		findMajorAxes(mon, majors, image, mask, roi, radialMin, radialDelta, ec, w - ec[0], h - ec[1]); // BR
+		findMajorAxes(mon, majors, image, mask, roi, radialMin, radialDelta, ec, 0 - ec[0], h - ec[1]); // BL
 
 		// and finally find POIs
 		List<EllipticalROI> ells = new ArrayList<EllipticalROI>();
@@ -382,9 +403,9 @@ public class PowderRingsUtils {
 			} else {
 				EllipticalROI er = new EllipticalROI(a, a/aspect, roi.getAngle(), ec[0], ec[1]);
 				try {
-					PolylineROI polyline = findPOIsNearEllipse(image, mask, er, arcLength, radialDelta, maxPoints);
+					PolylineROI polyline = findPOIsNearEllipse(mon, image, mask, er, arcLength, radialDelta, maxPoints);
 					if (polyline.getNumberOfPoints() > 2) {
-						er = fitAndTrimOutliers(polyline, trimDelta, roi.isCircular());
+						er = fitAndTrimOutliers(mon, polyline, trimDelta, roi.isCircular());
 						if (Math.abs(er.getSemiAxis(0) - last) < RING_SEPARATION) { // omit close rings
 							last = a;
 							System.err.println("Dropped as fit is too close");
@@ -405,6 +426,8 @@ public class PowderRingsUtils {
 					logger.debug("Problem with {}", er, e);
 					last = a;
 				}
+				if (mon != null)
+					mon.worked(1);
 			}
 		}
 
@@ -414,6 +437,7 @@ public class PowderRingsUtils {
 	/**
 	 * Find major axes by looking along thick line given by relative coordinates to centre for
 	 * maximum intensity values
+	 * @param mon
 	 * @param axes
 	 * @param image
 	 * @param mask
@@ -424,7 +448,7 @@ public class PowderRingsUtils {
 	 * @param dx
 	 * @param dy
 	 */
-	private static void findMajorAxes(TreeSet<Double> axes, AbstractDataset image, AbstractDataset mask, EllipticalROI roi, double radialMin, double radialDelta, double[] centre, double dx, double dy) {
+	private static void findMajorAxes(IMonitor mon, TreeSet<Double> axes, AbstractDataset image, AbstractDataset mask, EllipticalROI roi, double radialMin, double radialDelta, double[] centre, double dx, double dy) {
 		RectangularROI rroi = new RectangularROI();
 		rroi.setPoint(centre);
 		rroi.setAngle(Math.atan2(dy, dx));
@@ -433,6 +457,8 @@ public class PowderRingsUtils {
 		rroi.setClippingCompensation(true);
 		AbstractDataset profile = ROIProfile.maxInBox(image, mask, rroi)[0];
 		List<IdentifiedPeak> peaks = Generic1DFitter.findPeaks(AbstractDataset.arange(profile.getSize(), AbstractDataset.INT), profile, PEAK_SMOOTHING);
+		if (mon != null)
+			mon.worked(profile.getSize());
 
 		DescriptiveStatistics stats = new DescriptiveStatistics();
 		for (IdentifiedPeak p : peaks) {
@@ -463,6 +489,294 @@ public class PowderRingsUtils {
 				break;
 			}
 			axes.add(l*majorFactor);
+		}
+		if (mon != null)
+			mon.worked(peaks.size());
+
+	}
+
+	static QSpaceFitFunction createQFitFunction(List<EllipticalROI> ellipses, double pix, int n) {
+		double[] known1 = new double[n];
+		double[] known2 = new double[n];
+
+		double bestAngle = 0;
+		double min = Double.POSITIVE_INFINITY;
+		for (int i = 0; i < n; i++) {
+			EllipticalROI e = ellipses.get(i);
+			if (e instanceof EllipticalFitROI) {
+				double rms = ((EllipticalFitROI) e).getRMS();
+				if (rms < min) {
+					min = rms;
+					bestAngle = e.getAngle();
+				}
+			}
+			double a = e.getSemiAxis(0);
+			double rs = a/e.getSemiAxis(1);
+			a *= pix;
+			rs = 1./(rs*rs);
+			known1[i] = a*rs; // in mm
+			known2[i] = rs;
+		}
+		return new QSpaceFitFunction(known1, known2, bestAngle);
+	}
+
+	/**
+	 * @param mon
+	 * @param detector
+	 * @param env
+	 * @param ellipses
+	 * @param spacings a list of possible spacings
+	 * @return q-space
+	 */
+	public static QSpace fitToQSpace(IMonitor mon, DetectorProperties detector, DiffractionCrystalEnvironment env, List<EllipticalROI> ellipses, List<HKL> spacings) {
+		double[] init = new double[] {env.getWavelength()*1e-7, detector.getDetectorDistance(), Math.sin(detector.getTiltAngle())};
+		logger.debug("Init: {}", init);
+		int n = ellipses.size();
+
+		double dmin = spacings.get(0).getD().doubleValue(SI.MILLIMETRE);
+		{
+			double rmin = init[1] * Math.tan(2.0 * Math.asin(0.5 * init[0] / dmin)) / detector.getVPxSize();
+			int l = 0;
+			for (EllipticalROI e : ellipses) {
+				if (e.getSemiAxis(0) >= rmin)
+					break;
+				l++;
+			}
+
+			if (l >= n) {
+				throw new IllegalArgumentException("Maybe all rings are too small!");
+			}
+			if (l > 0) {
+				logger.debug("Discarding first {} rings", l);
+				ellipses = ellipses.subList(l, n);
+				n = ellipses.size();
+			}
+		}
+		
+
+		if (n > spacings.size()) {
+			logger.warn("The number of d-spacings ({}) should be greater than or equal to {}: using inner rings", spacings.size(), n);
+			n = spacings.size();
+		}
+		logger.debug("Using {} rings:", n);
+		{
+			for (EllipticalROI e : ellipses)
+				logger.debug("    {}", e);
+		}
+
+		QSpaceFitFunction f = createQFitFunction(ellipses, detector.getVPxSize(), n);
+		if (mon != null)
+			mon.worked(1);
+
+		// set up a combination generator for all (but first spacing)
+		List<Double> s = new ArrayList<Double>();
+		for (int i = 0, imax = spacings.size(); i < imax; i++ ) {
+			HKL d = spacings.get(i);
+			s.add(d.getD().doubleValue(SI.MILLIMETRE));
+		}
+		CombinationGenerator<Double> gen = new CombinationGenerator<Double>(s, n);
+
+		double min = Double.POSITIVE_INFINITY;
+		LevenbergMarquardtOptimizer opt = new LevenbergMarquardtOptimizer();
+//		opt.setMaxEvaluations(2000);
+		double[] params = null;
+		List<Double> fSpacings = null;
+
+		for (List<Double> list : gen) { // find combination that minimizes residuals
+//			list.add(0, dmin);
+			f.setSpacings(list);
+
+			try {
+				VectorialPointValuePair result = opt.optimize(f, f.getTarget(), f.getWeight(), init);
+
+//				logger.info("Q-space fit: rms = {}, x^2 = {}", opt.getRMS(), opt.getChiSquare());
+				double res = opt.getRMS();
+				if (res < min) {
+					min = res;
+					params = result.getPoint();
+					fSpacings = list;
+				}
+			} catch (FunctionEvaluationException e) {
+				logger.error("Function evaluation problem", e);
+				// cannot happen
+			} catch (IllegalArgumentException e) {
+				logger.error("Start point has wrong dimension", e);
+				// should not happen!
+			} catch (ConvergenceException e) {
+				logger.error("Convergence problem: max iterations ({}) exceeded", opt.getMaxIterations());
+				// ignore
+//				throw new IllegalArgumentException("Problem with optimizer converging");
+			}
+			if (mon != null)
+				mon.worked(10);
+		}
+
+		if (fSpacings == null || params == null) {
+			logger.warn("Problem with fitting - as could not find a single fit!");
+			return null;
+		}
+
+		double wlen = params[0]*1e7;
+		double tilt = Math.toDegrees(Math.asin(params[2]));
+		logger.debug("Parameters: w {}, D {}, e {} (min {})", new Object[] {wlen, params[1], tilt, min});
+		logger.debug("Spacings used: {}", fSpacings);
+		f.setSpacings(fSpacings);
+		logger.debug("Residual values for {}: {}", f.getRMS(params), f.value);
+
+		DetectorProperties nDetector = new DetectorProperties(detector);
+		nDetector.setDetectorDistance(params[1]);
+		nDetector.setNormalAnglesInDegrees(tilt, 0, f.getAngle());
+		DiffractionCrystalEnvironment nEnv = new DiffractionCrystalEnvironment(wlen);
+		return new QSpace(nDetector, nEnv);
+	}
+
+	/**
+	 * LS function uses parameters: wavelength (mm), perpendicular distance (mm), sine of tilt angle
+	 */
+	static class QSpaceFitFunction implements DifferentiableMultivariateVectorialFunction {
+
+		private double[] target;
+		private double[] spacing; // in mm
+		private double[] value;
+
+		private int n;
+		private int twoN;
+		private double[][] jac;
+		private double[] weight;
+		private double angle;
+
+		private static final double EXTRA_WEIGHT = 10; // for distance
+
+		public QSpaceFitFunction(double[] known1, double[] known2, double bestAngle) {
+			n = known1.length;
+			twoN = 2 * n;
+			angle = bestAngle;
+			target = new double[twoN];
+			weight = new double[twoN];
+			for (int i = 0, j = 0; i < n; i++) {
+				double k = known1[i];
+				target[j] = k;
+				weight[j++] = EXTRA_WEIGHT/(k*k); // make residual similar magnitude to known2's
+				target[j] = known2[i];
+				weight[j++] = 1;
+			}
+			spacing = new double[n];
+			value = new double[twoN];
+			jac = new double[twoN][3];
+		}
+
+		public double getAngle() {
+			return angle;
+		}
+
+		public double[] getWeight() {
+			return weight;
+		}
+
+		public double[] getTarget() {
+			return target;
+		}
+
+		/**
+		 * @param spacings (in mm)
+		 */
+		public void setSpacings(List<Double> spacings) {
+			for (int i = 0; i < n; i++) {
+				spacing[i] = spacings.get(i);
+			}
+		}
+
+		/**
+		 * @param arg wavelength (mm), perpendicular distance (mm), sine of tilt angle
+		 * @return values
+		 */
+		@Override
+		public double[] value(double[] arg) throws FunctionEvaluationException, IllegalArgumentException {
+			calcValues(arg);
+			return value;
+		}
+
+		private void calcValues(double[] arg) {
+			double wlen = arg[0];
+			double dist = arg[1];
+			double setas = arg[2] * arg[2];
+
+			for (int i = 0, j = 0; i < n; i++) {
+				double si = 0.5*wlen/spacing[i];
+				double x = 0.5 - si*si;
+				double ti = Math.sqrt(0.5 + x)*si/x;
+				value[j++] = ti*dist;
+				value[j++] = 1 - setas*(1+ti*ti);
+			}
+		}
+
+		private void calcResidualValues(double[] arg) {
+			double wlen = arg[0];
+			double dist = arg[1];
+			double setas = arg[2] * arg[2];
+
+			for (int i = 0, j = 0; i < n; i++) {
+				double si = 0.5*wlen/spacing[i];
+				double x = 0.5 - si*si;
+				double ti = Math.sqrt(0.5 + x)*si/x;
+				double t = ti*dist - target[j];
+				value[j] = t*t*weight[j];
+				j++;
+				t = 1 - setas*(1+ti*ti) - target[j];
+				value[j] = t*t*weight[j];
+				j++;
+			}
+		}
+
+		/**
+		 * @param arg wavelength (mm), perpendicular distance (mm), sine of tilt angle
+		 * @return root mean square of residuals
+		 */
+		public double getRMS(double[] arg) {
+			calcResidualValues(arg);
+			double s = 0;
+			for (int i = 0; i < twoN; i++) {
+				s += value[i];
+			}
+			return s/twoN;
+		}
+
+		@Override
+		public MultivariateMatrixFunction jacobian() {
+			return new MultivariateMatrixFunction() {
+				@Override
+				public double[][] value(double[] point) throws FunctionEvaluationException, IllegalArgumentException {
+					calcJacobian(point);
+					return jac;
+				}
+			};
+		}
+
+		private void calcJacobian(double[] arg) {
+			double wlen = arg[0];
+			double hdist = 0.5 * arg[1];
+			double hseta = arg[2];
+			double setas = hseta * hseta;
+			hseta *= 0.5;
+
+			for (int i = 0, j = 0; i < n; i++) {
+				double otdi = 0.5 / spacing[i];
+				double si = wlen * otdi;
+				double x = 0.5 - si * si;
+				double ti = Math.sqrt(0.5 + x) * si / x;
+				double fx = otdi / x;
+
+				jac[j][0] = hdist * ti * fx;
+				jac[j][1] = ti;
+				jac[j][2] = 0;
+				j++;
+
+				double gx = 1 / (x * x);
+				jac[j][0] = -fx * setas * si * gx;
+				jac[j][1] = 0;
+				jac[j][2] = -hseta * gx;
+				j++;
+			}
 		}
 	}
 }
--- uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/diffraction/PowderRingsUtilsTest.java
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/diffraction/PowderRingsUtilsTest.java
@@ -16,20 +16,36 @@
 
 package uk.ac.diamond.scisoft.analysis.diffraction;
 
+import gda.analysis.io.ScanFileHolderException;
 import gda.util.TestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+import javax.vecmath.Vector3d;
+
+import org.jscience.physics.amount.Amount;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import uk.ac.diamond.scisoft.analysis.crystallography.HKL;
 import uk.ac.diamond.scisoft.analysis.crystallography.MillerSpace;
 import uk.ac.diamond.scisoft.analysis.crystallography.UnitCell;
 import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.DoubleDataset;
+import uk.ac.diamond.scisoft.analysis.diffraction.PowderRingsUtils.QSpaceFitFunction;
 import uk.ac.diamond.scisoft.analysis.io.ADSCImageLoader;
+import uk.ac.diamond.scisoft.analysis.io.DataHolder;
+import uk.ac.diamond.scisoft.analysis.io.NumPyFileSaver;
 import uk.ac.diamond.scisoft.analysis.roi.CircularFitROI;
 import uk.ac.diamond.scisoft.analysis.roi.CircularROI;
 import uk.ac.diamond.scisoft.analysis.roi.EllipticalFitROI;
+import uk.ac.diamond.scisoft.analysis.roi.EllipticalROI;
 import uk.ac.diamond.scisoft.analysis.roi.PolylineROI;
 
 public class PowderRingsUtilsTest {
@@ -42,15 +58,23 @@ public class PowderRingsUtilsTest {
 		}
 	}
 
-	// NIST Silicon SRM 640C
-	static double wavelength = 0.15405929; // in nm
-	static double lattice = 0.5431020504;  // in nm
+	// NIST Silicon SRM 640C as mentioned in IUCR International Tables vC 5.2.10
+	static final double WAVELENGTH = 1.5405929; // in nm
+	static final double LATTICE_PARAMETER = 0.5431195;  // in nm
+	static final double[] CONE_ANGLES = {28.411, 47.300, 56.120};
+//	static final double[] CONE_ANGLES = {28.411, 47.300, 56.120, 69.126, 76.372, 88.025,
+//		94.947, 106.701, 114.084, 127.534, 136.880, 158.603}; // in degrees
 	UnitCell siliconCell;
 	MillerSpace mSpace;
+	private ArrayList<HKL> spacings;
 
 	@Before
 	public void setUpSilicon() {
-		siliconCell = new UnitCell(lattice);
+		siliconCell = new UnitCell(LATTICE_PARAMETER);
+		spacings = new ArrayList<HKL>();
+		for (double c : CONE_ANGLES) {
+			spacings.add(new HKL(Amount.valueOf(0.5 * WAVELENGTH / Math.sin(0.5 * Math.toRadians(c)), NonSI.ANGSTROM)));
+		}
 //		mSpace = new MillerSpace(siliconCell, null);
 	}
 
@@ -64,12 +88,83 @@ public class PowderRingsUtilsTest {
 		}
 
 		CircularROI roi = new CircularROI(631, 1528.6, 1533.9);
-		PolylineROI points = PowderRingsUtils.findPOIsNearCircle(image, null, roi);
+		PolylineROI points = PowderRingsUtils.findPOIsNearCircle(null, image, null, roi);
 		System.err.println(points);
 		System.err.println(new CircularFitROI(points));
 		System.err.println(new EllipticalFitROI(points));
 
-		System.err.println(PowderRingsUtils.fitAndTrimOutliers(points, true));
-		System.err.println(PowderRingsUtils.fitAndTrimOutliers(points, false));
+		System.err.println(PowderRingsUtils.fitAndTrimOutliers(null, points, true));
+		System.err.println(PowderRingsUtils.fitAndTrimOutliers(null, points, false));
+	}
+
+	private static final int N_W = 256;
+	private static final int N_D = 256;
+	private static final int N_T = 64;
+
+	@Ignore
+	public void checkFitFunction() {
+		double pixel = 0.25; // in mm
+		double distance = 153.0; // in mm
+		double wavelength = WAVELENGTH * 1e-7; // in mm
+	
+		List<EllipticalROI> ells = new ArrayList<EllipticalROI>();
+		List<Double> list = new ArrayList<Double>();
+		for (HKL d : spacings) {
+			double xi = 0.5 * WAVELENGTH / d.getD().doubleValue(NonSI.ANGSTROM);
+			list.add(d.getD().doubleValue(SI.MILLIMETRE));
+			double om = 0.5 - xi*xi;
+			double r = distance * xi * Math.sqrt(0.5 + om) / (pixel * om);
+			ells.add(new EllipticalROI(r, 0, 0));
+		}
+
+		QSpaceFitFunction f = PowderRingsUtils.createQFitFunction(ells, pixel, ells.size());
+		f.setSpacings(list);
+		double[] init = new double[3];
+		DoubleDataset rms = new DoubleDataset(N_W, N_D, N_T);
+		int l = 0;
+		for (int i = 0; i < N_W; i++) {
+			init[0] = wavelength + (i - N_W*0.5)*1e-7*0.01; // 0.01A
+			for (int j = 0; j < N_D; j++) {
+				init[1] = distance + (j - N_D*0.5)*0.01; // 0.01mm
+				for (int k = 0; k < N_T; k++) {
+					init[2] = Math.sin((k/128.)/N_T); // 1/128 radians
+					double x = f.getRMS(init);
+					rms.setAbs(l++, x);
+				}
+			}
+		}
+		DataHolder dh = new DataHolder();
+		dh.addDataset("RMS", rms);
+
+		String file = "/tmp/rms.npy";
+		try {
+			new NumPyFileSaver(file).saveFile(dh);
+		} catch (ScanFileHolderException e) {
+			System.err.println("Problem saving file: " + file);
+		}
+	}
+
+	@Test
+	public void calibrateDetector() {
+		double pixel = 0.25; // in mm
+		double distance = 153.0; // in mm
+		DetectorProperties det = new DetectorProperties(new Vector3d(0, 0, distance*0.99), 3000, 3000, pixel, pixel, null);
+
+		DiffractionCrystalEnvironment env = new DiffractionCrystalEnvironment(WAVELENGTH*1.01);
+
+		List<EllipticalROI> ells = new ArrayList<EllipticalROI>();
+		for (HKL d : spacings) {
+			double xi = 0.5 * WAVELENGTH / d.getD().doubleValue(NonSI.ANGSTROM);
+			double om = 0.5 - xi*xi;
+			double r = distance * xi * Math.sqrt(0.5 + om) / (pixel * om);
+			ells.add(new EllipticalROI(r+Math.random()*3, 0, 0));
+		}
+
+		QSpace q = PowderRingsUtils.fitToQSpace(null, det, env, ells, spacings);
+		DetectorProperties nDet = q.getDetectorProperties();
+
+		Assert.assertEquals("Distance", distance, nDet.getDetectorDistance(), 3);
+		Assert.assertEquals("Tilt", 0, nDet.getTiltAngle(), 1e-2);
+		Assert.assertEquals("Wavelength", WAVELENGTH, q.getWavelength(), 1e-2);
 	}
 }

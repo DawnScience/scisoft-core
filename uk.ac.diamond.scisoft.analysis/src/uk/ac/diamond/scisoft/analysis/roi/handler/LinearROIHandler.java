0af6ac688f820930811b78a7ae8e6b522d6819a1
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-04-16 18:10:15 +0100

    Cosmetic re-formatting

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/roi/handler/LinearROIHandler.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/roi/handler/LinearROIHandler.java
@@ -130,38 +130,38 @@ public class LinearROIHandler extends ROIHandler {
 
 	@Override
 	public IROI interpretMouseDragging(int[] spt, int[] ept) {
-			final LinearROI lroi = (LinearROI) roi;
-			LinearROI croi = null; // return null if not a valid event
+		final LinearROI lroi = (LinearROI) roi;
+		LinearROI croi = null; // return null if not a valid event
 
-			switch (status) {
-			case RMOVE:
-				croi = lroi.copy();
-				croi.addPoint(ept);
-				croi.subPoint(spt);
-				break;
-			case NONE:
-				croi = lroi.copy();
-				croi.setEndPoint(ept);
-				break;
-			case REORIENT:
-				croi = reorient(ept);
-				break;
-			case RESIZE:
-				croi = resize(ept);
-				break;
-			case ROTATE:
-				croi = lroi.copy();
-				double ang = croi.getAngleRelativeToMidPoint(ept);
-				double[] mpt = croi.getMidPoint();
-				croi.setAngle(ang);
-				croi.setMidPoint(mpt);
-				break;
-			case CMOVE:
-				break;
-			case CRMOVE:
-				break;
-			}
+		switch (status) {
+		case RMOVE:
+			croi = lroi.copy();
+			croi.addPoint(ept);
+			croi.subPoint(spt);
+			break;
+		case NONE:
+			croi = lroi.copy();
+			croi.setEndPoint(ept);
+			break;
+		case REORIENT:
+			croi = reorient(ept);
+			break;
+		case RESIZE:
+			croi = resize(ept);
+			break;
+		case ROTATE:
+			croi = lroi.copy();
+			double ang = croi.getAngleRelativeToMidPoint(ept);
+			double[] mpt = croi.getMidPoint();
+			croi.setAngle(ang);
+			croi.setMidPoint(mpt);
+			break;
+		case CMOVE:
+			break;
+		case CRMOVE:
+			break;
+		}
 
-			return croi;
+		return croi;
 	}
 }

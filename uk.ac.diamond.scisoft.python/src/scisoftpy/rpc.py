b72c39378eb59a9a3f269d21550d7b5a3d65d1a9
Jonah Graham <jonah@kichwacoders.com>
2013-08-23 14:06:35 +0100

    Set __name__ when running addHandlers and stop using locals version of exec

--- uk.ac.diamond.scisoft.python/src/scisoftpy/rpc.py
+++ uk.ac.diamond.scisoft.python/src/scisoftpy/rpc.py
@@ -49,11 +49,14 @@ if __name__ == '__main__':
             in handler_names must be defined by code and then
             are added to the server
         '''
+        # We run in the context of the rpc.py, however we must
+        # give a __name__ other than __main__ so that a script
+        # that has if ... __main__ does not run the main code
         g = dict(globals())
-        l = dict()
-        exec(code, g, l)
+        g['__name__'] = '<addHandlerCode>'
+        exec(code, g)
         for handler_name in handler_names:
-            server.add_handler(handler_name, l[handler_name])
+            server.add_handler(handler_name, g[handler_name])
     server.add_handler('addHandlers', addHandlers)
 
     def setPlottingPort(port):

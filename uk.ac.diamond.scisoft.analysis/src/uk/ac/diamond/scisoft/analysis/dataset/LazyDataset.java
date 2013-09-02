0a2cd5cbd4cdc60f25e4cdf41bf07d836642b6b3
matthew.gerring <matthew.gerring@gmail.com>
2013-09-02 11:01:13 +0100

    Removed TODO comment.

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/dataset/LazyDataset.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/dataset/LazyDataset.java
@@ -394,7 +394,6 @@ public class LazyDataset implements ILazyDataset {
 	@Override
 	public void setLazyErrors(ILazyDataset errors) {
 		if (this==errors) return;
-		// TODO check that errors != this
 		if (errors==null) {
 			lazyErrorDelegate = null;
 			return;

dff5fdb75f5ded1b7cffb9ea746b0c4c420aa2c9
matthew.gerring <matthew.gerring@gmail.com>
2013-09-24 16:26:36 +0100

    Attempt to fix suite_tools2d_diffraction/tst_lock_meta
    Which is failing due to meta data problem.

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/DataHolder.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/DataHolder.java
@@ -92,7 +92,7 @@ public class DataHolder implements IMetadataProvider, IDataHolder, Serializable 
 
 	
 	/**
-	 * 
+	 * Does not clone the meta data.
 	 * @return shallow copy of DataHolder
 	 */
 	@Override
@@ -101,7 +101,7 @@ public class DataHolder implements IMetadataProvider, IDataHolder, Serializable 
 		DataHolder ret = new DataHolder();
 		ret.data.addAll(data);
 		ret.names.addAll(names);
-		ret.metadata    = metadata!=null ? metadata.clone() : null;
+		ret.metadata    = metadata;
 		ret.filePath    = filePath;
 		ret.loaderClass = loaderClass;
 		return ret;

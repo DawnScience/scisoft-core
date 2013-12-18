fd7e046686964458f950c69e976fda1ea0cf5906
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-12-18 16:20:40 +0000

    Added new Commons Math3 optimizers

---
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/optimize/ApacheOptimizer.java
@@ -0,0 +1,197 @@
+/*-
+ * Copyright 2013 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.analysis.optimize;
+
+import org.apache.commons.math3.analysis.MultivariateFunction;
+import org.apache.commons.math3.analysis.MultivariateVectorFunction;
+import org.apache.commons.math3.exception.TooManyEvaluationsException;
+import org.apache.commons.math3.optim.InitialGuess;
+import org.apache.commons.math3.optim.MaxEval;
+import org.apache.commons.math3.optim.PointValuePair;
+import org.apache.commons.math3.optim.SimpleBounds;
+import org.apache.commons.math3.optim.SimplePointChecker;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+import org.apache.commons.math3.optim.nonlinear.scalar.MultivariateFunctionPenaltyAdapter;
+import org.apache.commons.math3.optim.nonlinear.scalar.MultivariateOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction;
+import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunctionGradient;
+import org.apache.commons.math3.optim.nonlinear.scalar.gradient.NonLinearConjugateGradientOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.gradient.NonLinearConjugateGradientOptimizer.Formula;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.CMAESOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.MultiDirectionalSimplex;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.NelderMeadSimplex;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.PowellOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer;
+import org.apache.commons.math3.random.Well19937c;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.analysis.fitting.functions.IParameter;
+
+public class ApacheOptimizer extends AbstractOptimizer {
+	private static Logger logger = LoggerFactory.getLogger(ApacheOptimizer.class);
+
+	public MultivariateFunction createFunction() {
+		// provide the fitting function which wrappers all the normal fitting functionality
+		MultivariateFunction f = new MultivariateFunction() {
+
+			@Override
+			public double value(double[] parameters) {
+				return calculateResidual(parameters);
+			}
+		};
+
+		return f;
+	}
+
+	public MultivariateVectorFunction createGradientFunction() {
+		MultivariateVectorFunction f = new MultivariateVectorFunction() {
+			
+			@Override
+			public double[] value(double[] parameters) throws IllegalArgumentException {
+				double[] result = new double[n];
+				
+				for (int i = 0; i < n; i++) {
+					result[i] = calculateResidualDerivative(params.get(i), parameters);
+				}
+				return result;
+			}
+		};
+
+		return f;
+	}
+
+	public Long seed = null;
+
+	private static final int MAX_ITER = 10000;
+	private static final int MAX_EVAL = 1000000;
+	private static final double REL_TOL = 1e-7;
+	private static final double ABS_TOL = 1e-15;
+
+	public enum Optimizer { SIMPLEX_MD, SIMPLEX_NM, POWELL, CMAES, BOBYQA, CONJUGATE_GRADIENT }
+
+	Optimizer optimizer;
+
+	public ApacheOptimizer(Optimizer opt) {
+		optimizer = opt;
+	}
+
+	private MultivariateOptimizer createOptimizer() {
+		SimplePointChecker<PointValuePair> checker = new SimplePointChecker<PointValuePair>(REL_TOL, ABS_TOL);
+		switch (optimizer) {
+		case CONJUGATE_GRADIENT:
+			return new NonLinearConjugateGradientOptimizer(Formula.POLAK_RIBIERE, checker);
+		case BOBYQA:
+			return new BOBYQAOptimizer(n + 2);
+		case CMAES:
+			return new CMAESOptimizer(MAX_ITER, 0., true, 0, 10, seed == null ? new Well19937c() : new Well19937c(seed),
+					false, new SimplePointChecker<PointValuePair>(REL_TOL, ABS_TOL));
+		case POWELL:
+			return new PowellOptimizer(REL_TOL, ABS_TOL, checker);
+		case SIMPLEX_MD:
+		case SIMPLEX_NM:
+		default:
+			return new SimplexOptimizer(checker);
+		}
+	}
+
+	private SimpleBounds createBounds() {
+		double[] lb = new double[n];
+		double[] ub = new double[n];
+		for (int i = 0; i < n; i++) {
+			IParameter p = params.get(i);
+			lb[i] = p.getLowerLimit();
+			ub[i] = p.getUpperLimit();
+		}
+
+		return new SimpleBounds(lb, ub);
+	}
+
+	@Override
+	void internalOptimize() throws Exception {
+		MultivariateOptimizer opt = createOptimizer();
+		MultivariateFunction fn = createFunction();
+		ObjectiveFunction of = new ObjectiveFunction(fn);
+		InitialGuess ig = new InitialGuess(getParameterValues());
+		SimpleBounds bd = createBounds();
+		MaxEval me = new MaxEval(MAX_EVAL);
+		double min = Double.POSITIVE_INFINITY;
+		double res = Double.NaN;
+		double offset = 1e12;
+		double[] scale = new double[n];
+		for (int i = 0; i < n; i++) {
+			scale[i] = offset*0.25;
+		}
+		MultivariateFunctionPenaltyAdapter af;
+
+		try {
+			PointValuePair result;
+
+			switch (optimizer) {
+			case CONJUGATE_GRADIENT:
+				af = new MultivariateFunctionPenaltyAdapter(fn, bd.getLower(), bd.getUpper(), offset, scale);
+				result = opt.optimize(ig, GoalType.MINIMIZE, new ObjectiveFunction(af), me,
+						new ObjectiveFunctionGradient(createGradientFunction()));
+				break;
+			case BOBYQA:
+				result = opt.optimize(ig, GoalType.MINIMIZE, of, me, bd);
+				break;
+			case CMAES:
+				double[] sigma = new double[n];
+				for (int i = 0; i < n; i++) {
+					IParameter p = params.get(i);
+					double v = p.getValue();
+					double r = Math.max(p.getUpperLimit()-v, v - p.getLowerLimit());
+					sigma[i] = r*0.05; // 5% of range
+				}
+				int p = (int) Math.ceil(4 + Math.log(n)) + 1;
+				logger.trace("Population size: {}", p);
+				result = opt.optimize(ig, GoalType.MINIMIZE, of, me, bd,
+						new CMAESOptimizer.Sigma(sigma), new CMAESOptimizer.PopulationSize(p));
+				break;
+			case SIMPLEX_MD:
+			default:
+				af = new MultivariateFunctionPenaltyAdapter(fn, bd.getLower(), bd.getUpper(), offset, scale);
+				result = opt.optimize(ig, GoalType.MINIMIZE, new ObjectiveFunction(af), me,
+						new MultiDirectionalSimplex(n));
+				break;
+			case SIMPLEX_NM:
+				af = new MultivariateFunctionPenaltyAdapter(fn, bd.getLower(), bd.getUpper(), offset, scale);
+				result = opt.optimize(ig, GoalType.MINIMIZE, new ObjectiveFunction(af), me,
+						new NelderMeadSimplex(n));
+				break;
+			}
+
+			// logger.info("Q-space fit: rms = {}, x^2 = {}", opt.getRMS(), opt.getChiSquare());
+			double ires = calculateResidual(opt.getStartPoint());
+			logger.trace("Residual: {} from {}", result.getValue(), ires);
+			res = result.getValue();
+			if (res < min)
+				setParameterValues(result.getPoint());
+			logger.trace("Used {} evals and {} iters", opt.getEvaluations(), opt.getIterations());
+//			System.err.printf("Used %d evals and %d iters\n", opt.getEvaluations(), opt.getIterations());
+			// logger.info("Q-space fit: rms = {}, x^2 = {}", opt.getRMS(), opt.getChiSquare());
+		} catch (IllegalArgumentException e) {
+			logger.error("Start point has wrong dimension", e);
+			// should not happen!
+		} catch (TooManyEvaluationsException e) {
+			throw new IllegalArgumentException("Could not fit as optimizer did not converge");
+//				logger.error("Convergence problem: max iterations ({}) exceeded", opt.getMaxIterations());
+		}
+	}
+}
---
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/fitting/ApacheFittingTest.java
@@ -0,0 +1,102 @@
+/*-
+ * Copyright 2011 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.analysis.fitting;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import uk.ac.diamond.scisoft.analysis.optimize.ApacheOptimizer;
+import uk.ac.diamond.scisoft.analysis.optimize.ApacheOptimizer.Optimizer;
+import uk.ac.diamond.scisoft.analysis.optimize.IOptimizer;
+
+@RunWith(Parameterized.class)
+public class ApacheFittingTest extends AbstractFittingTestBase {
+
+	static final long SEED = 12357L;
+	private Optimizer o;
+
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] {
+				{Optimizer.SIMPLEX_MD},
+				{Optimizer.SIMPLEX_NM},
+//				{Optimizer.POWELL}, // bad fit
+				{Optimizer.BOBYQA},
+//				{Optimizer.CMAES},  // bad fit
+//				{Optimizer.CONJUGATE_GRADIENT}, // cannot initialize yet
+				};
+		return Arrays.asList(data);
+	}
+
+	public ApacheFittingTest(Optimizer optimizer) {
+		o = optimizer;
+	}
+
+	@Override
+	public IOptimizer createOptimizer() {
+		
+		ApacheOptimizer opt = new ApacheOptimizer(o);
+		opt.seed = SEED;
+		return opt;
+	}
+
+	@Test
+	public void testFWHMGaussian() {
+		checkClose("Gaussian fwhm", fwhm, fittedGaussian.get(0).getPeak(0).getFWHM(), delta);
+	}
+
+	@Test
+	public void testFWHMLorentzian() {
+		checkClose("Lorentzian fwhm", fwhm, fittedLorentzian.get(0).getPeak(0).getFWHM(), 4*delta);
+	}
+
+	@Test
+	public void testFWHMPearsonVII() {
+		checkClose("Pearson7 fwhm", fwhm, fittedPearsonVII.get(0).getPeak(0).getFWHM(), delta);
+	}
+
+	@Test
+	public void testFWHMPseudoVoigt() {
+		checkClose("PseudoVoigt fwhm", fwhm, fittedPseudoVoigt.get(0).getPeak(0).getFWHM(), delta);
+	}
+
+	@Test
+	public void testAreaGaussian() {
+		checkClose("Gaussian area", area, fittedGaussian.get(0).getPeak(0).getArea(), delta);
+	}
+
+	@Test
+	public void testAreaLorentzian() {
+		checkClose("Lorentzian area", area, fittedLorentzian.get(0).getPeak(0).getArea(), 6*delta);
+	}
+
+	@Test
+	public void testAreaPearsonVII() {
+		checkClose("Pearson7 area", area, fittedPearsonVII.get(0).getPeak(0).getArea(), 2*delta);
+	}
+
+	@Test
+	public void testAreaPseudoVoigt() {
+		checkClose("PseudoVoigt area", area, fittedPseudoVoigt.get(0).getPeak(0).getArea(), 5*delta);
+	}
+}

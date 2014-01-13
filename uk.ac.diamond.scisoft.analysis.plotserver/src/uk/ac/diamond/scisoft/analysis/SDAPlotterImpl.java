2a11d68612e11b939ffb89b986e2595a68da4a3a
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-01-13 15:59:45 +0000

    Delete unnecessary comment

--- uk.ac.diamond.scisoft.analysis.plotserver/src/uk/ac/diamond/scisoft/analysis/SDAPlotterImpl.java
+++ uk.ac.diamond.scisoft.analysis.plotserver/src/uk/ac/diamond/scisoft/analysis/SDAPlotterImpl.java
@@ -887,15 +887,11 @@ public class SDAPlotterImpl implements ISDAPlotter {
 
 	@Override
 	public void exportPlot(String viewName, String fileFormat, String saveFullPath) throws Exception {
-//		PlotService plotServer = getPlotService();
-//		if (plotServer != null) {
-			GuiBean guiBean = new GuiBean();
-			guiBean.put(GuiParameters.PLOTMODE, GuiPlotMode.EXPORT);
-			guiBean.put(GuiParameters.FILEFORMAT, fileFormat);
-			guiBean.put(GuiParameters.SAVEPATH, saveFullPath);
-			setGuiBean(viewName, guiBean);
-//			plotServer.updateGui(viewName, guiBean);
-//		}
+		GuiBean guiBean = new GuiBean();
+		guiBean.put(GuiParameters.PLOTMODE, GuiPlotMode.EXPORT);
+		guiBean.put(GuiParameters.FILEFORMAT, fileFormat);
+		guiBean.put(GuiParameters.SAVEPATH, saveFullPath);
+		setGuiBean(viewName, guiBean);
 	}
 
 	@Override

d2b709344ffbc864ab65856e9367ed21df41798c
Peter Chang <Peter.Chang@diamond.ac.uk>
2014-02-05 15:10:56 +0000

    Fixed bug for object and string datasets where getting its hash is broken

--- uk.ac.diamond.scisoft.analysis.dataset/src/uk/ac/diamond/scisoft/analysis/dataset/StringDataset.java
+++ uk.ac.diamond.scisoft.analysis.dataset/src/uk/ac/diamond/scisoft/analysis/dataset/StringDataset.java
@@ -95,6 +95,21 @@ public class StringDataset extends StringDatasetBase {
 	}
 
 	@Override
+	protected void calculateMaxMin(final boolean ignoreNaNs, final boolean ignoreInfs) {
+		// override to skip max/min calculation for hash only
+		IndexIterator iter = getIterator();
+		double hash = 0;
+
+		while (iter.hasNext()) {
+			final int val = getStringAbs(iter.index).hashCode();
+			hash = (hash * 19 + val) % Integer.MAX_VALUE;
+		}
+
+		int ihash = ((int) hash) * 19 + getDtype() * 17 + getElementsPerItem();
+		setStoredValue(storeName(ignoreNaNs, ignoreInfs, STORE_SHAPELESS_HASH), ihash);
+	}
+
+	@Override
 	public boolean getElementBooleanAbs(int index) {
 		throw new UnsupportedOperationException("Unsupported method for class");
 	}

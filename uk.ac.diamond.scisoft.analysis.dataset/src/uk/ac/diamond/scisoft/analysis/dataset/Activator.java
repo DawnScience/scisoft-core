20294e22d25f4c0ef06a51a1f9eb4279c41ae0db
Peter Chang <Peter.Chang@diamond.ac.uk>
2014-05-15 14:22:55 +0100

    Added override annotations

--- uk.ac.diamond.scisoft.analysis.dataset/src/uk/ac/diamond/scisoft/analysis/dataset/Activator.java
+++ uk.ac.diamond.scisoft.analysis.dataset/src/uk/ac/diamond/scisoft/analysis/dataset/Activator.java
@@ -18,6 +18,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 	}
@@ -26,6 +27,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}

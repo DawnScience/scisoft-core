6db0a1f1aaaa6fbfcf4de65959a442865dc0eddb
matthew.gerring <matthew.gerring@gmail.com>
2014-07-17 13:23:27 +0100

    Added package-info.

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * This package defines operations on rich datasets which are standard to the
+ * operations service.
+ * 
+ * The package is manually loaded into the tests for testing the operations.
+ * 
+ * @author fcp94556
+ */
+package uk.ac.diamond.scisoft.analysis.processing.operations;
+

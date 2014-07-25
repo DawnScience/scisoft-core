850b842ab9066d255f393ddb9ecde2be8cba0a9c
Mark Basham <mark.basham@diamond.ac.uk>
2014-07-25 09:58:29 +0100

    Simple implementation of an ROI Selection IOperation

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/SelectROI.java
@@ -0,0 +1,70 @@
+package uk.ac.diamond.scisoft.analysis.processing.operations;
+
+import java.io.Serializable;
+
+import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.function.MapToRotatedCartesian;
+import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
+import uk.ac.diamond.scisoft.analysis.processing.IOperation;
+import uk.ac.diamond.scisoft.analysis.processing.IRichDataset;
+import uk.ac.diamond.scisoft.analysis.processing.OperationData;
+import uk.ac.diamond.scisoft.analysis.processing.OperationException;
+import uk.ac.diamond.scisoft.analysis.processing.OperationRank;
+import uk.ac.diamond.scisoft.analysis.roi.RectangularROI;
+
+public class SelectROI implements IOperation {
+
+	private RectangularROI roi = null;
+	
+	public SelectROI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getOperationDescription() {
+		return "Passes on only the selected region of interest down the pipeline";
+	}
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.selectROI";
+	}
+
+	@Override
+	public void setDataset(IRichDataset... data)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public OperationData execute(OperationData slice, IMonitor monitor)
+			throws OperationException {
+		
+		// Get the data ROI
+		MapToRotatedCartesian map = new MapToRotatedCartesian(roi);
+		AbstractDataset dataRegion = map.value(slice.getData()).get(0);
+		
+		OperationData result = new OperationData(dataRegion);
+		
+		return result;
+	}
+
+	@Override
+	public void setParameters(Serializable... parameters)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO;
+	}
+
+}

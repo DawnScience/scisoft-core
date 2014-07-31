a2851797a58895277b2cc84fb4831f8865b729f6
matthew.gerring <matthew.gerring@gmail.com>
2014-07-31 16:25:16 +0100

    Added function model

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/FunctionModel.java
@@ -0,0 +1,43 @@
+package uk.ac.diamond.scisoft.analysis.processing.operations;
+
+import uk.ac.diamond.scisoft.analysis.fitting.functions.IFunction;
+import uk.ac.diamond.scisoft.analysis.processing.model.AbstractOperationModel;
+
+public class FunctionModel extends AbstractOperationModel {
+
+	private IFunction function;
+
+	public IFunction getFunction() {
+		return function;
+	}
+
+	public void setFunction(IFunction function) {
+		this.function = function;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((function == null) ? 0 : function.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FunctionModel other = (FunctionModel) obj;
+		if (function == null) {
+			if (other.function != null)
+				return false;
+		} else if (!function.equals(other.function))
+			return false;
+		return true;
+	}
+}

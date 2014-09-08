0f4dea2eb0e1cde9fa7fab3c62b4cd236c150bf9
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-09-08 13:59:46 +0100

    Change interface name of boofcv filter

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/DilateMaskOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/DilateMaskOperation.java
@@ -2,7 +2,7 @@ package uk.ac.diamond.scisoft.analysis.processing.operations;
 
 import java.util.List;
 
-import org.dawb.common.services.IImageProcessingService;
+import org.dawb.common.services.IImageFilterService;
 import org.dawb.common.services.ServiceManager;
 
 import uk.ac.diamond.scisoft.analysis.dataset.Comparisons;
@@ -41,10 +41,10 @@ public class DilateMaskOperation extends AbstractOperation {
 		
 		if (mask == null) throw new OperationException(this, "No mask to dilate!");
 		
-		IImageProcessingService service = null;
+		IImageFilterService service = null;
 		
 		try {
-			service = (IImageProcessingService)ServiceManager.getService(IImageProcessingService.class);
+			service = (IImageFilterService)ServiceManager.getService(IImageFilterService.class);
 		} catch (Exception e) {
 			throw new OperationException(this, "Could not get image processing service");
 		}

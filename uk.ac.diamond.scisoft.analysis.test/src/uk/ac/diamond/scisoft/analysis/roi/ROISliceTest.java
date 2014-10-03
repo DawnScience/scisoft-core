7914164481f1e70a28b0cd82e69a8b632ff68932
matthew.gerring <matthew.gerring@gmail.com>
2014-10-03 10:38:31 +0100

    Moved classes to somewhere with less dependencies.

--- uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/roi/ROISliceTest.java
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/roi/ROISliceTest.java
@@ -14,6 +14,7 @@ import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.analysis.dataset.roi.LinearROI;
+import org.eclipse.dawnsci.analysis.dataset.roi.ROISliceUtils;
 import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
 import org.junit.Assert;
 import org.junit.Test;

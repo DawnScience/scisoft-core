836f15615a470b70f443f2fcd785193a5b1020b5
Jacob Filik <jacob.filik@diamond.ac.uk>
2014-11-11 16:07:01 +0000

    Nullify image when parent lazy dataset changes

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractBlankFrameOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractBlankFrameOperation.java
@@ -5,21 +5,37 @@ import java.util.Arrays;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
 import org.eclipse.dawnsci.analysis.api.metadata.OriginMetadata;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.dataset.impl.AbstractDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
+import org.eclipse.dawnsci.analysis.dataset.impl.Maths;
 import org.eclipse.dawnsci.analysis.dataset.impl.PositionIterator;
 
 public class SubtractBlankFrameOperation extends AbstractImageSubtrationOperation<SubtractBlankFrameModel> {
-
-	Dataset background = null;
+	
+	private ILazyDataset parent = null;
 	
 	@Override
 	public String getId() {
 		return "uk.ac.diamond.scisoft.analysis.processing.operations.backgroundsubtraction.SubtractBlankFrameOperation";
 	}
 
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		
+		// check file is the same
+		OriginMetadata om = getOriginMetadata(input);
+		if (om == null) throw new OperationException(this, "No origin metadata!");
+		if (parent != om.getParent()) {
+			parent = om.getParent();
+			image = null;
+		}
+		
+		return super.process(input, monitor);
+	}
+	
 	@Override
 	protected Dataset getImage(IDataset input) throws OperationException {
 		OriginMetadata om = getOriginMetadata(input);

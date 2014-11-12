e8c3735d52d14042dfbbfc419e5f1fca2be2733f
Jacob Filik <jacob.filik@diamond.ac.uk>
2014-11-12 10:16:05 +0000

    Fix downsample to include axes and separate x and y

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/DownsampleImageModel.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/DownsampleImageModel.java
@@ -1,25 +1,36 @@
 package uk.ac.diamond.scisoft.analysis.processing.operations.twod;
 
 import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
 
 import uk.ac.diamond.scisoft.analysis.dataset.function.DownsampleMode;
 
 public class DownsampleImageModel extends AbstractOperationModel {
 
+	@OperationModelField(label = "Set downsample mode")
 	private DownsampleMode downsampleMode = DownsampleMode.MEAN;
-	private int downsampleSize = 2;
+	@OperationModelField(label = "Set downsample size X")
+	private int downsampleSizeX = 2;
+	@OperationModelField(label = "Set downsample size Y")
+	private int downsampleSizeY = 2;
 	public DownsampleMode getDownsampleMode() {
 		return downsampleMode;
 	}
 	public void setDownsampleMode(DownsampleMode downsampleMode) {
 		firePropertyChange("downsampleMode", this.downsampleMode, this.downsampleMode = downsampleMode);
 	}
-	public int getDownsampleSize() {
-		return downsampleSize;
+	public int getDownsampleSizeX() {
+		return downsampleSizeX;
 	}
-	public void setDownsampleSize(int downsampleSize) {
-		this.downsampleSize = downsampleSize;
-		firePropertyChange("downsampleSize", this.downsampleSize, this.downsampleSize = downsampleSize);
+	public void setDownsampleSizeX(int downsampleSizeX) {
+		firePropertyChange("downsampleSizeX", this.downsampleSizeX, this.downsampleSizeX = downsampleSizeX);
+	}
+	
+	public int getDownsampleSizeY() {
+		return downsampleSizeY;
+	}
+	public void setDownsampleSizeY(int downsampleSizeY) {
+		firePropertyChange("downsampleSizeY", this.downsampleSizeY, this.downsampleSizeY = downsampleSizeY);
 	}
 	
 	
--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/DownsampleImageOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/DownsampleImageOperation.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
+import org.eclipse.dawnsci.analysis.api.metadata.AxesMetadata;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.AbstractOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
@@ -13,6 +14,8 @@ import org.eclipse.dawnsci.analysis.dataset.impl.Comparisons;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 
 import uk.ac.diamond.scisoft.analysis.dataset.function.Downsample;
+import uk.ac.diamond.scisoft.analysis.dataset.function.DownsampleMode;
+import uk.ac.diamond.scisoft.analysis.metadata.AxesMetadataImpl;
 
 public class DownsampleImageOperation extends AbstractOperation<DownsampleImageModel, OperationData> {
 
@@ -30,11 +33,36 @@ public class DownsampleImageOperation extends AbstractOperation<DownsampleImageM
 			((Dataset)input).setByBoolean(Double.NaN, Comparisons.logicalNot(m));
 		}
 		
-		Downsample downsample = new Downsample(((DownsampleImageModel)model).getDownsampleMode(), new int[] {((DownsampleImageModel)model).getDownsampleSize(),((DownsampleImageModel)model).getDownsampleSize()});
+		int x = model.getDownsampleSizeX();
+		int y = model.getDownsampleSizeY();
 		
+		Downsample downsample = new Downsample(((DownsampleImageModel)model).getDownsampleMode(), y,x);
 		List<Dataset> out = downsample.value(input);
 		Dataset dataset = out.get(0);
 		dataset.setName("downsampled");
+		
+		ILazyDataset[] firstAxes = getFirstAxes(input);
+		
+		if (firstAxes != null && (firstAxes[0] != null || firstAxes[1] != null)) {
+			AxesMetadata axm = new AxesMetadataImpl(2);
+			
+			Downsample dsax = new Downsample(DownsampleMode.MEAN, y,x);
+			
+			if (firstAxes[0] != null)	{
+				List<Dataset> ax1 = dsax.value(firstAxes[0].getSlice());
+				ax1.get(0).setName(firstAxes[0].getName());
+				axm.setAxis(0, ax1.get(0));
+			}
+			
+			if (firstAxes[1] != null)	{
+				List<Dataset> ax2 = dsax.value(firstAxes[1].getSlice());
+				ax2.get(0).setName(firstAxes[1].getName());
+				axm.setAxis(1, ax2.get(0));
+			}
+			
+			dataset.setMetadata(axm);
+		}
+		
 		return new OperationData(dataset);
 	}
 

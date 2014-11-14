c0f87a9aec6603878dc41253512ac2d8d9840bcc
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-11-14 10:00:05 +0000

    Change name of configuration file

--- uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/utils/PtychoConstants.java
+++ uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/utils/PtychoConstants.java
@@ -3,7 +3,7 @@ package uk.ac.diamond.scisoft.ptychography.rcp.utils;
 public class PtychoConstants {
 
 	public static final String PTYCHOPLOTVIEWS = "uk.ac.diamond.scisoft.ptychography.rcp.plotView";
-	public static final String SPREADSHEET_FILE = "NEW-ptycho.csv";
+	public static final String SPREADSHEET_FILE = "ptypy_parameters_descriptions.csv";
 	public static final String RESOURCES = "resources";
 
 }

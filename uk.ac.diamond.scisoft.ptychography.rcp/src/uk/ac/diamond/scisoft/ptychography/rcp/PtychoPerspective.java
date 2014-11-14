0f1493308c4ebf787b418553a3c0b8f6c6e0a573
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-11-14 16:08:45 +0000

    Open ptycho editor in perpective creation

--- uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/PtychoPerspective.java
+++ uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/PtychoPerspective.java
@@ -1,21 +1,32 @@
 package uk.ac.diamond.scisoft.ptychography.rcp;
 
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.handlers.IHandlerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import uk.ac.diamond.scisoft.ptychography.rcp.handlers.CallPtychoEditor;
 import uk.ac.diamond.scisoft.ptychography.rcp.utils.PtychoConstants;
 
 public class PtychoPerspective implements IPerspectiveFactory {
 
+	private static final Logger logger = LoggerFactory.getLogger(PtychoPerspective.class);
 	public static final String ID = "uk.ac.diamond.scisoft.ptychography.rcp.perspective";
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 
 		String editorArea = layout.getEditorArea();
-		
 		IFolderLayout navigatorFolder = layout.createFolder("navigator-folder", IPageLayout.LEFT, 0.2f, editorArea);
 		navigatorFolder.addView("org.eclipse.ui.navigator.ProjectExplorer");
 		navigatorFolder.addView("uk.ac.diamond.sda.navigator.views.FileView");
@@ -36,5 +47,24 @@ public class PtychoPerspective implements IPerspectiveFactory {
 		IFolderLayout bottomLayout = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.6f, editorArea);
 		bottomLayout.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 
+		Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				IHandlerService handlerService = (IHandlerService) getSite().getService(IHandlerService.class);
+				try {
+					handlerService.executeCommand(CallPtychoEditor.COMMAND_ID, null);
+				} catch (ExecutionException | NotDefinedException | NotEnabledException
+						| NotHandledException e) {
+					logger.equals("Error opening Ptycho editor:" + e.getMessage());
+				}
+			}
+		});
+	}
+
+	private IWorkbenchPartSite getSite() {
+		IWorkbenchPartSite site = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage().getActiveEditor()
+				.getSite();
+		return site;
 	}
 }

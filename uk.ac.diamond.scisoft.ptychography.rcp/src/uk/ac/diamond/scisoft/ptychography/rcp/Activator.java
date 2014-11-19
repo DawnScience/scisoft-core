ca8945c56be2f8b40a1e8768bcb81eb3ea016c96
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-11-19 15:07:51 +0000

    *Fix Tree filter *Add toolbar actions (TODO link to RCP editor actions )

--- uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/Activator.java
+++ uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "uk.ac.diamond.scisoft.ptychography"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "uk.ac.diamond.scisoft.ptychography.rcp"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
--- uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/editors/PtychoTreeViewerEditor.java
+++ uk.ac.diamond.scisoft.ptychography.rcp/src/uk/ac/diamond/scisoft/ptychography/rcp/editors/PtychoTreeViewerEditor.java
@@ -3,6 +3,11 @@ package uk.ac.diamond.scisoft.ptychography.rcp.editors;
 import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -10,6 +15,7 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyEvent;
@@ -26,12 +32,15 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
+import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.part.EditorPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.diamond.scisoft.ptychography.rcp.Activator;
 import uk.ac.diamond.scisoft.ptychography.rcp.model.PtychoInput;
 import uk.ac.diamond.scisoft.ptychography.rcp.model.PtychoNode;
 import uk.ac.diamond.scisoft.ptychography.rcp.model.PtychoTreeUtils;
@@ -41,7 +50,7 @@ public class PtychoTreeViewerEditor extends EditorPart {
 
 	public final static String ID = "uk.ac.diamond.scisoft.ptychography.rcp.ptychoEditor";
 	private TreeViewer viewer;
-	private FilteredTree filteredTree;
+//	private FilteredTree filteredTree;
 	private static final Logger logger = LoggerFactory.getLogger(PtychoTreeViewerEditor.class);
 	private List<PtychoInput> levels;
 	private List<PtychoNode> tree;
@@ -54,22 +63,22 @@ public class PtychoTreeViewerEditor extends EditorPart {
 	private Text shortDocText;
 	private StyledText longDocStyledText;
 	private PtychoNode currentNode;
-	private boolean isDirtyFlag;
+	private boolean isDirtyFlag = false;
 	private Color white;
 	private Color gray;
 	private Color darkGray;
 	private Color black;
+	private FilteredTree filteredTree;
 	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
+		isDirtyFlag = false;
 
 	}
 
 	@Override
 	public void doSaveAs() {
-		// TODO Auto-generated method stub
-
+		isDirtyFlag = false;
 	}
 
 	@Override
@@ -139,22 +148,30 @@ public class PtychoTreeViewerEditor extends EditorPart {
 
 	@Override
 	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
-	public void createPartControl(Composite parent) {
+	public void createPartControl(final Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
 		container.setLayout(new GridLayout(1, false));
 		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
+		PatternFilter filter = new PatternFilter() {
+			@Override
+			protected boolean isLeafMatch(Viewer viewer, Object element) {
+				String name = ((PtychoNode) element).getData().getName();
+				if (name == null) {
+					return false;
+				}
+				return wordMatches(name);
+			}
+		};
 		this.filteredTree = new FilteredTree(
 				container,
 				SWT.FULL_SELECTION | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER,
-				new PatternFilter(),
+				filter,
 				true);
-				//"Enter search string to filter the tree.\nThis will match on name, value or units");
 		viewer = filteredTree.getViewer();
 		viewer.getControl().setLayoutData(
 				new GridData(SWT.FILL, SWT.FILL, true, true));
@@ -267,10 +284,12 @@ public class PtychoTreeViewerEditor extends EditorPart {
 		runButton.setToolTipText("Run ptychography process on cluster");
 		runButton.setLayoutData(new GridData(SWT.RIGHT, SWT.BOTTOM, false, false));
 
+		createActions();
 		getSite().setSelectionProvider(viewer);
 	}
 
 	private void updateAttributes(ModifyEvent event) {
+		isDirtyFlag = true;
 		if (currentNode == null)
 			return;
 		PtychoInput data = currentNode.getData();
@@ -298,6 +317,92 @@ public class PtychoTreeViewerEditor extends EditorPart {
 		}
 	}
 
+	private void createActions() {
+//		final IPreferenceStore store = Activator.getPlottingPreferenceStore();
+		IToolBarManager toolBarMan = ((IEditorSite) getSite()).getActionBars().getToolBarManager();
+		IMenuManager menuMan = ((IEditorSite) getSite()).getActionBars().getMenuManager();
+
+		final Action expandAll = new Action("Expand All", Activator.getImageDescriptor("icons/expand_all.png")) {
+			public void run() {
+				if (viewer != null) {
+					viewer.expandAll();
+				}
+			}
+		};
+		expandAll.setToolTipText("Expand All");
+
+		final Action collapseAll = new Action("Collapse All", Activator.getImageDescriptor("icons/collapse_all.png")) {
+			public void run() {
+				if (viewer != null) {
+					viewer.collapseAll();
+				}
+			}
+		};
+		collapseAll.setToolTipText("Collapse All");
+
+		final Action open = new Action("Open previously saved table", Activator.getImageDescriptor("icons/file_obj.gif")) {
+			public void run() {
+			}
+		};
+		open.setToolTipText("Open a previously saved configuration parameter file (CSV)");
+
+		final Action exportAs = new Action("Export table to file", Activator.getImageDescriptor("icons/export_wiz.gif")) {
+			public void run() {
+				try {
+				} catch (Exception e) {
+					logger.error("Problem exporting to file", e);
+				}
+			}
+		};
+
+		final Action save = new Action("Save table", Activator.getImageDescriptor("icons/save_edit.gif")) {
+			public void run() {
+				try {
+				} catch (Exception e) {
+					logger.error("Problem saving table", e);
+				}
+			}
+		};
+
+		final Action preferences = new Action("Preferences...") {
+			public void run() {
+				PreferenceDialog pref = PreferencesUtil
+						.createPreferenceDialogOn(PlatformUI.getWorkbench()
+								.getActiveWorkbenchWindow().getShell(),
+								PtychoTreeViewerEditor.ID, null, null);
+				if (pref != null)
+					pref.open();
+			}
+		};
+		preferences.setToolTipText("Open Region Editor preferences");
+
+		//toolbar buttons
+		toolBarMan.add(new Separator());
+		toolBarMan.add(expandAll);
+		toolBarMan.add(collapseAll);
+		toolBarMan.add(new Separator());
+		toolBarMan.add(open);
+		toolBarMan.add(exportAs);
+		toolBarMan.add(save);
+		toolBarMan.add(new Separator());
+
+		// right click menu buttons
+//		MenuManager rightClickMenuMan = new MenuManager();
+//		addRightClickMenuCheckActions(rightClickMenuMan);
+//		rightClickMenuMan.add(copy);
+//		rightClickMenuMan.add(show);
+//		rightClickMenuMan.add(clear);
+//		rightClickMenuMan.add(deleteMenuAction);
+//		viewer.getControl().setMenu(rightClickMenuMan.createContextMenu(viewer.getControl()));
+
+		// menu buttons
+		menuMan.add(open);
+		menuMan.add(exportAs);
+		menuMan.add(save);
+		menuMan.add(new Separator());
+		menuMan.add(preferences);
+	}
+
 	@Override
 	public void dispose() {
 		if (viewer != null)
@@ -318,4 +423,5 @@ public class PtychoTreeViewerEditor extends EditorPart {
 		if (viewer != null)
 			viewer.getControl().setFocus();
 	}
+
 }

887e3becb413d2e4b717534fc7c817a89a647d41
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2014-12-03 13:25:07 +0000

    Initial commit of Crop2D

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/Crop2DOperation.java
@@ -0,0 +1,46 @@
+/*-
+ * Copyright 2014 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations.twod;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.processing.AbstractOperation;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+
+public class Crop2DOperation extends AbstractOperation<Crop2DModel, OperationData> {
+	
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.twod.Crop2DOperation";
+	}
+	
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO;
+	}
+	
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO;
+	}
+	
+	protected OperationData process(IDataset input, IMonitor monitor) {
+		int[] xMinIndex;
+		int[] xMaxIndex;
+		int[] yMinIndex;
+		int[] yMaxIndex;
+		
+		Double xMinVal = model.getxMin();
+		Double xMaxVal = model.getxMax();
+		Double yMinVal = model.getyMin();
+		Double yMaxVal = model.getyMax();
+	}
+}

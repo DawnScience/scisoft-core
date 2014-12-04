e22a91bba2ca9139f47d5a2345ad8937f78cf4e4
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2014-12-04 14:58:50 +0000

    Functional 2D crop. Code needs a bit of pruning and problem if two indices are same.

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/Crop2DOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/twod/Crop2DOperation.java
@@ -39,23 +39,47 @@ public class Crop2DOperation extends AbstractOperation<Crop2DModel, OperationDat
 		int[] minIndices = new int[2];
 		int[] maxIndices = new int[2];
 		
-		Double xMinVal = model.getxMin();
-		Double xMaxVal = model.getxMax();
-		Double yMinVal = model.getyMin();
-		Double yMaxVal = model.getyMax();
+		Double[] minVal = new Double[]{model.getxMin(), model.getyMin()};
+		Double[] maxVal = new Double[]{model.getxMax(), model.getyMax()};
 		
 		//Get axes data from dataset and see if X & Y axes exist
 		ILazyDataset[] axes = getFirstAxes(input);
 		
-		ILazyDataset theXAxis = axes[0];
-		ILazyDataset theYAxis = axes[1];
-		
-		minIndices[0] = DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, xMinVal);
-		maxIndices[0] = DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, xMaxVal);
-		
-		minIndices[1] = DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, yMinVal);
-		maxIndices[1] = DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, yMaxVal);
-		
+		if ((axes == null) || ((axes[0] == null ) && (axes[1] == null))) { //Check whether axes == null could occur
+			// There are no axis metadata. Given values are crop values.
+			minIndices[0] = minVal[0] == null ? 0 : (int) minVal[0].doubleValue();
+			maxIndices[0] = maxVal[0] == null ? 0 : (int) maxVal[0].doubleValue();
+			minIndices[1] = minVal[1] == null ? 0 : (int) minVal[1].doubleValue();
+			maxIndices[1] = maxVal[1] == null ? 0 : (int) maxVal[1].doubleValue();
+		} else if (axes[0] == null) {
+			//If there is no X-axis metadata, given values are crop values...
+			minIndices[0] = (int) minVal[0].doubleValue();
+			maxIndices[0] = (int) maxVal[0].doubleValue();
+			
+			//... and get Dataset indices of non-null crop values for the Y-axis 
+			ILazyDataset theYAxis = axes[1];
+			minIndices[1] = minVal[1] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, minVal[1]);
+			maxIndices[1] = maxVal[1] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, maxVal[1]);
+		} else if (axes[1] == null) {
+			//Get Dataset indices of non-null crop values for X-axis...
+			ILazyDataset theXAxis = axes[0];
+			minIndices[0] = minVal[0] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, minVal[0]);
+			maxIndices[0] = maxVal[0] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, maxVal[0]);
+			
+			//... and get set given value to Y-crop values
+			minIndices[1] = (int) minVal[0].doubleValue();
+			maxIndices[1] = (int) maxVal[0].doubleValue();
+		} else {
+			//Axes are non-null...
+			ILazyDataset theXAxis = axes[0];
+			ILazyDataset theYAxis = axes[1];
+			
+			// ...get indices of non-null crop values for X- and Y-axes
+			minIndices[0] = minVal[0] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, minVal[0]);
+			maxIndices[0] = maxVal[0] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theXAxis, maxVal[0]);
+			minIndices[1] = minVal[1] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, minVal[1]);
+			maxIndices[1] = maxVal[1] == null ? 0 : DatasetUtils.findIndexGreaterThanOrEqualTo((Dataset) theYAxis, maxVal[1]);
+		}
 		return new OperationData(input.getSlice(minIndices, maxIndices, null));
 		
 	}

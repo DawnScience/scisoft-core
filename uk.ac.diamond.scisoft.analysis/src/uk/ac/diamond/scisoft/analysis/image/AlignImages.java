d13aaee84d3c69a88a4e750367e40945caf42306
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2015-01-30 17:07:06 +0000

    Refactoring: move align calculation with shifts to AlignImages

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/image/AlignImages.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/image/AlignImages.java
@@ -14,9 +14,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Image;
 import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
+import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROIList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -92,5 +94,97 @@ public class AlignImages {
 		}
 
 		return align(images, shifted, roi, fromStart, preShift);
+	}
+
+	/**
+	 * 
+	 * @param data
+	 *            original list of dataset to be aligned
+	 * @param shifts
+	 *            output where to put resulting shifts
+	 * @param roi
+	 *            rectangular ROI used for the alignment
+	 * @param mode
+	 *            number of columns used: can be 2 or 4
+	 * @param monitor
+	 * @return aligned list of dataset
+	 */
+	public static List<IDataset> alignWithROI(List<IDataset> data, List<List<double[]>> shifts, RectangularROI roi, int mode, IMonitor monitor) {
+		int nsets = data.size() / mode;
+
+		if (roi == null)
+			return null;
+		RectangularROIList rois = new RectangularROIList();
+		rois.add(roi);
+
+		if (monitor != null)
+			monitor.worked(1);
+
+		if (shifts == null)
+			shifts = new ArrayList<List<double[]>>();
+		List<IDataset> shiftedImages = new ArrayList<IDataset>();
+
+		int index = 0;
+		int nr = rois.size();
+		if (nr > 0) {
+			if (nr < mode) { // clean up roi list
+				if (mode == 2) {
+					rois.add(rois.get(0));
+				} else {
+					switch (nr) {
+					case 1:
+						rois.add(rois.get(0));
+						rois.add(rois.get(0));
+						rois.add(rois.get(0));
+						break;
+					case 2:
+					case 3:
+						rois.add(2, rois.get(0));
+						rois.add(3, rois.get(1));
+						break;
+					}
+				}
+			}
+
+			IDataset[] tImages = new IDataset[nsets];
+			List<IDataset> shifted = new ArrayList<IDataset>(nsets);
+			boolean fromStart = false;
+			// align first images across columns:
+			// Example: [0,1,2]-[3,4,5]-[6,7,8]-[9,10,11] for 12 images on 4 columns
+			// with images 0,3,6,9 as the top images of each column.
+			List<double[]> topShifts = new ArrayList<double[]>();
+			IDataset[] topImages = new IDataset[mode];
+			List<IDataset> anchorList = new ArrayList<IDataset>();
+			for (int i = 0; i < mode; i++) {
+				topImages[i] = data.get(i * nsets);
+			}
+			// align top images
+			topShifts = align(topImages, anchorList, rois.get(0), true, null);
+
+			for (int p = 0; p < mode; p++) {
+				for (int i = 0; i < nsets; i++) {
+					tImages[i] = data.get(index++);
+				}
+				IDataset anchor = anchorList.get(p);
+				shifted.clear();
+				try {
+					// align rest of images
+					shifts.add(AlignImages.align(tImages, shifted, rois.get(p), true, topShifts.get(p)));
+					shifted.remove(0); // remove unshifted anchor
+					shiftedImages.add(anchor); // add shifted anchor
+					shiftedImages.addAll(shifted); // add aligned images
+				} catch (Exception e) {
+					logger.warn("Problem with alignment: " + e);
+					return null;
+				}
+
+				fromStart = !fromStart;
+				if (monitor != null)
+					monitor.worked(1);
+			}
+		}
+		if (monitor != null)
+			monitor.worked(1);
+		return shiftedImages;
 	}
 }

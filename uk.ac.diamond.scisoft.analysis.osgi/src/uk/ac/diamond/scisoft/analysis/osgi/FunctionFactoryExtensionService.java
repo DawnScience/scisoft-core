7700609cbe9aa500ef0854f12d58e3c7465b064e
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-02-04 16:09:35 +0000

    Adds FunctionFactoryExtensionService class, loading u.a.d.s.a.fitting.function extension point

---
+++ uk.ac.diamond.scisoft.analysis.osgi/src/uk/ac/diamond/scisoft/analysis/osgi/FunctionFactoryExtensionService.java
@@ -0,0 +1,60 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.osgi;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.dawnsci.analysis.api.fitting.functions.IFunction;
+import org.eclipse.dawnsci.analysis.api.fitting.functions.IFunctionFactoryExtensionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.analysis.fitting.functions.FunctionFactory;
+
+public class FunctionFactoryExtensionService implements IFunctionFactoryExtensionService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FunctionFactoryExtensionService.class);
+	
+	//Added the following two members following LoaderFactoryExt.Serv.
+	static {
+		System.out.println("Starting function factory extension service");
+	}
+	public FunctionFactoryExtensionService() {
+		// Important do nothing here, OSGI may start the service more than once.
+	}
+
+	/**
+	 * Get all extension points of type u.a.d.s.a.fitting.function
+	 * and register them sequentially.
+	 */
+	@Override
+	public void registerExtensionPoints() {
+		final IConfigurationElement[] extPts = Platform.getExtensionRegistry().getConfigurationElementsFor("uk.ac.diamond.scisoft.analysis.fitting.function");
+		for (IConfigurationElement eP : extPts) {
+			registerFunction(eP);
+		}
+	}
+	
+	/**
+	 * Take an extension point, determine it's class and then register it with
+	 * the function factory.
+	 * @param extPt
+	 */
+	private final static void registerFunction(IConfigurationElement extPt) {
+		try {
+			final IFunction function = (IFunction) extPt.createExecutableExtension("class");
+			Class<? extends IFunction> clazz = function.getClass();
+			FunctionFactory.registerFunction(clazz);
+		} catch (Exception e) {
+			logger.error("Cannot import function "+extPt.getAttribute("class"), e);
+		}
+	}
+
+}

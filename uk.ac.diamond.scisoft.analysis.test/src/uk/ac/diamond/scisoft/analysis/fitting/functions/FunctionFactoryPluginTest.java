c8e3d3f0d7d51c89022ccca6a99ea2f53318f2f3
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-02-14 11:42:24 +0000

    Removes unused imports.

--- uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/functions/FunctionFactoryPluginTest.java
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/functions/FunctionFactoryPluginTest.java
@@ -13,7 +13,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;

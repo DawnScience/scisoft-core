76775a119c24bf73ff7be3a95ac3105c3e4b9a15
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-02-19 19:37:36 +0000

    Now makes use of the XAxis conversion strategy.

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/diffraction/powder/PowderDataUtils.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/diffraction/powder/PowderDataUtils.java
@@ -14,7 +14,7 @@ import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.diamond.scisoft.analysis.roi.ROIProfile.XAxis;
+import uk.ac.diamond.scisoft.analysis.roi.XAxis;
 
 public class PowderDataUtils {
 	
@@ -33,23 +33,23 @@ public class PowderDataUtils {
 		
 		Dataset result = DatasetFactory.zeros(shape, Dataset.FLOAT64);
 		
-		//FIXME Change the names given to the axis labels to use the ones in the enum.
+		//TODO FIXME Change the names given to the axis labels to use the ones in the enum.
 		if (outAxisType.equals(XAxis.ANGLE)) {
 			for (int i = 0; i < axisData.getSize(); i++) {
 				Double xAxisVal = axisData.getDouble(i);
-				result.set(inAxisType.toANGLE(xAxisVal, lambda), i);
+				result.set(inAxisType.convertToANGLE(xAxisVal, lambda), i);
 			}
 			result.setName("2theta");
 		} else if (outAxisType.equals(XAxis.Q)) {
 			for (int i = 0; i < axisData.getSize(); i++) {
 				Double xAxisVal = axisData.getDouble(i);
-				result.set(inAxisType.toQ(xAxisVal, lambda), i);
+				result.set(inAxisType.convertToQ(xAxisVal, lambda), i);
 			}
 			result.setName("Q");
 		} else if (outAxisType.equals(XAxis.RESOLUTION)) {
 			for (int i = 0; i < axisData.getSize(); i++) {
 				Double xAxisVal = axisData.getDouble(i);
-				result.set(inAxisType.toRESOLUTION(xAxisVal, lambda), i);
+				result.set(inAxisType.convertToRESOLUTION(xAxisVal, lambda), i);
 			}
 			result.setName("d-spacing");
 		} else {

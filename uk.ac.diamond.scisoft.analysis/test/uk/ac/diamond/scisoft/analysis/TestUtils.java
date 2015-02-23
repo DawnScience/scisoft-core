fd1602fae56cbd3a3c0e1ad92e1ed8f70bf6865c
Peter Chang <peter.chang@diamond.ac.uk>
2015-02-23 16:48:04 +0000

    Refactor to expose assert method with relative and absolute tolerances

--- uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/TestUtils.java
+++ uk.ac.diamond.scisoft.analysis/test/uk/ac/diamond/scisoft/analysis/TestUtils.java
@@ -228,12 +228,33 @@ public class TestUtils {
 
 		while (at.hasNext() && bt.hasNext()) {
 			for (int j = 0; j < is; j++) {
-				double bv = calc.getElementDoubleAbs(at.index + j);
-				double av = expected.getElementDoubleAbs(bt.index + j);
-
-				Assert.assertEquals("Value does not match at " + Arrays.toString(at.getPos()) + "; " + j +
-						": ", av, bv, absTolerance + relTolerance*Math.abs(av));
+				assertEquals("Value does not match at " + Arrays.toString(at.getPos()) + "; " + j +
+						": ", expected.getElementDoubleAbs(bt.index + j), calc.getElementDoubleAbs(at.index + j),
+						relTolerance, absTolerance);
 			}
 		}
+	}
+
+	/**
+	 * Assert equality if abs(e - a) <= max(1e-20, 1e-14*max(abs(e), abs(a)))
+	 * @param s message for assert exception
+	 * @param e expected value
+	 * @param a actual value
+	 */
+	public static void assertEquals(String s, double e, double a) {
+		assertEquals(s, e, a, 1e-14, 1e-20);
+	}
+
+	/**
+	 * Assert equality if abs(e - a) <= max(absTol, relTol*max(abs(e), abs(a)))
+	 * @param s message for assert exception
+	 * @param e expected value
+	 * @param a actual value
+	 * @param relTol relative tolerance
+	 * @param absTol absolute tolerance
+	 */
+	public static void assertEquals(String s, double e, double a, double relTol, double absTol) {
+		double t = Math.max(absTol, relTol*Math.max(Math.abs(e), Math.abs(a)));
+		Assert.assertEquals(s, e, a, t);
 	}
 }

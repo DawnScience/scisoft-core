976bead1f65a8ad7b084c01922972f475afb5bc0
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-03-12 16:23:05 +0000

    Fix model hierarchy

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/externaldata/SubtractDataOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/externaldata/SubtractDataOperation.java
@@ -9,29 +9,12 @@
 
 package uk.ac.diamond.scisoft.analysis.processing.operations.externaldata;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.Arrays;
 
-import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
-import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
-import org.eclipse.dawnsci.analysis.api.processing.OperationData;
-import org.eclipse.dawnsci.analysis.api.processing.OperationException;
-import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
-import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
-import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
-import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
-import org.eclipse.dawnsci.analysis.dataset.impl.IndexIterator;
-import org.eclipse.dawnsci.analysis.dataset.impl.LazyMaths;
-import org.eclipse.dawnsci.analysis.dataset.impl.Maths;
-import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
-
 import uk.ac.diamond.scisoft.analysis.processing.operations.utils.ProcessingUtils;
 
-public class SubtractDataOperation extends SubtractCurrentDataOperation<ExternalDataModel> {
+public class SubtractDataOperation extends SubtractCurrentDataOperation<ExternalDataSelectedFramesModel> {
 	
 	@Override
 	public String getId() {
@@ -39,7 +22,7 @@ public class SubtractDataOperation extends SubtractCurrentDataOperation<External
 	}
 	
 	protected ILazyDataset getLazyDataset(SliceFromSeriesMetadata ssm){
-		return ProcessingUtils.getLazyDataset(this, ((ExternalDataModel)model).getFilePath(), ((ExternalDataModel)model).getDatasetName());
+		return ProcessingUtils.getLazyDataset(this, ((ExternalDataSelectedFramesModel)model).getFilePath(), ((ExternalDataSelectedFramesModel)model).getDatasetName());
 	}
 
 	protected boolean subtractOneToOneIfMatch(){

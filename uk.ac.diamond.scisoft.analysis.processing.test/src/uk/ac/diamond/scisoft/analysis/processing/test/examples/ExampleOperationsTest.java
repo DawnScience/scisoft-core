326059189b9a22e315daa3b373f755fda42e2a99
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-03-12 11:38:38 +0000

    Remove test anti pattern

--- uk.ac.diamond.scisoft.analysis.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/test/examples/ExampleOperationsTest.java
+++ uk.ac.diamond.scisoft.analysis.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/test/examples/ExampleOperationsTest.java
@@ -63,232 +63,217 @@ public class ExampleOperationsTest {
 	
 	
 	@Test
-	public void sumExample() {
+	public void sumExample() throws Exception{
 		
 		String path = ExampleDataUtils.createExampleDataFile("myfilesum", new int[] {10, 100, 200});
 		
 		if (path == null) fail("Could not create data file");
-		
-		try {
-			IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
-			IConversionContext context = service.open(path);
-			
-			context.setConversionScheme(ConversionScheme.PROCESS);
-			context.setDatasetName("/entry1/data/data");
-			context.addSliceDimension(0, "all");
-			Map<Integer, String> axesNames = new HashMap<Integer,String>();
-			axesNames.put(1, "/entry1/data/axis0");
-			axesNames.put(2, "/entry1/data/axis1");
-			axesNames.put(3, "/entry1/data/axis2");
-			context.setAxesNames(axesNames);
-			
-			File f = new File(path);
-			String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
-			File o = new File(outpath);
-			o.mkdirs();
-			o.deleteOnExit();
-			
-			context.setOutputPath(outpath);
-			context.setUserObject(new IProcessingConversionInfo() {
-				
-				@Override
-				public IOperation[] getOperationSeries() {
-					ExampleSumOperation sumop = new ExampleSumOperation();
-					sumop.setModel(new EmptyModel());
-					return new IOperation[]{sumop};
-				}
-				
-				@Override
-				public IExecutionVisitor getExecutionVisitor(String fileName) {
-					return new HierarchicalFileExecutionVisitor(fileName);
-				}
 
-				@Override
-				public ProcessingOutputType getProcessingOutputType() {
-					return ProcessingOutputType.PROCESSING_ONLY;
-				}
-			});
-			
-			service.process(context);
-			
-			File f1 = new File(outpath);
-			File[] matchingFiles = f1.listFiles(new FilenameFilter() {
-			    public boolean accept(File dir, String name) {
-			        return name.startsWith("myfilesum") && name.endsWith("nxs");
-			    }
-			});
-			
-			File result = matchingFiles[0];
-			result.deleteOnExit();
-			
-			IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
-			
-			IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
-			double val = d1.getDouble(0);
-			assertEquals(100, val, 0);
-			
-			IDataset a1 = dh.getLazyDataset("/entry/result/axis1").getSlice();
-			val = a1.getDouble(1);
-			assertEquals(1, val, 0);
-			
-			IDataset a2 = dh.getLazyDataset("/entry/result/axis2").getSlice();
-			val = a2.getDouble(1);
-			assertEquals(2, val, 0);
-			
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+		IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
+		IConversionContext context = service.open(path);
+
+		context.setConversionScheme(ConversionScheme.PROCESS);
+		context.setDatasetName("/entry1/data/data");
+		context.addSliceDimension(0, "all");
+		Map<Integer, String> axesNames = new HashMap<Integer,String>();
+		axesNames.put(1, "/entry1/data/axis0");
+		axesNames.put(2, "/entry1/data/axis1");
+		axesNames.put(3, "/entry1/data/axis2");
+		context.setAxesNames(axesNames);
+
+		File f = new File(path);
+		String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
+		File o = new File(outpath);
+		o.mkdirs();
+		o.deleteOnExit();
+
+		context.setOutputPath(outpath);
+		context.setUserObject(new IProcessingConversionInfo() {
+
+			@Override
+			public IOperation[] getOperationSeries() {
+				ExampleSumOperation sumop = new ExampleSumOperation();
+				sumop.setModel(new EmptyModel());
+				return new IOperation[]{sumop};
+			}
+
+			@Override
+			public IExecutionVisitor getExecutionVisitor(String fileName) {
+				return new HierarchicalFileExecutionVisitor(fileName);
+			}
+
+			@Override
+			public ProcessingOutputType getProcessingOutputType() {
+				return ProcessingOutputType.PROCESSING_ONLY;
+			}
+		});
+
+		service.process(context);
+
+		File f1 = new File(outpath);
+		File[] matchingFiles = f1.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("myfilesum") && name.endsWith("nxs");
+			}
+		});
+
+		File result = matchingFiles[0];
+		result.deleteOnExit();
+
+		IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
+
+		IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
+		double val = d1.getDouble(0);
+		assertEquals(100, val, 0);
+
+		IDataset a1 = dh.getLazyDataset("/entry/result/axis1").getSlice();
+		val = a1.getDouble(1);
+		assertEquals(1, val, 0);
+
+		IDataset a2 = dh.getLazyDataset("/entry/result/axis2").getSlice();
+		val = a2.getDouble(1);
+		assertEquals(2, val, 0);
+
 		
 	}
 	
 	@Test
-	public void subtractionExample() {
+	public void subtractionExample() throws Exception {
 		
 		String path = ExampleDataUtils.createExampleDataFile("myfilesub", new int[] {2, 5, 100, 200});
 		final String pathsub = ExampleDataUtils.createExampleDataFile("myfile", new int[] {3, 100, 200});
 		
 		if (path == null) fail("Could not create data file");
 		
-		try {
-			IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
-			IConversionContext context = service.open(path);
-			
-			context.setConversionScheme(ConversionScheme.PROCESS);
-			context.setDatasetName("/entry1/data/data");
-			context.addSliceDimension(0, "all");
-			context.addSliceDimension(1, "all");
-			Map<Integer, String> axesNames = new HashMap<Integer,String>();
-			axesNames.put(1, "/entry1/data/axis0");
-			axesNames.put(2, "/entry1/data/axis1");
-			axesNames.put(3, "/entry1/data/axis2");
-			axesNames.put(4, "/entry1/data/axis3");
-			context.setAxesNames(axesNames);
-			
-			File f = new File(path);
-			String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
-			File o = new File(outpath);
-			o.mkdirs();
-			o.deleteOnExit();
-			
-			context.setOutputPath(outpath);
-			context.setUserObject(new IProcessingConversionInfo() {
-				
-				@Override
-				public IOperation[] getOperationSeries() {
-					ExampleExternalDataSubtractionOperation subop = new ExampleExternalDataSubtractionOperation();
-					ExampleExternalDataModel mod = new ExampleExternalDataModel();
-					mod.setFilePath(pathsub);
-					mod.setDatasetName("/entry1/data/data");
-					subop.setModel(mod);
-					return new IOperation[]{subop};
-				}
-				
-				@Override
-				public IExecutionVisitor getExecutionVisitor(String fileName) {
-					return new HierarchicalFileExecutionVisitor(fileName);
-				}
+		IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
+		IConversionContext context = service.open(path);
 
-				@Override
-				public ProcessingOutputType getProcessingOutputType() {
-					return ProcessingOutputType.PROCESSING_ONLY;
-				}
-			});
-			
-			service.process(context);
-			
-			File f1 = new File(outpath);
-			File[] matchingFiles = f1.listFiles(new FilenameFilter() {
-			    public boolean accept(File dir, String name) {
-			        return name.startsWith("myfilesub") && name.endsWith("nxs");
-			    }
-			});
-			
-			File result = matchingFiles[0];
-			result.deleteOnExit();
-			IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
-			
-			IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
-			double val = d1.getDouble(0);
-			assertEquals(0, val, 0);
-			
-			
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+		context.setConversionScheme(ConversionScheme.PROCESS);
+		context.setDatasetName("/entry1/data/data");
+		context.addSliceDimension(0, "all");
+		context.addSliceDimension(1, "all");
+		Map<Integer, String> axesNames = new HashMap<Integer,String>();
+		axesNames.put(1, "/entry1/data/axis0");
+		axesNames.put(2, "/entry1/data/axis1");
+		axesNames.put(3, "/entry1/data/axis2");
+		axesNames.put(4, "/entry1/data/axis3");
+		context.setAxesNames(axesNames);
+
+		File f = new File(path);
+		String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
+		File o = new File(outpath);
+		o.mkdirs();
+		o.deleteOnExit();
+
+		context.setOutputPath(outpath);
+		context.setUserObject(new IProcessingConversionInfo() {
+
+			@Override
+			public IOperation[] getOperationSeries() {
+				ExampleExternalDataSubtractionOperation subop = new ExampleExternalDataSubtractionOperation();
+				ExampleExternalDataModel mod = new ExampleExternalDataModel();
+				mod.setFilePath(pathsub);
+				mod.setDatasetName("/entry1/data/data");
+				subop.setModel(mod);
+				return new IOperation[]{subop};
+			}
+
+			@Override
+			public IExecutionVisitor getExecutionVisitor(String fileName) {
+				return new HierarchicalFileExecutionVisitor(fileName);
+			}
+
+			@Override
+			public ProcessingOutputType getProcessingOutputType() {
+				return ProcessingOutputType.PROCESSING_ONLY;
+			}
+		});
+
+		service.process(context);
+
+		File f1 = new File(outpath);
+		File[] matchingFiles = f1.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("myfilesub") && name.endsWith("nxs");
+			}
+		});
+
+		File result = matchingFiles[0];
+		result.deleteOnExit();
+		IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
+
+		IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
+		double val = d1.getDouble(0);
+		assertEquals(0, val, 0);
 		
 	}
 	
 	@Test
-	public void normExample() {
+	public void normExample() throws Exception {
 		
 		String path = ExampleDataUtils.createExampleDataFile("myfilenorm", new int[] {10, 100, 200});
 		final String pathn = ExampleDataUtils.createExampleDataFile("myfile", new int[] {10});
-		
-		if (path == null) fail("Could not create data file");
-		
-		try {
-			IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
-			IConversionContext context = service.open(path);
-			
-			context.setConversionScheme(ConversionScheme.PROCESS);
-			context.setDatasetName("/entry1/data/data");
-			context.addSliceDimension(0, "all");
-			Map<Integer, String> axesNames = new HashMap<Integer,String>();
-			axesNames.put(1, "/entry1/data/axis0");
-			axesNames.put(2, "/entry1/data/axis1");
-			axesNames.put(3, "/entry1/data/axis2");
-			context.setAxesNames(axesNames);
-			
-			File f = new File(path);
-			String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
-			File o = new File(outpath);
-			o.mkdirs();
-			o.deleteOnExit();
-			
-			context.setOutputPath(outpath);
-			context.setUserObject(new IProcessingConversionInfo() {
-				
-				@Override
-				public IOperation[] getOperationSeries() {
-					ExampleExternalDataNormalisationOperation subop = new ExampleExternalDataNormalisationOperation();
-					ExampleExternalDataModel mod = new ExampleExternalDataModel();
-					mod.setFilePath(pathn);
-					mod.setDatasetName("/entry1/data/data");
-					subop.setModel(mod);
-					return new IOperation[]{subop};
-				}
-				
-				@Override
-				public IExecutionVisitor getExecutionVisitor(String fileName) {
-					return new HierarchicalFileExecutionVisitor(fileName);
-				}
 
-				@Override
-				public ProcessingOutputType getProcessingOutputType() {
-					return ProcessingOutputType.PROCESSING_ONLY;
-				}
-			});
-			
-			service.process(context);
-			
-			File f1 = new File(outpath);
-			File[] matchingFiles = f1.listFiles(new FilenameFilter() {
-			    public boolean accept(File dir, String name) {
-			        return name.startsWith("myfilenorm") && name.endsWith("nxs");
-			    }
-			});
-			
-			File result = matchingFiles[0];
-			result.deleteOnExit();
-			IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
-			
-			IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
-			double val = d1.getDouble(0);
-			assertEquals(1, val, 0);
-			
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+		if (path == null) fail("Could not create data file");
+
+		IConversionService service = (IConversionService)ServiceManager.getService(IConversionService.class);
+		IConversionContext context = service.open(path);
+
+		context.setConversionScheme(ConversionScheme.PROCESS);
+		context.setDatasetName("/entry1/data/data");
+		context.addSliceDimension(0, "all");
+		Map<Integer, String> axesNames = new HashMap<Integer,String>();
+		axesNames.put(1, "/entry1/data/axis0");
+		axesNames.put(2, "/entry1/data/axis1");
+		axesNames.put(3, "/entry1/data/axis2");
+		context.setAxesNames(axesNames);
+
+		File f = new File(path);
+		String outpath = f.getParentFile().getAbsolutePath() + File.separator + "output" + File.separator;
+		File o = new File(outpath);
+		o.mkdirs();
+		o.deleteOnExit();
+
+		context.setOutputPath(outpath);
+		context.setUserObject(new IProcessingConversionInfo() {
+
+			@Override
+			public IOperation[] getOperationSeries() {
+				ExampleExternalDataNormalisationOperation subop = new ExampleExternalDataNormalisationOperation();
+				ExampleExternalDataModel mod = new ExampleExternalDataModel();
+				mod.setFilePath(pathn);
+				mod.setDatasetName("/entry1/data/data");
+				subop.setModel(mod);
+				return new IOperation[]{subop};
+			}
+
+			@Override
+			public IExecutionVisitor getExecutionVisitor(String fileName) {
+				return new HierarchicalFileExecutionVisitor(fileName);
+			}
+
+			@Override
+			public ProcessingOutputType getProcessingOutputType() {
+				return ProcessingOutputType.PROCESSING_ONLY;
+			}
+		});
+
+		service.process(context);
+
+		File f1 = new File(outpath);
+		File[] matchingFiles = f1.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith("myfilenorm") && name.endsWith("nxs");
+			}
+		});
+
+		File result = matchingFiles[0];
+		result.deleteOnExit();
+		IDataHolder dh = LoaderFactory.getData(result.getAbsolutePath());
+
+		IDataset d1 = dh.getLazyDataset("/entry/result/data").getSlice();
+		double val = d1.getDouble(0);
+		assertEquals(1, val, 0);
 		
 	}
 

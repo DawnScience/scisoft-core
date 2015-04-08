035c09a8d73f8a686570cbb7205ffaf766b74cf1
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-04-08 18:20:58 +0100

    Initial commit.

---
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/peakfinding/PeakyData.java
@@ -0,0 +1,60 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.peakfinding;
+
+import org.eclipse.dawnsci.analysis.api.fitting.functions.IFunction;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Add;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Gaussian;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Lorentzian;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Offset;
+
+public class PeakyData {
+	
+	private Double xAxisMax = 50.;
+	private DoubleDataset xAxisRange = (DoubleDataset) DatasetFactory.createRange(0, xAxisMax, 0.01, Dataset.FLOAT64);
+	
+	private Add makeGauLorPeaks(IFunction bkg) {
+		Add peaks = new Add();
+		
+		Gaussian g1 = new Gaussian(0.1080*xAxisMax, 0.0200*xAxisMax, 50.);
+		Gaussian g2 = new Gaussian(0.4934*xAxisMax, 0.0520*xAxisMax, 47.);
+		Gaussian g3 = new Gaussian(0.7780*xAxisMax, 0.0264*xAxisMax, 8.51);
+		
+		Lorentzian l1 = new Lorentzian(0.1840*xAxisMax, 0.0178*xAxisMax, 32.58);
+		Lorentzian l2 = new Lorentzian(0.5716*xAxisMax, 0.0364*xAxisMax, 25.58);
+		Lorentzian l3 = new Lorentzian(0.9504*xAxisMax, 0.0104*xAxisMax, 6.57);
+		
+		peaks.addFunction(g1);
+		peaks.addFunction(g2);
+		peaks.addFunction(g3);
+		peaks.addFunction(l1);
+		peaks.addFunction(l2);
+		peaks.addFunction(l3);
+		peaks.addFunction(bkg);
+				
+		return peaks;
+	}
+	
+	public Dataset makeOffsetGauLorPeakyData() {
+		Offset offsetBkg = new Offset();
+		offsetBkg.setParameterValues(5.7);
+		
+		Add profileFn = makeGauLorPeaks(offsetBkg);
+		
+		DoubleDataset profile = profileFn.calculateValues(xAxisRange);
+		
+		return profile;
+	}
+
+}

4488f7dea5c6c41fe59db637f3b17108fbf797fb
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-04-20 17:40:40 +0100

    Implemented peak finding method.

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/peakfinding/MaximaDifference.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/peakfinding/MaximaDifference.java
@@ -9,9 +9,11 @@
 
 package uk.ac.diamond.scisoft.analysis.peakfinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
 
 
 public class MaximaDifference extends AbstractPeakFinder {
@@ -33,8 +35,48 @@ public class MaximaDifference extends AbstractPeakFinder {
 	}
 	
 	@Override
-	public List<Double> findPeaks(IDataset xData, IDataset yData, Integer nPeaks) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Integer> findPeaks(IDataset xData, IDataset yData, Integer nPeaks) {
+		List<Integer> peakPosns = new ArrayList<Integer>();
+		
+		//Put our peak finding parameters into more accessible variables
+		Integer minSignificance;
+		Integer windowSize;
+		try {
+			minSignificance = (Integer)getParameter("minSignificance");
+			windowSize = (Integer)getParameter("windowSize");
+		} catch(Exception e) {
+			logger.error("Could not find specified peak finding parameters");
+			return null;
+		}
+		
+		int nrPoints = yData.getSize();
+		
+		for(int i = windowSize; i <= (nrPoints-windowSize-1); i++) {
+			double posSig = calcPosPeakSig(i, windowSize, yData);
+			
+			if (posSig >= minSignificance) {
+				peakPosns.add(i);
+			}
+		}
+		
+		return peakPosns;
+	}
+	
+	public double calcPosPeakSig(int position, int windowSize, IDataset yData) {
+		double posVal = yData.getDouble(position);
+		
+		//Calculate the differences between the position & each point across
+		//the two windows. N.B. left & right diffs are in opposite directions. 
+		IDataset leftDiffs = new DoubleDataset(windowSize);
+		IDataset rightDiffs = new DoubleDataset(windowSize);
+		for(int i = 0; i < windowSize; i++) {
+			leftDiffs.set(posVal-yData.getDouble(position-i-1), i);
+			rightDiffs.set(posVal-yData.getDouble(position+i+1), i);
+		}
+		
+		//Calculate the average of the maximum of the differences (i.e. significance)
+		double sig = (leftDiffs.max().doubleValue() + rightDiffs.max().doubleValue())/2; 
+		
+		return sig;
 	}
 }

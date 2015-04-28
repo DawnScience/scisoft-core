71033fdf9da25f7586dc540d135875b4267bf3e7
Jun Aishima <jun.aishima@diamond.ac.uk>
2015-04-28 14:53:25 +0100

    handle cleared mask file path correctly
     * when cleared, clear existing stored mask
     * model.filePath will be null after being cleared, so check for this
       so that a useful exception will be thrown

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/mask/ImportMaskOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/mask/ImportMaskOperation.java
@@ -8,6 +8,8 @@
  */
 package uk.ac.diamond.scisoft.analysis.processing.operations.mask;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Arrays;
 
 import org.dawb.common.services.ServiceManager;
@@ -20,6 +22,7 @@ import org.eclipse.dawnsci.analysis.api.persistence.IPersistentFile;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
 import org.eclipse.dawnsci.analysis.dataset.impl.Comparisons;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.analysis.dataset.metadata.MaskMetadataImpl;
@@ -32,6 +35,7 @@ public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOper
 
 	
 	IDataset mask;
+	private PropertyChangeListener listener;
 	
 	@Override
 	public String getId() {
@@ -45,6 +49,7 @@ public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOper
 		
 		if (mask == null) {
 			String filePath = ((ImportMaskModel)model).getFilePath();
+			if (filePath == null) throw new IllegalArgumentException("mask file path may not be null");
 			try {
 				
 				if (HierarchicalDataFactory.isHDF5(filePath)) {
@@ -86,6 +91,25 @@ public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOper
 		return new OperationData(input);
 	}
 
+	@Override
+	public void setModel(ImportMaskModel model) {
+		
+		super.setModel(model);
+		if (listener == null) {
+			listener = new PropertyChangeListener() {
+				
+				@Override
+				public void propertyChange(PropertyChangeEvent evt) {
+					mask = null;
+				}
+			};
+		} else {
+			((AbstractOperationModel)this.model).removePropertyChangeListener(listener);
+		}
+		
+		((AbstractOperationModel)this.model).addPropertyChangeListener(listener);
+	}
+	
 	@Override
 	public OperationRank getInputRank() {
 		return OperationRank.TWO;

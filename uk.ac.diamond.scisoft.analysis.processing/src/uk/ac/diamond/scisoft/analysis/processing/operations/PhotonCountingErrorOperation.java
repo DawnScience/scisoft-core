326f4cf70ea6fe6f5907d66f8ee013bba7776e27
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-05-19 09:16:29 +0100

    Speed up error generation

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/PhotonCountingErrorOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/PhotonCountingErrorOperation.java
@@ -1,12 +1,14 @@
 package uk.ac.diamond.scisoft.analysis.processing.operations;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
-import org.eclipse.dawnsci.analysis.api.metadata.AxesMetadata;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.IndexIterator;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 
 public class PhotonCountingErrorOperation extends AbstractOperation<EmptyModel, OperationData> {
@@ -18,13 +20,21 @@ public class PhotonCountingErrorOperation extends AbstractOperation<EmptyModel, 
 	
 	@Override
 	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+
+		DoubleDataset er = new DoubleDataset(input.getShape());
+		Dataset in = DatasetUtils.convertToDataset(input.getSliceView());
 		
-		IDataset er = input.getSliceView();
-		er.clearMetadata(AxesMetadata.class);
+		IndexIterator i = in.getIterator();
+		double val = 0;
+		while (i.hasNext()) {
+			val = in.getElementDoubleAbs(i.index);
+			if (val < 0) continue;
+			er.setAbs(i.index, Math.sqrt(val));
+		}
 		
-		((Dataset)input).setErrorBuffer(er);
-		
-		return new OperationData(input);
+		in.setError(er);
+
+		return new OperationData(in);
 	}
 	
 

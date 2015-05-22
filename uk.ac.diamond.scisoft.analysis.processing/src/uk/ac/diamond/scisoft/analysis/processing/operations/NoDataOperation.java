f078780888f04a41af50038c752ecf65225526d2
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-05-22 11:21:17 +0100

    Added some utility operations

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/NoDataOperation.java
@@ -0,0 +1,40 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
+
+public class NoDataOperation extends AbstractOperation<EmptyModel, OperationData> {
+
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		return null;
+	}
+	
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.NoDataOperation";
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.ANY;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.SAME;
+	}
+
+}

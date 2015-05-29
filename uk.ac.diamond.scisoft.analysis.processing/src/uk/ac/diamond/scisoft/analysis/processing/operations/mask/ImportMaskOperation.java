e4678ae33d782f3ae05a4b88e4785f46c69e5a86
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-05-29 11:36:44 +0100

    Threadsafe import mask

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/mask/ImportMaskOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/mask/ImportMaskOperation.java
@@ -15,14 +15,17 @@ import java.util.Arrays;
 import org.dawb.common.services.ServiceManager;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
+import org.eclipse.dawnsci.analysis.api.metadata.IDiffractionMetadata;
 import org.eclipse.dawnsci.analysis.api.metadata.MaskMetadata;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.persistence.IPersistenceService;
 import org.eclipse.dawnsci.analysis.api.persistence.IPersistentFile;
+import org.eclipse.dawnsci.analysis.api.processing.Atomic;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
 import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+import org.eclipse.dawnsci.analysis.dataset.impl.BooleanDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Comparisons;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.analysis.dataset.metadata.MaskMetadataImpl;
@@ -30,11 +33,14 @@ import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 import org.eclipse.dawnsci.hdf5.HierarchicalDataFactory;
 
 import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
+import uk.ac.diamond.scisoft.analysis.io.NexusDiffractionMetaReader;
+import uk.ac.diamond.scisoft.analysis.processing.operations.twod.DiffractionMetadataImportModel;
 
+@Atomic
 public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOperation<ImportMaskModel,OperationData>{
 
 	
-	IDataset mask;
+	private volatile IDataset mask;
 	private PropertyChangeListener listener;
 	
 	@Override
@@ -45,44 +51,16 @@ public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOper
 	
 	@Override
 	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		if (model.getFilePath() == null) throw new IllegalArgumentException("mask file path may not be null");
+		
 		IDataset inM = DatasetUtils.convertToDataset(getFirstMask(input));
 		
-		if (mask == null) {
-			String filePath = ((ImportMaskModel)model).getFilePath();
-			if (filePath == null) throw new IllegalArgumentException("mask file path may not be null");
-			try {
-				
-				if (HierarchicalDataFactory.isHDF5(filePath)) {
-					IPersistenceService service = (IPersistenceService)ServiceManager.getService(IPersistenceService.class);
-					IPersistentFile pf = service.getPersistentFile(filePath);
-					IDataset m = pf.getMask(pf.getMaskNames(null).get(0),null);
-					if (Arrays.equals(m.squeeze().getShape(), input.getShape())) {
-						mask = m;
-					} else {
-						throw new IllegalArgumentException("mask not compatible shape");
-					}
-					
-				} else {
-					IDataHolder dh = LoaderFactory.getData(filePath);
-					IDataset ds = dh.getDataset(0);
-					if (Arrays.equals(ds.squeeze().getShape(), input.getShape())) {
-						mask = ds;
-					} else {
-						throw new IllegalArgumentException("mask not compatible shape");
-					}
-				}
-				
-				
-			} catch (Exception e) {
-				throw new OperationException(this, e);
-			}
-			
-		}
+		IDataset m = getMask(model.getFilePath(), input.getShape());
 		
 		if (inM == null) {
-			inM = mask;
+			inM = m;
 		} else {
-			inM = Comparisons.logicalAnd(inM, mask);
+			inM = Comparisons.logicalAnd(inM, m);
 		}
 		
 		MaskMetadata mm = new MaskMetadataImpl(inM);
@@ -90,6 +68,47 @@ public class ImportMaskOperation<T extends ImportMaskModel> extends AbstractOper
 
 		return new OperationData(input);
 	}
+	
+	private IDataset getMask(String path, int[] shape) {
+
+		IDataset lmask = mask;
+		if (lmask == null) {
+			synchronized(this) {
+				lmask = mask;
+				if (lmask == null) {
+					try {
+						if (HierarchicalDataFactory.isHDF5(path)) {
+							IPersistenceService service = (IPersistenceService)ServiceManager.getService(IPersistenceService.class);
+							IPersistentFile pf = service.getPersistentFile(path);
+							IDataset m = pf.getMask(pf.getMaskNames(null).get(0),null);
+							if (Arrays.equals(m.squeeze().getShape(), shape)) {
+								mask = lmask = m;
+							} else {
+								throw new IllegalArgumentException("mask not compatible shape");
+							}
+							
+						} else {
+							IDataHolder dh = LoaderFactory.getData(path);
+							IDataset ds = dh.getDataset(0);
+							if (Arrays.equals(ds.squeeze().getShape(), shape)) {
+								mask = lmask = ds;
+							} else {
+								throw new IllegalArgumentException("mask not compatible shape");
+							}
+						}
+						
+						
+					} catch (Exception e) {
+						throw new OperationException(this, e);
+					}
+				
+					
+				}
+			}
+		}
+		return lmask;
+	}
+	
 
 	@Override
 	public void setModel(ImportMaskModel model) {

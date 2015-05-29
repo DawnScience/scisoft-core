e85da086c2de228dc89a61dc80620381daf8fe4f
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-05-29 14:20:54 +0100

    Make utilities methods atomic

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/NoDataOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/NoDataOperation.java
@@ -11,11 +11,13 @@ package uk.ac.diamond.scisoft.analysis.processing.operations;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.processing.Atomic;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 
+@Atomic
 public class NoDataOperation extends AbstractOperation<EmptyModel, OperationData> {
 
 	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {

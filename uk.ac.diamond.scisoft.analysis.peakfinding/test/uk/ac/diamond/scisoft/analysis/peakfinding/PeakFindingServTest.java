3c1a4d902e806b38f152950a25cc2fd5666be27c
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-06-17 14:32:03 +0100

    JUnit test of peak finding service.

---
+++ uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingServTest.java
@@ -0,0 +1,42 @@
+package uk.ac.diamond.scisoft.analysis.peakfinding;
+
+import java.util.Collection;
+
+import org.eclipse.dawnsci.analysis.api.peakfinding.IPeakFinder;
+import org.junit.BeforeClass;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeakFindingServTest {
+	
+	/**
+	 * This has been heavily based on OperationsTest (u.a.d.s.a.processing.test).
+	 * The role of this class is to test with JUnit, therefore not using extension points
+	 */
+	
+	private static IPeakFindingService peakFindServ;
+	
+	@BeforeClass
+	public static void setupNonOSGiService() {
+		peakFindServ = (IPeakFindingService)Activator.getService(IPeakFindingService.class);
+		
+		//Grab the all the PeakFinders in u.a.d.s.a.peakfinding.peakfinders
+		peakFindServ.addPeakFindersByClass(peakFindServ.getClass().getClassLoader(), "uk.ac.diamond.scisoft.analysis.processing.peakFinding.peakFinders");
+		
+	}
+	
+	@Test
+	public void testGetService() {
+		assertNotNull(peakFindServ);
+	}
+	
+	@Test
+	public void testServiceHasPeakFinders() {
+		final Collection<String> peakFinderNames = peakFindServ.getRegisteredNames();
+		assertNotNull(peakFinderNames);
+		assertTrue(peakFinderNames.isEmpty());
+	}
+
+}

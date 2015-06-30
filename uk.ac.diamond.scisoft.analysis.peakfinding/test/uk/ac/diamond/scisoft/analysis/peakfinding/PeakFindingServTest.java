d17470876642bb53ec3e44203ee19218feb320f9
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-06-30 15:31:21 +0100

    Adds tests for activate and peakfinding.

--- uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingServTest.java
+++ uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingServTest.java
@@ -1,12 +1,23 @@
 package uk.ac.diamond.scisoft.analysis.peakfinding;
 
+//import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.junit.BeforeClass;
-
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import uk.ac.diamond.scisoft.analysis.peakfinding.peakfinders.DummyPeakFinder;
 
 public class PeakFindingServTest {
 	
@@ -36,6 +47,48 @@ public class PeakFindingServTest {
 		final Collection<String> peakFinderNames = peakFindServ.getRegisteredPeakFinders();
 		assertNotNull(peakFinderNames);
 		assertFalse(peakFinderNames.isEmpty());
+	}
+	
+	@Test
+	public void testActivatePeakFinders() throws Exception {
+		//Peak finders to set active
+		String dummyID = DummyPeakFinder.class.getName();
+		
+		peakFindServ.setPeakFinderActive(dummyID);
+		
+		Set<String> activePFs = (Set<String>) peakFindServ.getActivePeakFinders();
+		Iterator<String> activePFIter = activePFs.iterator();
+		boolean gotDummy = false;
+		while (activePFIter.hasNext()) {
+			String pfID = activePFIter.next();
+			if (pfID.equals(dummyID)) gotDummy = true;
+		}
+		assertTrue(gotDummy);
+		
+	}
+	
+	@Test
+	public void testOnePeakFinder() throws Exception {
+		final Set<String> peakFinderIDs = (Set<String>) peakFindServ.getRegisteredPeakFinders();
+		
+		Map<Integer, Double>testData = new TreeMap<Integer, Double>();
+		testData.put(1, 0.6);
+		testData.put(2, 1.2);
+		testData.put(3, 0.9);
+		testData.put(5, 1.7);
+		testData.put(7, 2.5);
+		testData.put(11, 0.9);
+		testData.put(13, 0.6);
+		
+		//Find the dummy peakfinder
+		String dummyPFClassName = DummyPeakFinder.class.getName();
+		List<String> peakFinderIDList = new ArrayList<String>(peakFinderIDs);
+		if (!peakFinderIDList.contains(dummyPFClassName)) {
+			fail("Dummy peak finder not registered");
+		}
+//		IPeakFinder testPF = peakFindServ.getPeakFinder(dummyPFClassName);
+//		Map<Integer, Double> peakPosnsSigs = testPF.findPeaks(null, null, null);
+//		assertEquals(testData, peakPosnsSigs);
 	}
 
 }

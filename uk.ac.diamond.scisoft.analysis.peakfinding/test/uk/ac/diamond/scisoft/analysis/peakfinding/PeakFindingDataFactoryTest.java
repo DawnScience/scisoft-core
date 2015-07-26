a9f8f8e74b8c9ea45e21a56b298c569a74260a54
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-07-26 16:10:08 +0100

    Completes re-write of create parameter set tests.

--- uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingDataFactoryTest.java
+++ uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingDataFactoryTest.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.peakfinding.IPeakFinderParameter;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,6 +26,13 @@ public class PeakFindingDataFactoryTest {
 	private static IPeakFindingService peakFindServ;
 	private String dummyID = DummyPeakFinder.class.getName();
 	private String maxDiffID = MaximaDifference.class.getName();
+	
+	//Dummy values for creating PeakFindingData DTOs
+	private static List<String> activePFs;
+	private Integer nPeaks = DummyPeakFinder.getFakePeaks().size();
+	private IDataset xData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
+	private IDataset yData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
+	
 
 	@BeforeClass
 	public static void setupNonOSGiService() throws Exception {
@@ -31,7 +40,11 @@ public class PeakFindingDataFactoryTest {
 		
 		//Grab the all the PeakFinders in u.a.d.s.a.peakfinding.peakfinders
 		peakFindServ.addPeakFindersByClass(peakFindServ.getClass().getClassLoader(), "uk.ac.diamond.scisoft.analysis.peakfinding.peakfinders");
-		
+	}
+	
+	@Before
+	public static void createVars() {
+		activePFs = new ArrayList<String>();
 	}
 	
 	@Rule
@@ -39,26 +52,18 @@ public class PeakFindingDataFactoryTest {
 	
 	@Test
 	public void createPeakFindingData() {
-		List<String> activePFs = new ArrayList<String>();
+		
+		
 		activePFs.add(dummyID);
-		
-		Integer nPeaks = DummyPeakFinder.getFakePeaks().size();
-		
-		IDataset xData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
-		IDataset yData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
-		
 		IPeakFindingData pfd = PeakFindingDataFactory.createPeakFindingData(peakFindServ, activePFs, nPeaks, xData, yData);
 		
 		assertTrue(pfd.hasActivePeakFinders());
 		assertTrue(pfd.hasData());
-		
 		assertEquals(nPeaks, pfd.getNPeaks());
-		
 		assertEquals(xData, pfd.getData()[0]);
 		assertEquals(yData, pfd.getData()[1]);
 		
 		activePFs.add(maxDiffID);
-		
 		IPeakFindingData pfd2 = PeakFindingDataFactory.createPeakFindingData(peakFindServ, activePFs, nPeaks, xData, yData);
 		
 		assertTrue(pfd2.hasActivePeakFinders());
@@ -68,26 +73,33 @@ public class PeakFindingDataFactoryTest {
 	}
 	
 	@Test
-	public void createPSet() throws Exception {
-		
-		Number[] vals = new Number[]{12, 14.2};
-		
-		Map<String, IPeakFinderParameter> pSet = PeakFindingDataFactory.createParameterSet(dummyID, vals);
+	public void createPSetFromServ() throws Exception {
+		Map<String, Number> vals = new TreeMap<String, Number>();
+		vals.put("testParamA", 14.2);
+		vals.put("testParamB", 12);
+				
+		Map<String, IPeakFinderParameter> pSet = PeakFindingDataFactory.createParameterSet(peakFindServ, dummyID, vals);
 		assertEquals(2, pSet.size());
+		assertEquals(14.2, pSet.get("testParamA").getValue());
+		assertEquals(12, pSet.get("testParamB").getValue());
 	}
 	
-	/*
-	 * The next tests check errors are thrown when nonsense is given to the 
-	 * factory
-	 */
 	@Test
-	public void createPSetDiffLengthsException() throws Exception {
-		thrower.expect(Exception.class);
-		thrower.expectMessage("Lengths of supplied");
+	public void createPSetFromDTO() throws Exception {
+		activePFs.add(dummyID);
 		
-		Number[] vals = new Number[]{12, 14.2, 65, 86};
+		IPeakFindingData pfDTO = PeakFindingDataFactory.createPeakFindingData(peakFindServ, activePFs, nPeaks, xData, yData);
 		
-		PeakFindingDataFactory.createParameterSet(peakFindServ, dummyID, vals);
+		Map<String, Number> vals = new TreeMap<String, Number>();
+		vals.put("testParamA", 14.2);
+		vals.put("testParamB", 12);
+		Map<String, IPeakFinderParameter> pSet = PeakFindingDataFactory.createParameterSet(pfDTO, dummyID, vals);
+		assertEquals(2, pSet.size());
+		assertEquals(14.2, pSet.get("testParamA").getValue());
+		assertEquals(12, pSet.get("testParamB").getValue());
+		
 	}
+	
+
 
 }

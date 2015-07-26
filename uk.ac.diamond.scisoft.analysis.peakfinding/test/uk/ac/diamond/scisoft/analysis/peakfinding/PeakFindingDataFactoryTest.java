7f9dfa7a907a111c1578b4a3ebdc1fbcdf7b8e7b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-07-26 15:26:07 +0100

    Adds new createPeakFindingData test and refactors createParameterSet tests.

--- uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingDataFactoryTest.java
+++ uk.ac.diamond.scisoft.analysis.peakfinding/test/uk/ac/diamond/scisoft/analysis/peakfinding/PeakFindingDataFactoryTest.java
@@ -2,25 +2,77 @@ package uk.ac.diamond.scisoft.analysis.peakfinding;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.peakfinding.IPeakFinderParameter;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class PeakFindingDataFactoryTest {
+import uk.ac.diamond.scisoft.analysis.peakfinding.peakfinders.DummyPeakFinder;
+import uk.ac.diamond.scisoft.analysis.peakfinding.peakfinders.MaximaDifference;
 
+public class PeakFindingDataFactoryTest {
+	
+	private static IPeakFindingService peakFindServ;
+	private String dummyID = DummyPeakFinder.class.getName();
+	private String maxDiffID = MaximaDifference.class.getName();
+
+	@BeforeClass
+	public static void setupNonOSGiService() throws Exception {
+		peakFindServ = (IPeakFindingService)Activator.getService(IPeakFindingService.class);
+		
+		//Grab the all the PeakFinders in u.a.d.s.a.peakfinding.peakfinders
+		peakFindServ.addPeakFindersByClass(peakFindServ.getClass().getClassLoader(), "uk.ac.diamond.scisoft.analysis.peakfinding.peakfinders");
+		
+	}
+	
 	@Rule
 	public ExpectedException thrower = ExpectedException.none();
 	
 	@Test
-	public void createPSet() throws Exception {		
-		String[] names = new String[]{"paramA", "paramB"};
-		Boolean[] isInts = new Boolean[]{true, false};
+	public void createPeakFindingData() {
+		List<String> activePFs = new ArrayList<String>();
+		activePFs.add(dummyID);
+		
+		Integer nPeaks = DummyPeakFinder.getFakePeaks().size();
+		
+		IDataset xData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
+		IDataset yData = DatasetFactory.createRange(0, 10, 1, Dataset.FLOAT32);
+		
+		IPeakFindingData pfd = PeakFindingDataFactory.createPeakFindingData(peakFindServ, activePFs, nPeaks, xData, yData);
+		
+		assertTrue(pfd.hasActivePeakFinders());
+		assertTrue(pfd.hasData());
+		
+		assertEquals(nPeaks, pfd.getNPeaks());
+		
+		assertEquals(xData, pfd.getData()[0]);
+		assertEquals(yData, pfd.getData()[1]);
+		
+		activePFs.add(maxDiffID);
+		
+		IPeakFindingData pfd2 = PeakFindingDataFactory.createPeakFindingData(peakFindServ, activePFs, nPeaks, xData, yData);
+		
+		assertTrue(pfd2.hasActivePeakFinders());
+		Set<String> pfd2Actives = (Set<String>) pfd2.getActivePeakFinders();
+		assertTrue(pfd2Actives.contains(dummyID));
+		assertTrue(pfd2Actives.contains(maxDiffID));
+	}
+	
+	@Test
+	public void createPSet() throws Exception {
+		
 		Number[] vals = new Number[]{12, 14.2};
 		
-		Map<String, IPeakFinderParameter> pSet = PeakFindingDataFactory.createParameterSet(names, isInts, vals);
+		Map<String, IPeakFinderParameter> pSet = PeakFindingDataFactory.createParameterSet(dummyID, vals);
 		assertEquals(2, pSet.size());
 	}
 	
@@ -33,11 +85,9 @@ public class PeakFindingDataFactoryTest {
 		thrower.expect(Exception.class);
 		thrower.expectMessage("Lengths of supplied");
 		
-		String[] names = new String[]{"paramA", "paramB", "boris"};
-		Boolean[] isInts = new Boolean[]{true, false};
 		Number[] vals = new Number[]{12, 14.2, 65, 86};
 		
-		PeakFindingDataFactory.createParameterSet(names, isInts, vals);
+		PeakFindingDataFactory.createParameterSet(peakFindServ, dummyID, vals);
 	}
 
 }

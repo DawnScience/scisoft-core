f74afe93bce08f43454295beafc0798f068ab523
Tim Spain <timothy.spain@diamond.ac.uk>
2015-11-11 11:58:46 +0000

    Stubby version of the XPDF View.

---
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/XPDFSampleView.java
@@ -0,0 +1,20 @@
+package uk.ac.diamond.scisoft.xpdf.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class XPDFSampleView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

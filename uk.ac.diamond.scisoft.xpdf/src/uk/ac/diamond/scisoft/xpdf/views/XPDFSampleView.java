d5d11da12fb2bf9b3e4c5b23e637e225a35efe61
Tim Spain <timothy.spain@diamond.ac.uk>
2015-11-12 08:01:16 +0000

    Add basic functionality to the XPDF sample View. It doesn't work yet, but I want to change branches in Eclipse.

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/XPDFSampleView.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/XPDFSampleView.java
@@ -11,12 +11,18 @@ import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
 
 public class XPDFSampleView extends ViewPart {
-	public XPDFSampleView() {
+	TableViewer sampleTV;
+	Button cifButton;
+	Button eraButton;
+	
+	public XPDFSampleView(Composite parent, int style) {
+		createPartControl(parent);
 	}
 
 	@Override
@@ -26,24 +32,45 @@ public class XPDFSampleView extends ViewPart {
 		sampleTableCompo.setLayout(new FormLayout());
 		
 		// Table viewer to hold the main data table
-		final TableViewer sampleTV = new TableViewer(sampleTableCompo);
-//		FormData sampleTVData = new FormData(800, 600);
-		Button cifButton = new Button(sampleTableCompo, SWT.NONE);
+		sampleTV = new TableViewer(sampleTableCompo);
+		FormData formData= new FormData(800, 600);
+		sampleTV.getTable().setLayoutData(formData);
+		createLoadButtons();
+	}
+
+	private void createLoadButtons() {
+		Composite stCompo = sampleTV.getTable().getParent();
+		cifButton = new Button(stCompo, SWT.NONE);
 		FormData formData = new FormData();
-		formData.left = new FormAttachment(sampleTableCompo);
+		formData.left = new FormAttachment(stCompo);
 		formData.top = new FormAttachment(sampleTV.getControl());
 		cifButton.setLayoutData(formData);
-		Button eraButton = new Button(sampleTableCompo, SWT.NONE);
+		cifButton.setText("New sample from CIF file.");
+		eraButton = new Button(stCompo, SWT.NONE);
 		formData = new FormData();
-		formData.left = new FormAttachment(sampleTableCompo);
+		formData.left = new FormAttachment(stCompo);
 		formData.top = new FormAttachment(cifButton);
 		eraButton.setLayoutData(formData);
+		eraButton.setText("New sample from ERA file.");
 		
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
+		sampleTV.getControl().setFocus();
 	}
+	
+	// Probably needs more eclipse wrappings
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		XPDFSampleView xSV = new XPDFSampleView(shell, SWT.NONE);
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed())
+			if (!display.readAndDispatch())
+				display.sleep();
+		display.dispose();
+	}
+	
 }

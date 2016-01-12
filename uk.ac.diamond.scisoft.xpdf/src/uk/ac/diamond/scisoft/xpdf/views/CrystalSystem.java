4ea9095d4912a133ce4b28a07d520a835e59108b
Tim Spain <timothy.spain@diamond.ac.uk>
2016-01-12 15:08:29 +0000

    Return fixed angles from the crystal system.

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/CrystalSystem.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/CrystalSystem.java
@@ -49,6 +49,10 @@ class CrystalSystem {
 		}		
 	}
 	
+	public int[] getFixedAngles() {
+		return fixedAxes[number];
+	}
+	
 	private static void generateSystems() {
 		systems = new CrystalSystem[nSystems+1]; // 7 crystal systems, and amorphous at 0
 		for (int iSystem = 0; iSystem <= nSystems ; iSystem++) {
@@ -60,7 +64,17 @@ class CrystalSystem {
 	
 	private final static int[] lowestGroups = {0, 1, 3, 16, 75, 143, 168, 195};
 	private final static int[] highestGroups = {0, 2, 15, 74, 142, 167, 194, 230};
-	private final static String[] names = {"Amorphous", "Triclinic", "Monoclinic", "Orthorhombic",
+	private final static String[] names = {"Undefined", "Triclinic", "Monoclinic", "Orthorhombic",
 		"Tetragonal", "Trigonal", "Hexagonal", "Cubic"};
 	private final static int[] nFreeAxes = {0, 3, 3, 3, 2, 1, 2, 1};
+	private final static int[][] fixedAxes = {
+		new int[] {-1, -1, -1},
+		new int[] {-1, -2, -3},
+		new int[] {90, -2, 90},
+		new int[] {90, 90, 90},
+		new int[] {90, 90, 90},
+		new int[] {-1, -1, -1},
+		new int[] {90, 90, 120},
+		new int[] {90, 90, 90}};
+		
 }

161c62914f7ecb07e42c7ad277f21232ed4dc952
Tim Spain <timothy.spain@diamond.ac.uk>
2016-01-13 09:18:39 +0000

    Remove the 0 position 'undefined' crystal system, since this is now signalled separately in the phase.

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/CrystalSystem.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/CrystalSystem.java
@@ -19,20 +19,28 @@ class CrystalSystem {
 	static final int nSystems = 7;
 	
 	public static CrystalSystem get(int systemOrdinal) {
-		if (systems == null || systems.length <= nSystems+1) {
+		if (systems == null || systems.length < nSystems) {
 			generateSystems();
 		}
 		return systems[systemOrdinal];
 	}
 		
 	public String getName() {
-		return (number >= 0 && number <= nSystems) ? names[number] : "Unknown";
+		return (number >= 0 && number < nSystems) ? names[number] : "Unknown";
+	}
+	
+	public int getOrdinal() {
+		return number;
+	}
+	
+	public static String[] getNames() {
+		return names;
 	}
 	
 	public List<XPDFSpaceGroup> getGroups() {
-		List<XPDFSpaceGroup> groupList = new ArrayList<XPDFSpaceGroup>(highestGroups[number] - lowestGroups[number] + 1);
-		for (int group = lowestGroups[number]; group <= highestGroups[number]+1; group++)
-			groupList.add(group, XPDFSpaceGroup.get(group));
+		List<XPDFSpaceGroup> groupList = new ArrayList<XPDFSpaceGroup>();
+		for (int group = lowestGroups[number]; group <= highestGroups[number]; group++)
+			groupList.add(XPDFSpaceGroup.get(group));
 		return groupList;
 	}
 	
@@ -54,21 +62,20 @@ class CrystalSystem {
 	}
 	
 	private static void generateSystems() {
-		systems = new CrystalSystem[nSystems+1]; // 7 crystal systems, and amorphous at 0
-		for (int iSystem = 0; iSystem <= nSystems ; iSystem++) {
+		systems = new CrystalSystem[nSystems]; // 7 crystal systems
+		for (int iSystem = 0; iSystem < nSystems ; iSystem++) {
 			CrystalSystem system = new CrystalSystem();
 			system.number = iSystem;
 			systems[iSystem] = system;
 		}
 	}
 	
-	private final static int[] lowestGroups = {0, 1, 3, 16, 75, 143, 168, 195};
-	private final static int[] highestGroups = {0, 2, 15, 74, 142, 167, 194, 230};
-	private final static String[] names = {"Undefined", "Triclinic", "Monoclinic", "Orthorhombic",
+	private final static int[] lowestGroups = {1, 3, 16, 75, 143, 168, 195};
+	private final static int[] highestGroups = {2, 15, 74, 142, 167, 194, 230};
+	private final static String[] names = {"Triclinic", "Monoclinic", "Orthorhombic",
 		"Tetragonal", "Trigonal", "Hexagonal", "Cubic"};
-	private final static int[] nFreeAxes = {0, 3, 3, 3, 2, 1, 2, 1};
+	private final static int[] nFreeAxes = {3, 3, 3, 2, 1, 2, 1};
 	private final static int[][] fixedAxes = {
-		new int[] {-1, -1, -1},
 		new int[] {-1, -2, -3},
 		new int[] {90, -2, 90},
 		new int[] {90, 90, 90},

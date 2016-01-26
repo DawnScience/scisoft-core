DawnScience/scisoft-core

commit 65a4795c180ec071dfdd2b7499c2a81fda11893c

Remove unnecessary methods and add more tests

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/AFunction.java
@@ -84,9 +84,10 @@ public abstract class AFunction implements IFunction, Serializable {
 	 * @param numberOfParameters
 	 */
 	public AFunction(int numberOfParameters) {
-		fillParameters(new double[numberOfParameters]);
-
-		setNames();
+		parameters = new IParameter[numberOfParameters];
+		for (int i = 0; i < numberOfParameters; i++) {
+			parameters[i] = new Parameter();
+		}
 	}
 
 	/**
@@ -96,8 +97,9 @@ public abstract class AFunction implements IFunction, Serializable {
 	 *            An array of starting parameter values as doubles.
 	 */
 	public AFunction(double... params) {
-		fillParameters(params);
+		this(params.length);
 
+		setParameterValues(params);
 		setNames();
 	}
 
@@ -108,32 +110,10 @@ public abstract class AFunction implements IFunction, Serializable {
 	 *            An array of parameters
 	 */
 	public AFunction(IParameter... params) {
-		fillParameters(params);
+		this(params.length);
 
+		setParameters(params);
 		setNames();
-	}
-
-	protected void fillParameters(double... params) {
-		if (parameters == null) {
-			parameters = new IParameter[params.length];
-		}
-
-		int n = Math.min(params.length, parameters.length);
-		for (int i = 0; i < n; i++) {
-			parameters[i] = new Parameter(params[i]);
-		}
-	}
-
-	protected void fillParameters(IParameter... params) {
-		if (parameters == null) {
-			parameters = new IParameter[params.length];
-		}
-
-		int n = Math.min(params.length, parameters.length);
-		for (int i = 0; i < n; i++) {
-			IParameter p = params[i];
-			parameters[i] = new Parameter(p);
-		}
 	}
 
 	/**

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/AOperator.java
@@ -219,12 +219,8 @@ abstract public class AOperator extends AFunction implements IOperator {
 	@Override
 	public AOperator copy() throws Exception {
 		Constructor<? extends AOperator> c = getClass().getConstructor();
-
-		IParameter[] localParameters = getParameters();
-			
 		AOperator operator = c.newInstance();
-		operator.fillParameters(localParameters); //Do we need to do this?
-		
+
 		for (IFunction f : getFunctions()) {
 			if (f == null) {
 				operator.addFunction(null);

uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/FitterTest.java
@@ -18,7 +18,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import uk.ac.diamond.scisoft.analysis.fitting.functions.AFunction;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Add;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.Gaussian;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Offset;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.Polynomial;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.Quadratic;
 
@@ -42,6 +44,36 @@ public class FitterTest {
 		Assert.assertEquals(pos, result.getParameterValue(0), 0.1);
 		Assert.assertEquals(fwhm, result.getParameterValue(1), 0.1);
 		Assert.assertEquals(area, result.getParameterValue(2), 0.1);
+	}
+
+	@Test
+	public void testGaussianOffsetFit() {
+		double pos = 14.5;
+		double fwhm = 4.5;
+		double area = 4.5;
+		double coff = 2.5;
+		Gaussian gauss = new Gaussian(pos,fwhm,area);
+		gauss.getParameter(0).setLimits(10, 19);
+		gauss.getParameter(1).setLimits(0, 9);
+		gauss.getParameter(2).setLimits(0, 45);
+		Offset off = new Offset(coff);
+		off.getParameter(0).setLimits(0, 5);
+		Add add = new Add();
+		add.addFunction(gauss);
+		add.addFunction(off);
+		DoubleDataset xAxis = (DoubleDataset) DatasetFactory.createRange(10, 20, 1, Dataset.FLOAT64);
+		DoubleDataset data = (DoubleDataset) DatasetFactory.createFromObject(new double[] {0, 1, 2, 3, 4, 5, 4, 3, 2, 1}, Dataset.FLOAT64);
+//		DoubleDataset ds = gauss.calculateValues(xAxis);
+
+		try {
+			Fitter.geneticFit(new Dataset[] {xAxis}, data, add);
+		} catch (Exception e) {
+		}
+
+		Assert.assertEquals(15.0, gauss.getParameterValue(0), 0.1);
+		Assert.assertEquals(5.2, gauss.getParameterValue(1), 0.1);
+		Assert.assertEquals(25.8, gauss.getParameterValue(2), 0.1);
+		Assert.assertEquals(0.0, off.getParameterValue(0), 0.1);
 	}
 
 	@Test

uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/functions/PolynomialTest.java
@@ -42,6 +42,24 @@ public class PolynomialTest {
 
 		dx = f.calculatePartialDerivativeValues(f.getParameter(3), xd);
 		Assert.assertArrayEquals(new double[] {1, 1, 1}, dx.getData(), ABS_TOL);
+
+		((Polynomial) f).setDegree(2);
+		Assert.assertEquals(3, f.getNoOfParameters());
+		f.setParameterValues(23., -10., 1.2);
+		Assert.assertArrayEquals(new double[] {23., -10., 1.2}, f.getParameterValues(), ABS_TOL);
+		Assert.assertEquals(23. + 10. + 1.2, f.val(-1), ABS_TOL);
+
+		dx = f.calculateValues(xd);
+		Assert.assertArrayEquals(new double[] {23. + 10. + 1.2, 1.2, 23.*4 - 10.*2 + 1.2}, dx.getData(), ABS_TOL);
+
+		dx = f.calculatePartialDerivativeValues(f.getParameter(0), xd);
+		Assert.assertArrayEquals(new double[] {1, 0, 4}, dx.getData(), ABS_TOL);
+
+		dx = f.calculatePartialDerivativeValues(f.getParameter(1), xd);
+		Assert.assertArrayEquals(new double[] {-1, 0, 2}, dx.getData(), ABS_TOL);
+
+		dx = f.calculatePartialDerivativeValues(f.getParameter(2), xd);
+		Assert.assertArrayEquals(new double[] {1, 1, 1}, dx.getData(), ABS_TOL);
 	}
 
 	@Test

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/Fano.java
@@ -49,7 +49,7 @@ public class Fano extends AFunction {
 		if (params.length != PARAMS.length) {
 			throw new IllegalArgumentException("A Fano profile requires 4 parameters, and it has been given " + params.length);
 		}
-		fillParameters(params);
+		setParameterValues(params);
 	}
 
 	public Fano(IParameter... params) {
@@ -58,7 +58,7 @@ public class Fano extends AFunction {
 			throw new IllegalArgumentException("A Fano profile requires 4 parameters, and it has been given " + params.length);
 		}
 
-		fillParameters(params);
+		setParameters(params);
 	}
 
 	@Override

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/Polynomial.java
@@ -51,6 +51,7 @@ public class Polynomial extends AFunction {
 	 */
 	public Polynomial(final int degree) {
 		super(degree + 1);
+		nparams = degree + 1;
 	}
 
 	/**
@@ -59,6 +60,7 @@ public class Polynomial extends AFunction {
 	 */
 	public Polynomial(double[] params) {
 		super(params);
+		nparams = params.length;
 	}
 
 	/**
@@ -67,6 +69,7 @@ public class Polynomial extends AFunction {
 	 */
 	public Polynomial(IParameter... params) {
 		super(params);
+		nparams = params.length;
 	}
 
 	/**
@@ -80,7 +83,7 @@ public class Polynomial extends AFunction {
 	public Polynomial(double[] min, double[] max) {
 		super(0);
 		if (min.length != max.length) {
-			throw new IllegalArgumentException("");
+			throw new IllegalArgumentException("Bound arrays must be of equal length");
 		}
 		nparams = min.length;
 		parameters = new Parameter[nparams];
@@ -94,42 +97,30 @@ public class Polynomial extends AFunction {
 
 	@Override
 	protected void setNames() {
-		int n = getNoOfParameters();
-		String[] paramNames = new String[n];
-		for (int i = 0; i < n; i++) {
+		String[] paramNames = new String[nparams];
+		for (int i = 0; i < nparams; i++) {
 			paramNames[i] = "a_" + i;
 		}
 
 		setNames(NAME, DESC, paramNames);
 	}
 
-	@Override
-	protected void fillParameters(double... params) {
-		super.fillParameters(params);
-		nparams = getNoOfParameters();
-	}
-
-	@Override
-	protected void fillParameters(IParameter... params) {
-		super.fillParameters(params);
-		nparams = getNoOfParameters();
-	}
-
 	private void calcCachedParameters() {
 		if (a == null) {
 			a = new double[nparams];
 		}
-		for (int i = 0; i < nparams; i++)
+		for (int i = 0; i < nparams; i++) {
 			a[i] = getParameterValue(i);
+		}
 
 		setDirty(false);
 	}
 
 	@Override
 	public double val(double... values) {
-		if (isDirty())
+		if (isDirty()) {
 			calcCachedParameters();
-
+		}
 		final double position = values[0];
 
 		double v = a[0];
@@ -231,16 +222,21 @@ public class Polynomial extends AFunction {
 	 * @param degree
 	 */
 	public void setDegree(int degree) {
-		parameters = null;
-		a = new double[degree + 1];
+		nparams = degree + 1;
+		parameters = new IParameter[nparams];
+		for (int i = 0; i < nparams; i++) {
+			parameters[i] = new Parameter();
+		}
+		dirty = false;
 
-		fillParameters(a);
+		setNames();
 
-		if (parent != null)
+		if (parent != null) {
 			parent.updateParameters();
+		}
 	}
 	
-	public String getStringEquation(){
+	public String getStringEquation() {
 		
 		StringBuilder out = new StringBuilder();
 		

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/SmoothGoldEdgeFunction.java
@@ -64,14 +64,14 @@ public class SmoothGoldEdgeFunction extends AFunction implements
 		// make sure that there are 2 parameters, otherwise, throw a sensible error
 		if (params.length != PARAMS.length) 
 			throw new IllegalArgumentException("A SmoothGoldEdgeFunction requires 2 parameters, and it has only been given "+params.length);
-		fillParameters(params);
+		setParameterValues(params);
 	}
 
 	public SmoothGoldEdgeFunction(IParameter... params) {
 		super(PARAMS.length);
 		if (params.length != PARAMS.length) 
 			throw new IllegalArgumentException("A SmoothGoldEdgeFunction requires 2 parameters, and it has only been given "+params.length);
-		fillParameters(params);
+		setParameters(params);
 	}
 
 	@Override

commit 91588a2f59c7cb79dbfc5242fd9d8a23bd972bbf

Fix naming issues caused by 65a4795

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/AFunction.java
@@ -79,15 +79,31 @@ public abstract class AFunction implements IFunction, Serializable {
 
 
 	/**
+	 * Constructor for zero parameter functions
+	 */
+	public AFunction() {
+		parameters = new IParameter[0];
+
+		setNames();
+	}
+
+	/**
 	 * Constructor which simply generates the parameters but uninitialised
 	 * 
 	 * @param numberOfParameters
 	 */
 	public AFunction(int numberOfParameters) {
-		parameters = new IParameter[numberOfParameters];
+		parameters = createParameters(numberOfParameters);
+
+		setNames();
+	}
+
+	private static IParameter[] createParameters(int numberOfParameters) {
+		IParameter[] params = new IParameter[numberOfParameters];
 		for (int i = 0; i < numberOfParameters; i++) {
-			parameters[i] = new Parameter();
+			params[i] = new Parameter();
 		}
+		return params;
 	}
 
 	/**
@@ -97,7 +113,7 @@ public abstract class AFunction implements IFunction, Serializable {
 	 *            An array of starting parameter values as doubles.
 	 */
 	public AFunction(double... params) {
-		this(params.length);
+		parameters = createParameters(params.length);
 
 		setParameterValues(params);
 		setNames();
@@ -110,7 +126,7 @@ public abstract class AFunction implements IFunction, Serializable {
 	 *            An array of parameters
 	 */
 	public AFunction(IParameter... params) {
-		this(params.length);
+		parameters = createParameters(params.length);
 
 		setParameters(params);
 		setNames();

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/AOperator.java
@@ -24,7 +24,7 @@ abstract public class AOperator extends AFunction implements IOperator {
 	protected List<IParameter> params; // unique parameters
 
 	public AOperator() {
-		super(0);
+		super();
 		params = new ArrayList<>();
 	}
 

uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/APeak.java
@@ -54,6 +54,7 @@ public abstract class APeak extends AFunction implements IPeak {
 			throw new IllegalArgumentException("A peak requires 3 parameters, and it has been given " + params.length);
 		}
 		setParameterValues(params);
+		setNames();
 
 		getParameter(FWHM).setLowerLimit(0.0);
 	}
@@ -75,6 +76,7 @@ public abstract class APeak extends AFunction implements IPeak {
 			throw new IllegalArgumentException("A peak requires 3 parameters, and it has been given " + params.length);
 		}
 		setParameters(params);
+		setNames();
 
 		getParameter(FWHM).setLowerLimit(0.0);
 	}
@@ -83,6 +85,7 @@ public abstract class APeak extends AFunction implements IPeak {
 		super(3);
 
 		setParameters(peakParameters);
+		setNames();
 	}
 
 	protected void setParameters(IdentifiedPeak peakParameters) {
@@ -119,6 +122,7 @@ public abstract class APeak extends AFunction implements IPeak {
 	public APeak(double minPeakPosition, double maxPeakPosition, double maxFWHM, double maxArea) {
 		super(3);
 
+		setNames();
 		internalSetPeakParameters(minPeakPosition, maxPeakPosition, maxFWHM, maxArea);
 	}
 

uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/functions/OperatorTest.java
@@ -446,8 +446,13 @@ public class OperatorTest {
 	}
 
 	private static final class TestGenericBinaryOperator extends ABinaryOperator {
+		public TestGenericBinaryOperator() {
+			super();
+		}
+
 		@Override
 		protected void setNames() {
+			setNames("Hello", "World");
 		}
 
 		@Override
@@ -463,8 +468,12 @@ public class OperatorTest {
 	@Test
 	public void testSetFunction() {
 		IOperator myOp = new TestGenericBinaryOperator();
+		assertEquals("Hello", myOp.getName());
+		assertEquals("World", myOp.getDescription());
 
 		myOp.setFunction(1, new Gaussian());
+		assertEquals("Hello", myOp.getName());
+		assertEquals("World", myOp.getDescription());
 		assertEquals(1, myOp.getNoOfFunctions());
 		assertEquals(2, myOp.getFunctions().length);
 		assertEquals(new Gaussian(), myOp.getFunction(1));
@@ -474,9 +483,13 @@ public class OperatorTest {
 	@Test
 	public void testNoOfFunctions() {
 		IOperator myOp = new TestGenericBinaryOperator();
+		assertEquals("Hello", myOp.getName());
+		assertEquals("World", myOp.getDescription());
 
 		myOp.addFunction(new Gaussian());
 		myOp.addFunction(new Gaussian());
+		assertEquals("Hello", myOp.getName());
+		assertEquals("World", myOp.getDescription());
 		assertEquals(2, myOp.getNoOfFunctions());
 		myOp.setFunction(0, null); // functions can be set to null(!)
 		assertEquals(1, myOp.getNoOfFunctions());
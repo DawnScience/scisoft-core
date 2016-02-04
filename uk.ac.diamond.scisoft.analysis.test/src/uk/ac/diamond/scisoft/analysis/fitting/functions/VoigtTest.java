12c57a8ae0849b6dd5803a3694438c6d3184c11d
Peter Chang <peter.chang@diamond.ac.uk>
2016-02-04 13:00:06 +0000

    Add Voigt function and tests

---
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/fitting/functions/VoigtTest.java
@@ -0,0 +1,68 @@
+/*-
+ * Copyright (c) 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.fitting.functions;
+
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import uk.ac.diamond.scisoft.analysis.TestUtils;
+
+public class VoigtTest {
+
+	private static final double ABS_TOL = 1e-7;
+	private static final double REL_TOL = 1e-15;
+
+	@Test
+	public void testFunction() {
+		Voigt f = new Voigt();
+		Assert.assertEquals(4, f.getNoOfParameters());
+		f.setParameterValues(23., 2., 1.2, 2.3);
+		Assert.assertArrayEquals(new double[] {23., 2., 1.2, 2.3}, f.getParameterValues(), ABS_TOL);
+
+		double h = f.getHeight();
+		Assert.assertEquals(h, f.val(23.), ABS_TOL);
+
+		// check half-maximum intersections
+		h *= 0.5;
+		double dx = 1.77;
+		Assert.assertTrue(f.val(23. - dx - 0.01) < h && f.val(23. - dx) > h);
+		Assert.assertTrue(f.val(23. + dx) > h && f.val(23. + dx + 0.01) < h);
+
+		// check area
+		Dataset x = DatasetUtils.linSpace(-20+23, 20+23, 401, Dataset.FLOAT64);
+		Dataset v = DatasetUtils.convertToDataset(f.calculateValues(x));
+		double s = ((Number) v.sum()).doubleValue() * Math.abs(x.getDouble(0) - x.getDouble(1));
+		Assert.assertEquals(1.2, s, 1e-1);
+	}
+
+	@Test
+	public void testExtremes() {
+		Dataset x = DatasetUtils.linSpace(-20+23, 20+23, 401, Dataset.FLOAT64);
+
+		Voigt v = new Voigt();
+		v.setParameterValues(23., 2., 1.2, 0);
+		Dataset pl = DatasetUtils.convertToDataset(v.calculateValues(x));
+
+		v.setParameterValues(23., 0, 1.2, 2.3);
+		Dataset pg = DatasetUtils.convertToDataset(v.calculateValues(x));
+
+		Lorentzian lf = new Lorentzian();
+		lf.setParameterValues(23., 2., 1.2);
+		Dataset l = DatasetUtils.convertToDataset(lf.calculateValues(x));
+		TestUtils.assertDatasetEquals(l, pl, REL_TOL, ABS_TOL);
+
+		Gaussian gf = new Gaussian();
+		gf.setParameterValues(23., 2.3, 1.2);
+		Dataset g = DatasetUtils.convertToDataset(gf.calculateValues(x));
+		TestUtils.assertDatasetEquals(g, pg, REL_TOL, ABS_TOL);
+	}
+}
---
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/fitting/functions/Voigt.java
@@ -0,0 +1,126 @@
+/*-
+ * Copyright (c) 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.fitting.functions;
+
+import org.apache.commons.math3.complex.Complex;
+import org.eclipse.dawnsci.analysis.api.fitting.functions.IParameter;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+
+import uk.ac.diamond.scisoft.analysis.utils.Faddeeva;
+
+
+/**
+ * Class for a Voigt profile which is a Lorentzian convolved with a Gaussian
+ */
+public class Voigt extends APeak {
+	private static final String NAME = "Voigt";
+	private static final String DESC = "A Voigt profile which is a Lorentzian convolved with a Gaussian function."
+			+ "\n    y(x) = Convolve(Lorentzian(x; posn, l_fwhm, area), Gaussian(x; 0, g_fwhm, 1)"
+			+ "\nwhere Lorentzian(x; posn, w, area) = (w*area/(2*pi)) / (w^2/4 + (x-posn)^2),"
+			+ "\nposn is the resonant position, w is the Lorentzian full width.";
+
+	private static final String[] LOCAL_PARAM_NAMES = new String[] { "posn", "l_fwhm", "area", "g_fwhm" };
+	private static final double[] PARAMS = new double[] {0, 1, 1, 1};
+
+	private static final int FWHMG = AREA + 1;
+	public Voigt() {
+		this(PARAMS);
+	}
+
+	/**
+	 * Constructor which takes the five parameters required, which are
+	 * 
+	 * <pre>
+	 *     posn - resonant position
+	 *     l_fwhm - Lorentzian width
+	 *     area - Lorentzian area
+	 *     g_fwhm - Gaussian width
+	 * </pre>
+	 * 
+	 * @param params
+	 */
+	public Voigt(double... params) {
+		super(PARAMS.length);
+		if (params.length != PARAMS.length) {
+			throw new IllegalArgumentException("A Fano-Gaussian function requires 5 parameters, and it has been given " + params.length);
+		}
+
+		setParameterValues(params);
+		getParameter(FWHM).setLowerLimit(0.0);
+		getParameter(FWHMG).setLowerLimit(0.0);
+	}
+
+	public Voigt(IParameter... params) {
+		super(PARAMS.length);
+		if (params.length != PARAMS.length) { 
+			throw new IllegalArgumentException("A Fano-Gaussian function requires 5 parameters, and it has been given " + params.length);
+		}
+
+		setParameters(params);
+		getParameter(FWHM).setLowerLimit(0.0);
+		getParameter(FWHMG).setLowerLimit(0.0);
+	}
+
+	@Override
+	protected void setNames() {
+		setNames(NAME, DESC, LOCAL_PARAM_NAMES);
+	}
+
+
+	private static final double CONST = Math.sqrt(8*Math.log(2.));
+
+	private transient double r, ft, fr, zi;
+
+	@Override
+	protected void calcCachedParameters() {
+		r = getParameterValue(POSN);
+		double l = getParameterValue(FWHM) / 2.;
+		double sigma = getParameterValue(FWHMG) / CONST;
+		if (sigma < 5*Double.MIN_NORMAL) { // fix Lorentzian limit
+			sigma = 10*Double.MIN_NORMAL;
+		}
+		fr = Math.sqrt(0.5)/sigma;
+		zi = fr * l;
+		ft = fr*getParameterValue(AREA)/Math.sqrt(Math.PI);
+		height = ft*Faddeeva.erfcx(zi);
+		setDirty(false);
+	}
+
+	@Override
+	public double val(double... values) {
+		if (isDirty()) {
+			calcCachedParameters();
+		}
+
+		Complex z = new Complex(fr*(values[0] - r), zi);
+		Complex w = Faddeeva.w(z, 0);
+
+		double result = ft*w.getReal();
+		return result;
+	}
+
+	@Override
+	public void fillWithValues(DoubleDataset data, CoordinatesIterator it) {
+		if (isDirty()) {
+			calcCachedParameters();
+		}
+
+		it.reset();
+		double[] coords = it.getCoordinates();
+		int i = 0;
+		double[] buffer = data.getData();
+		while (it.hasNext()) {
+			Complex z = new Complex(fr*(coords[0] - r), zi);
+			Complex w = Faddeeva.w(z, 0);
+
+			buffer[i++] = ft*w.getReal();
+		}
+	}
+}

cf883149f131aab79e87c0ff68d69638e04ecbc9
Tim Spain <timothy.spain@diamond.ac.uk>
2016-02-05 15:54:47 +0000

    Add error propagating static methods that operate on a Dataset, and return the errors attached to the ouptut Dataset

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ErrorPropagationUtils.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ErrorPropagationUtils.java
@@ -12,6 +12,7 @@ package uk.ac.diamond.scisoft.analysis.processing.operations;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.IndexIterator;
 
 public class ErrorPropagationUtils {
 
@@ -33,6 +34,73 @@ public class ErrorPropagationUtils {
 		return new DoubleDataset[]{out,oute};
 	}
 	
+
+	public static DoubleDataset addWithUncertainty(Dataset a, double b) {
+		return operateWithUncertainty(a, b, new AddOp());
+	}
+	public static DoubleDataset subtractWithUncertainty(Dataset a, double b) {
+		return operateWithUncertainty(a, b, new SubtractOp());
+	}
+	public static DoubleDataset multiplyWithUncertainty(Dataset a, double b) {
+		return operateWithUncertainty(a, b, new MultiplyOp());
+	}
+	public static DoubleDataset divideWithUncertainty(Dataset a, double b) {
+		return operateWithUncertainty(a, b, new DivideOp());
+	}
 	
-	
+	private static DoubleDataset operateWithUncertainty(Dataset input, double oprahend, UncertaintyOp uncertaintyOp) {
+		DoubleDataset inputUncert;
+		DoubleDataset output, outputUncert;
+		
+		inputUncert = (input.getError() instanceof DoubleDataset) ? (DoubleDataset) input.getError() : null;
+		output = (DoubleDataset) DatasetFactory.zeros(input, Dataset.FLOAT64);
+		outputUncert = (inputUncert == null) ? null : (DoubleDataset) DatasetFactory.zeros(inputUncert, Dataset.FLOAT64);
+		
+		IndexIterator iter = input.getIterator();
+		
+		if (outputUncert != null) {
+			while(iter.hasNext()) {
+				output.setAbs(iter.index, uncertaintyOp.erator(input.getElementDoubleAbs(iter.index), oprahend));
+				outputUncert.setAbs(iter.index, uncertaintyOp.erator(input.getElementDoubleAbs(iter.index), oprahend));
+			}
+			output.setError(outputUncert);
+		} else {
+			while (iter.hasNext())
+				output.setAbs(iter.index, uncertaintyOp.erator(input.getElementDoubleAbs(iter.index), oprahend));
+		}
+		return output;
+	}
+
+}
+
+interface UncertaintyOp {
+	double erator(double a, double b);
+}
+
+class AddOp implements UncertaintyOp {
+	@Override
+	public double erator(double a, double b) {
+		return a+b;
+	}
+}
+
+class SubtractOp implements UncertaintyOp {
+	@Override
+	public double erator(double a, double b) {
+		return a-b;
+	}
+}
+
+class MultiplyOp implements UncertaintyOp {
+	@Override
+	public double erator(double a, double b) {
+		return a*b;
+	}
+}
+
+class DivideOp implements UncertaintyOp {
+	@Override
+	public double erator(double a, double b) {
+		return a/b;
+	}
 }

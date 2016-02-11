d3e3a946d08052cc14c61c3b43589f8ab5ca2817
Tim Spain <timothy.spain@diamond.ac.uk>
2016-02-11 10:51:14 +0000

    Add a unique divide by external data Operation and corresponding class.

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/externaldata/DivideExternalDataOperation.java
@@ -0,0 +1,30 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations.externaldata;
+
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+
+import uk.ac.diamond.scisoft.analysis.processing.operations.ErrorPropagationUtils;
+
+public class DivideExternalDataOperation extends
+		OperateOnExternalDataAbstractOperation<ExternalDataModel, OperationData> {
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.externaldata.DivideExternalDataOperation";
+	}
+
+	@Override
+	protected Dataset doMathematics(Dataset a, double b) {
+		return ErrorPropagationUtils.divideWithUncertainty(a, b);
+	}
+
+}

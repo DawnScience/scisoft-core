3c8dcda9e2ac4f64ec6985879f764411f318a62e
Tim Spain <timothy.spain@diamond.ac.uk>
2016-02-22 13:12:45 +0000

    Add 'Add' and 'Clear' Actions to the XPDF unit cell editing dialog.

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/UnitCellDialog.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/views/UnitCellDialog.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -60,6 +63,7 @@ class UnitCellDialog extends Dialog {
 		cellTable.setInput(atoms);
 
 		createAtomButtons(container);
+		createAtomActions();
 		
 		return container;
 	}
@@ -101,6 +105,12 @@ class UnitCellDialog extends Dialog {
 		addAtomButton.setText("Add");
 		addAtomButton.setToolTipText("Add a new atom to the unit cell");
 		addAtomButton.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 1, 1));
+		addAtomButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				addAtomAction.run();
+			}
+		});
 		copyAtomButton = new Button(parent, SWT.PUSH);
 		copyAtomButton.setText("Copy");
 		copyAtomButton.setToolTipText("Copy the selected atom");
@@ -113,12 +123,39 @@ class UnitCellDialog extends Dialog {
 		clearAtomsButton.setText("Clear");
 		clearAtomsButton.setToolTipText("Clear all atoms");
 		clearAtomsButton.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 1, 1));
+		clearAtomsButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				clearAtomsAction.run();
+			}
+		});
 	}
 
-//	private void createAtomActions() {
-//		addAtomAction = new Action() {
-//			
-//		};
-//	}
+	private void createAtomActions() {
+		addAtomAction = new Action() {
+			@Override
+			public void run() {
+				int i;
+				for (i = 0; atoms.containsKey(Integer.toString(i)); i++)
+					;
+				atoms.put(Integer.toString(i), new XPDFAtom(0));
+				cellTable.refresh();
+			}
+		};
+		addAtomAction.setText("Add");
+		addAtomAction.setToolTipText("Add a new atom to the unit cell");
+		
+		clearAtomsAction = new Action() {
+			@Override
+			public void run() {
+				atoms.clear();
+				cellTable.refresh();
+				
+			}
+		};
+		clearAtomsAction.setText("Clear");
+		clearAtomsAction.setToolTipText("Clear all atoms");
+			
+	}
 	
 }

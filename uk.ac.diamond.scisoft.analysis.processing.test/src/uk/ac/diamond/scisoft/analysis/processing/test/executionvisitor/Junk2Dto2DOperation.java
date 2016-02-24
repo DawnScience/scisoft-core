279d401d4d33a1ab7ff7f46ea312834577f0cc50
Peter Chang <peter.chang@diamond.ac.uk>
2016-02-24 14:00:52 +0000

    Remove unused import

--- uk.ac.diamond.scisoft.analysis.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/test/executionvisitor/Junk2Dto2DOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/test/executionvisitor/Junk2Dto2DOperation.java
@@ -8,7 +8,6 @@ import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
-import org.eclipse.dawnsci.analysis.dataset.impl.Random;
 import org.eclipse.dawnsci.analysis.dataset.metadata.AxesMetadataImpl;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 

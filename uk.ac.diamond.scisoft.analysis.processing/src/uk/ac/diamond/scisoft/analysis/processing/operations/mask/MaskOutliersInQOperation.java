e67515a5ce481ece4a33daffce1dc6caa5ee5c47
Jacob Filik <jacob.filik@diamond.ac.uk>
2016-04-29 17:14:42 +0100

    Initial commit of masking outliers in Q

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/mask/MaskOutliersInQOperation.java
@@ -0,0 +1,74 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations.mask;
+
+import java.util.List;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.api.processing.model.EmptyModel;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
+import org.eclipse.dawnsci.analysis.dataset.metadata.MaskMetadataImpl;
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
+
+import uk.ac.diamond.scisoft.analysis.dataset.function.Histogram;
+import uk.ac.diamond.scisoft.analysis.diffraction.QSpace;
+import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegration;
+import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegrationBean;
+import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegrationCache;
+import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegrationUtils;
+import uk.ac.diamond.scisoft.analysis.io.DiffractionMetadata;
+import uk.ac.diamond.scisoft.analysis.processing.operations.powder.AzimuthalPixelIntegrationModel;
+import uk.ac.diamond.scisoft.analysis.roi.XAxis;
+
+public class MaskOutliersInQOperation extends AbstractOperation<EmptyModel, OperationData> {
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.mask.MaskOutliersInQOperation";
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		
+		DiffractionMetadata meta = input.getFirstMetadata(DiffractionMetadata.class);
+		
+		if (meta == null) throw new OperationException(this, "Does not contain calibration information!");
+		
+		PixelIntegrationBean bean = new PixelIntegrationBean();
+		bean.setUsePixelSplitting(false);
+		bean.setxAxis(XAxis.Q);
+		bean.setAzimuthalIntegration(true);
+		bean.setTo1D(true);
+		bean.setShape(input.getShape());
+		PixelIntegrationCache lcache = new PixelIntegrationCache(meta, bean);
+		
+		Dataset calculateOutlierMask = PixelIntegration.calculateOutlierMask(DatasetUtils.convertToDataset(input), null, lcache);
+		
+		input.setMetadata(new MaskMetadataImpl(calculateOutlierMask));
+		
+		return new OperationData(input);
+	}
+
+}

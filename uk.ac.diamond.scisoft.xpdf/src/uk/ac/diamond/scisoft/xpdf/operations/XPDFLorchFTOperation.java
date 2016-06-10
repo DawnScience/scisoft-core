7a5d4ccbda63ceecd4e5bce7f6c7706921124ca7
Peter Chang <peter.chang@diamond.ac.uk>
2016-06-10 17:51:26 +0100

    Handle new lazy interface exceptions

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/XPDFLorchFTOperation.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/XPDFLorchFTOperation.java
@@ -12,6 +12,7 @@ package uk.ac.diamond.scisoft.xpdf.operations;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.dawnsci.analysis.api.dataset.DatasetException;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.Atomic;

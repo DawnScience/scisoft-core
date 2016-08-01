6f3ebd912e6b4e22536ae0a3d74981d440d253df
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2016-08-01 13:01:05 +0100

    One polynomial fitting added with model.

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/reflectivityandsxrd/BoxSlicer1DModel.java
@@ -0,0 +1,65 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations.reflectivityandsxrd;
+
+import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
+import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
+
+import uk.ac.diamond.scisoft.analysis.processing.operations.roiprofile.BoxIntegration.Direction;
+
+public class BoxSlicer1DModel extends AbstractOperationModel {
+	
+	
+	@OperationModelField(label="Boundary Box Direction", hint = "X or Y direction?" )
+	private Direction direction = Direction.Y;
+	
+	@OperationModelField(label="Boundary Box size (pixels)", hint = "Size of the boundary box used for background calculation" )
+	private int boundaryBox = 20;
+
+	private RectangularROI box = new RectangularROI(100d, 100d, 10d, 10d, 0d);
+	
+	public int getBoundaryBox() {
+		return boundaryBox;
+	}
+
+	
+	public Direction getDirection() {
+		return direction;
+	}
+	
+	public void setBoundaryBox(int boundaryBox) {
+		firePropertyChange("boundaryBox", this.boundaryBox, this.boundaryBox = boundaryBox);
+	}
+
+	public RectangularROI getBox() {
+		return box;
+	}
+	
+	public void setDirection(Direction direction) {
+		firePropertyChange("direction", this.direction, this.direction = direction);
+	}
+	
+	public void setBox(RectangularROI box) {
+		firePropertyChange("box", this.box, this.box = box);
+	}
+	
+	@OperationModelField(label="Fit power", hint = "Fit power" )
+	private int fitPower = 2;
+	
+	public int getFitPower() {
+		return fitPower;
+	}
+
+	public void setFitPower(int fitPower) {
+		firePropertyChange("fitPower", this.fitPower, this.fitPower= fitPower);
+	}
+}
+//TEST
---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/reflectivityandsxrd/OneDPolynomialBackgroundFitAndSubtract.java
@@ -0,0 +1,94 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations.reflectivityandsxrd;
+
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
+import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
+import org.eclipse.january.IMonitor;
+import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetFactory;
+import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.IndexIterator;
+import org.eclipse.january.dataset.Maths;
+
+import uk.ac.diamond.scisoft.analysis.processing.surfacescattering.BackgroundSetting;
+
+public class OneDPolynomialBackgroundFitAndSubtract extends AbstractOperation<BoxSlicer1DModel, OperationData> {
+
+	
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.reflectivityandsxrd.OneDPolynomialBackgroundFitAndSubtract";
+	}
+		
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO ;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO ;
+	}
+		
+	@Override
+	protected OperationData process(IDataset input, IMonitor monitor) {
+		
+		RectangularROI box = model.getBox();
+		
+		Dataset in1 = BoxSlicerRodScanUtils.rOIBox(input, monitor, box.getIntLengths(), box.getIntPoint());
+		
+		IDataset[] background = new IDataset[2];
+				
+		background[0] = BoxSlicerRodScanUtils.iBelowOrRightBox(input, monitor, box.getIntLengths(), box.getIntPoint()
+													, model.getBoundaryBox(), model.getDirection());
+		
+		background[1] = BoxSlicerRodScanUtils.iAboveOrLeftBox(input, monitor, box.getIntLengths(), box.getIntPoint()
+				, model.getBoundaryBox(), model.getDirection());
+		
+		
+		Dataset in1Background = DatasetFactory.zeros(in1.getShape(), Dataset.FLOAT64);
+		
+		in1Background = BackgroundSetting.rOIBackground1(background, in1Background
+				, box.getIntLengths(), box.getIntPoint()
+				, model.getBoundaryBox(), model.getFitPower()
+				, model.getDirection());
+		
+		IndexIterator it = in1Background.getIterator();
+		
+		while (it.hasNext()) {
+			double v = in1Background.getElementDoubleAbs(it.index);
+			if (v < 0) in1Background.setObjectAbs(it.index, 0);
+		}
+
+		Dataset pBackgroundSubtracted = Maths.subtract(in1, in1Background, null);
+
+		pBackgroundSubtracted.setName("Region of Interest, polynomial background removed");
+
+
+		
+		IndexIterator it1 = pBackgroundSubtracted.getIterator();
+		
+		while (it1.hasNext()) {
+			double q = pBackgroundSubtracted.getElementDoubleAbs(it1.index);
+			if (q < 0) pBackgroundSubtracted.setObjectAbs(it1.index, 0);
+		}
+		
+		
+		return new OperationData(pBackgroundSubtracted);
+
+	}
+	
+}
+//
+

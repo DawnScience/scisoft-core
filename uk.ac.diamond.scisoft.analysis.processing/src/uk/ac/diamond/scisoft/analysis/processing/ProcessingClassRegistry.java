619843afef4d84a9c5aaa6746c6e5bc3962b1271
Matthew Gerring <matthew.gerring@gmail.com>
2016-11-11 12:46:12 +0000

    Class registry changes to provide OperationBean

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/ProcessingClassRegistry.java
@@ -0,0 +1,32 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.dawnsci.analysis.api.persistence.IClassRegistry;
+
+import uk.ac.diamond.scisoft.analysis.processing.bean.OperationBean;
+
+public class ProcessingClassRegistry implements IClassRegistry {
+
+	private Map<String, Class<?>> idMap;
+	public ProcessingClassRegistry() {
+		idMap = new HashMap<String, Class<?>>();
+		idMap.put(OperationBean.class.getSimpleName(), OperationBean.class);
+	}
+
+	@Override
+	public Map<String, Class<?>> getIdToClassMap() {
+		return idMap;
+	}
+
+}

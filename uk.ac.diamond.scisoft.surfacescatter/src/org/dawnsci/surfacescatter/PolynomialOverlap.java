5f05dc27a5cab7b653f079db01a9e49061a24ad4
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2016-11-28 23:10:25 +0000

    Polynomial interpolation for tracking

---
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/PolynomialOverlap.java
@@ -0,0 +1,77 @@
+package org.dawnsci.surfacescatter;
+
+import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetFactory;
+import org.eclipse.january.dataset.Maths;
+
+import uk.ac.diamond.scisoft.analysis.fitting.Fitter;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Polynomial;
+
+public class PolynomialOverlap {
+
+	
+	public static double correctionRatio(Dataset[] xLowerDataset, Dataset yLowerDataset,
+			Dataset[] xHigherDataset, Dataset yHigherDataset, double attenuationFactor) {
+	
+	
+		Polynomial polyFitLower = Fitter.polyFit(xLowerDataset, yLowerDataset, 1e-5,4);
+		Polynomial polyFitHigher = Fitter.polyFit(xHigherDataset, yHigherDataset, 1e-5,4);
+		
+		Dataset calculatedValuesHigher = polyFitHigher.calculateValues(xLowerDataset);
+		Dataset calculatedValuesLower = polyFitLower.calculateValues(xLowerDataset);
+		
+		Dataset correctionsRatioDataset = Maths.divide(calculatedValuesLower.sum(), 
+				calculatedValuesHigher.sum());
+		
+		
+		double correction = ((double) correctionsRatioDataset.sum())/((double) correctionsRatioDataset.getSize())*attenuationFactor;
+		
+		
+		return correction;
+	}
+	
+	
+	public static double[] extrapolatedLocation(double desiredl, 
+												Dataset lValues, 
+												Dataset xValues,
+												Dataset yValues,
+												int[] len,
+												int fitPower){
+		
+		
+		Dataset[] lValuesHolder = new Dataset[1];
+		Dataset lValuesNullHolder = DatasetFactory.zeros(lValues, Dataset.ARRAYFLOAT64);
+		Dataset[] lValuesHolder2 = new Dataset[1];
+		Dataset lValuesNullHolder2 = DatasetFactory.zeros(lValues, Dataset.ARRAYFLOAT64);
+		
+		
+		for(int t = 0; t<lValues.getSize(); t++){
+			lValuesNullHolder.set(lValues.getDouble(t), lValues.getSize()-1-t);
+		}
+		
+		lValuesHolder[0] = lValuesNullHolder;
+		lValuesHolder2[0] = lValues;
+		
+		
+		Polynomial polyFitYValues = Fitter.polyFit(lValuesHolder, yValues, 1e-30,fitPower);
+		Polynomial polyFitXValues = Fitter.polyFit(lValuesHolder2, xValues, 1e-30,fitPower);
+		
+		Dataset desiredlDat = DatasetFactory.zeros(1);
+		desiredlDat.set(desiredl, 0);
+		
+		Dataset yValueDat = polyFitYValues.calculateValues(desiredlDat);
+		Dataset xValueDat = polyFitXValues.calculateValues(desiredlDat);
+		
+		
+		double yValue = yValueDat.getDouble(0);
+		double xValue = xValueDat.getDouble(0);
+		
+		double[] output = new double[] {xValue, yValue,
+									    (xValue + len[1]), yValue,
+										xValue, (yValue +len[0]),
+										(xValue + len[1]), (yValue +len[0])}; 
+		
+		return output;
+	}
+		
+}

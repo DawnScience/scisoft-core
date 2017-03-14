97d78582d146f905cc179be512c12c8b96d5a71e
Tim Snow <tim.snow@diamond.ac.uk>
2017-03-14 11:15:38 +0000

    Changing path to glassy carbon file from absolute to relative

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/saxs/AbsoluteIntensityCalibrationOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/saxs/AbsoluteIntensityCalibrationOperation.java
@@ -114,7 +114,7 @@ public class AbsoluteIntensityCalibrationOperation extends AbstractOperation<Abs
 		try {
 			if (model.getUseInternalCalibrant() == true) {
 				// By opening the default file
-				URL calibrantFileURL = this.getClass().getResource("/data/GlassyCarbon_T.dat");
+				URL calibrantFileURL = this.getClass().getResource("data/GlassyCarbon_T.dat");
 				DatLoader calibrantDataLoader = new DatLoader(FileLocator.resolve(calibrantFileURL).getPath());
 				DataHolder calibrantData = calibrantDataLoader.loadFile();
 				// and extracting out the relevant datasets

a889747c98c52948d1bf4442cdf56907895f6166
Tim Spain <timothy.spain@diamond.ac.uk>
2017-08-18 08:52:37 +0100

    Add a metadata test for XRMC files.

--- uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/io/XRMCLoaderTest.java
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/io/XRMCLoaderTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.fail;
 
 import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
 import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.metadata.IMetadata;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,6 +28,9 @@ public class XRMCLoaderTest {
 		loader = new XRMCLoader(testFileName);
 	}
 
+	// expected array sizes
+	final int nOrder = 2, nEnergy = 1, nX = 40, nY = 40;  
+	
 	@Test
 	public void testLoadFile() {
 		
@@ -40,8 +44,36 @@ public class XRMCLoaderTest {
 		}
 		
 		IDataset data = dh.getDataset(0);
-		assertArrayEquals("Error in array shape", new int[]{2,  1, 40, 40}, data.getShape());
+		assertArrayEquals("Error in array shape", new int[]{nOrder, nEnergy, nX, nY}, data.getShape());
 		assertEquals("Maximum values differ", 720000., (double) data.max(), 10000.);
+	}
+	
+	@Test
+	public void testMetadata() {
+		IDataHolder dh;
+		try {
+			dh = loader.loadFile();
+		} catch (Exception e) {
+			fail("Exception loading file:" + e.toString());
+			return;
+		}
+		IMetadata meta = dh.getMetadata();
+		try {
+			assertEquals("Error in metadata", (int) (double) meta.getMetaValue("nOrder"), nOrder);
+			assertEquals("Error in metadata", (int) (double) meta.getMetaValue("nColumns"), nX);
+			assertEquals("Error in metadata", (int) (double) meta.getMetaValue("nRows"), nY);
+			assertEquals("Error in metadata", (double) meta.getMetaValue("pixelSizeX"), 1, 1e-12); // pixels are 1 cm × 1 cm
+			assertEquals("Error in metadata", (double) meta.getMetaValue("pixelSizeY"), 1, 1e-12);
+			assertEquals("Error in metadata", (double) meta.getMetaValue("exposureTime"), 1.0, 1e-12);
+			assertEquals("Error in metadata", (int) (double) meta.getMetaValue("nEnergy"), 1);
+			assertEquals("Error in metadata", (double) meta.getMetaValue("minEnergy"), 0., 1e-12);
+			assertEquals("Error in metadata", (double) meta.getMetaValue("maxEnergy"), 80., 1e-12);
+
+
+		} catch(Exception e) {
+			fail("Exception reading metadata: " + e.toString());
+		}
+	
 	}
 
 }

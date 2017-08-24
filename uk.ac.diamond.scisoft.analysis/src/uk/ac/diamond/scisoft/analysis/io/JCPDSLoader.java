ced139206297701ddbd173044d533a21f8cc89bc
Tim Spain <timothy.spain@diamond.ac.uk>
2017-08-24 17:23:14 +0100

    Calculate unit cell volume, and convert strings to enums

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/JCPDSLoader.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/io/JCPDSLoader.java
@@ -25,37 +25,62 @@ import java.util.Set;
 import org.eclipse.dawnsci.analysis.api.io.ScanFileHolderException;
 import org.eclipse.january.dataset.DatasetFactory;
 
+import uk.ac.diamond.scisoft.analysis.crystallography.UnitCell;
+
 public class JCPDSLoader extends AbstractFileLoader {
 
 	protected Map<String,String> metadataMap;
-	Set<String> allowedKeywords = new HashSet<String>(Arrays.asList(
-			"COMMENT",
-			"K0",
-			"K0P",
-			"DK0DT",
-			"DK0PDT",
-			"SYMMETRY",
-			"A",
-			"B",
-			"C",
-			"ALPHA",
-			"BETA",
-			"GAMMA",
-			"VOLUME",
-			"ALPHAT",
-			"DALPHADT",
-			"DIHKL"
-			));
+
+	public enum Keyword {
+		COMMENT("COMMENT"), K0("K0"), K0P("K0P"), DK0DT("DK0DT"), DK0PDT("DK0PDT"), SYMMETRY("SYMMETRY"),
+		A("A"), B("B"), C("C"), ALPHA("ALPHA"), BETA("BETA"), GAMMA("GAMMA"),
+		VOLUME("VOLUME"), ALPHAT("ALPHAT"), DALPHADT("DALPHADT"), DIHKL("DIHKL");
+		
+		
+		private String name;
+		
+		private Keyword(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+	}
+	private String[] pluckNames(Keyword[] keywords) {
+		ArrayList<String> names = new ArrayList<>();
+		for(Keyword keyword: keywords) {
+			names.add(keyword.getName());
+		}
+		return names.toArray(new String[names.size()]);
+	}
+	Set<String> allowedKeywords = new HashSet<>(Arrays.asList(pluckNames(Keyword.values())));
 	
-	Set<String> symmetries = new HashSet<String>(Arrays.asList(
-			"CUBIC",
-			"TETRAGONAL",
-			"HEXAGONAL",
-			"RHOMBOHEDRAL",
-			"ORTHORHOMBIC",
-			"MONOCLINIC",
-			"TRICLINIC"
-			));
+	
+	public enum Symmetry {
+		CUBIC("CUBIC"), TETRAGONAL("TERAGONAL"), HEXAGONAL("HEXAGONAL"), RHOMBOHEDRAL("RHOMBOHEDRAL"),
+		ORTHORHOMBIC("ORTHORHOMBIC"), MONOCLINIC("MONOCLINIC"), TRICILINIC("TRICLINIC");
+		
+		private String name;
+		
+		private Symmetry(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+	}
+	
+	private String[] pluckNames(Symmetry[] symmetries) {
+		ArrayList<String> names = new ArrayList<>();
+		for(Symmetry symmetry: symmetries) {
+			names.add(symmetry.getName());
+		}
+		return names.toArray(new String[names.size()]);
+	}
+	
+	Set<String> symmetries = new HashSet<>(Arrays.asList(pluckNames(Symmetry.values())));
 
 	// Data lists
 	List<Double> d, i, h, k, l;
@@ -111,7 +136,7 @@ public class JCPDSLoader extends AbstractFileLoader {
 				// check the keyword is in the allowed set
 				if (!allowedKeywords.contains(keyword)) continue;
 				String restOfLine = jLine.replace(keyword+":", "").trim();
-				if (!keyword.equals("DIHKL")) {
+				if (!keyword.equals(Keyword.DIHKL.getName())) {
 					metadataMap.put(keyword, restOfLine);
 				} else {
 					// DIHKL Data
@@ -141,6 +166,63 @@ public class JCPDSLoader extends AbstractFileLoader {
 			result.setDataset("h", DatasetFactory.createFromList(h));
 			result.setDataset("k", DatasetFactory.createFromList(k));
 			result.setDataset("l", DatasetFactory.createFromList(l));
+			
+			// unit cell volume
+			// This is one of the more useful pieces of metadata, and may be
+			// encoded directly. If it is not, however, then it can be 
+			// calculated from the encoded symmetry and unit cell parameters
+			if (!metadataMap.containsKey(Keyword.VOLUME.getName())) {
+				String symString = metadataMap.get(Keyword.SYMMETRY.getName());
+				UnitCell unitCell;
+				double a, b, c, alpha, beta, gamma;
+				switch (symString) {
+				case("CUBIC"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					unitCell = new UnitCell(new double[] {a, a, a}, new double[] {90, 90, 90});
+					break;
+				case("TETRAGONAL"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					c = Double.parseDouble(metadataMap.get(Keyword.C.getName()));
+					unitCell = new UnitCell(new double[] {a, a, c}, new double[] {90, 90, 90});
+					break;
+				case("HEXAGONAL"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					c = Double.parseDouble(metadataMap.get(Keyword.C.getName()));
+					unitCell = new UnitCell(new double[] {a, a, c}, new double[] {90, 90, 120});
+					break;
+				case("RHOMOHEDRAL"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					gamma = Double.parseDouble(metadataMap.get(Keyword.GAMMA.getName()));
+					unitCell = new UnitCell(new double[] {a, a, a}, new double[] {90, 90, gamma});
+					break;
+				case("ORTHORHOMBIC"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					b = Double.parseDouble(metadataMap.get(Keyword.B.getName()));
+					c = Double.parseDouble(metadataMap.get(Keyword.C.getName()));
+					unitCell = new UnitCell(new double[] {a, b, c}, new double[] {90, 90, 90});
+					break;
+				case("MONOCLINIC"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					b = Double.parseDouble(metadataMap.get(Keyword.B.getName()));
+					c = Double.parseDouble(metadataMap.get(Keyword.C.getName()));
+					beta = Double.parseDouble(metadataMap.get(Keyword.BETA.getName()));
+					unitCell = new UnitCell(new double[] {a, b, c}, new double[] {90, beta, 90});
+					break;
+				case("TRICLINIC"):
+					a = Double.parseDouble(metadataMap.get(Keyword.A.getName()));
+					b = Double.parseDouble(metadataMap.get(Keyword.B.getName()));
+					c = Double.parseDouble(metadataMap.get(Keyword.C.getName()));
+					alpha = Double.parseDouble(metadataMap.get(Keyword.ALPHA.getName()));
+					beta = Double.parseDouble(metadataMap.get(Keyword.BETA.getName()));
+					gamma = Double.parseDouble(metadataMap.get(Keyword.GAMMA.getName()));
+					unitCell = new UnitCell(new double[] {a, b, c}, new double[] {alpha, beta, gamma});
+					break;
+				default:
+					unitCell = new UnitCell(1.0);
+				}
+				
+				metadataMap.put("VOLUME", Double.toString(unitCell.volume()));
+			}
 			
 			// metadata
 			metadata = new ExtendedMetadata(new File(fileName));

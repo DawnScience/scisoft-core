40e87ca3610644484af66212005765d9ac5f329c
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-10-03 19:41:12 +0100

    Batch UI work

---
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/BatchRodDataTransferObject.java
@@ -0,0 +1,45 @@
+package org.dawnsci.surfacescatter;
+
+public class BatchRodDataTransferObject {
+	
+	private String[] datFiles;
+	private String imageFolderPath; 
+	private String paramFiles;
+	private String nexusSaveFilePaths;
+	private String rodName;
+	
+	
+	
+	public String[] getDatFiles() {
+		return datFiles;
+	}
+	public void setDatFiles(String[] datFiles) {
+		this.datFiles = datFiles;
+	}
+	public String getImageFolderPath() {
+		return imageFolderPath;
+	}
+	public void setImageFolderPath(String imageFolderPath) {
+		this.imageFolderPath = imageFolderPath;
+	}
+	public String getParamFiles() {
+		return paramFiles;
+	}
+	public void setParamFiles(String paramFiles) {
+		this.paramFiles = paramFiles;
+	}
+	public String getNexusSaveFilePaths() {
+		return nexusSaveFilePaths;
+	}
+	public void setNexusSaveFilePaths(String nexusSaveFilePaths) {
+		this.nexusSaveFilePaths = nexusSaveFilePaths;
+	}
+	public String getRodName() {
+		return rodName;
+	}
+	public void setRodName(String rodName) {
+		this.rodName = rodName;
+	}
+	
+
+}
--- uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/MethodSettingEnum.java
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/MethodSettingEnum.java
@@ -41,43 +41,10 @@ public class MethodSettingEnum {
 					return xA;
 				}
 			}
-			//			
-			//			
-			//			if (in.equals("SXRD")){
-			//				return MethodSetting.SXRD;
-			//			}
-			//			else if (in.equals("Reflectivity with Flux Correction")){
-			//				return MethodSetting.Reflectivity_with_Flux_Correction;
-			//			}
-			//			else if (in.equals("Reflectivity without Flux Correction")){
-			//				return MethodSetting.Reflectivity_without_Flux_Correction;
-			//			}
-			//			
-			//			else if (in.equals("NO Correction")){
-			//				return MethodSetting.Reflectivity_NO_Correction;
-			//			}
-			//			
+		
 			return null;
 		}
 
-		//		public static int toInt(MethodSetting in){
-		//
-		//			if (in.equals(MethodSetting.SXRD)){
-		//				return 0;
-		//			}
-		//			else if (in.equals(MethodSetting.Reflectivity_with_Flux_Correction)){
-		//				return 1;
-		//			}
-		//			else if (in.equals(MethodSetting.Reflectivity_without_Flux_Correction)){
-		//				return 2;
-		//			}
-		//
-		//			else if (in.equals(MethodSetting.Reflectivity_NO_Correction)){
-		//				return 3;
-		//			}
-		//
-		//			return (Integer) null;
-		//		}
 
 		public static MethodSetting toMethod(int in){
 
@@ -86,22 +53,7 @@ public class MethodSettingEnum {
 					return xA;
 				}
 			}
-			//			
-			//			
-			//			if (in == 0){
-			//				return MethodSetting.SXRD;
-			//			}
-			//			else if (in == 1){
-			//				return MethodSetting.Reflectivity_with_Flux_Correction;
-			//			}
-			//			else if (in == 2){
-			//				return MethodSetting.Reflectivity_without_Flux_Correction;
-			//			}
-			//
-			//			else if (in == 3){
-			//				return MethodSetting.Reflectivity_NO_Correction;
-			//			}
-
+		
 			return null;
 		}
 	}	

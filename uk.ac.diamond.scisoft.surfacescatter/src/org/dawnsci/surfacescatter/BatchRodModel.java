6933461592994097e0f06d9bc27d1ffff680a7ed
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-10-03 23:19:48 +0100

    Batch UI initial working support

--- uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/BatchRodModel.java
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/BatchRodModel.java
@@ -5,60 +5,91 @@ import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
 public class BatchRodModel {
-	
-	
+
 	private ArrayList<BatchRodDataTransferObject> brdtoList;
 	private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 	private boolean poke = true;
-	
-	
+	private boolean updateControl =true;
+	private String nxsFolderPath;
+
 	public ArrayList<BatchRodDataTransferObject> getBrdtoList() {
-		
-		if(brdtoList == null){
+
+		if (brdtoList == null) {
 			brdtoList = new ArrayList<>();
 		}
-		
+
 		return brdtoList;
 	}
 
 	public void setBrdtoList(ArrayList<BatchRodDataTransferObject> brdto) {
 		this.brdtoList = brdto;
-		firePropertyChange("brdtoArray", this.brdtoList,
-				this.brdtoList= brdto);
+		firePropertyChange("brdtoArray", this.brdtoList, this.brdtoList = brdto);
 	}
-	
-	public void addToBrdtoList(BatchRodDataTransferObject brdto){
-		if(brdtoList == null){
+
+	public void addToBrdtoList(BatchRodDataTransferObject brdto) {
+		if (brdtoList == null) {
 			brdtoList = new ArrayList<>();
 		}
-		
+
 		brdtoList.add(brdto);
-		
+
 		setPoke(!poke);
 	}
-	
+
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		propertyChangeSupport.addPropertyChangeListener(listener);
 	}
 
-	public void removePropertyChangeListener(String propertyName,
-			PropertyChangeListener listener) {
-		propertyChangeSupport.removePropertyChangeListener(propertyName,
-				listener);
+	public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+		propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
 	}
 
-	protected void firePropertyChange(String propertyName, Object oldValue,
-			Object newValue) {
-		propertyChangeSupport.firePropertyChange(propertyName, oldValue,
-				newValue);
+	protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+		propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
 	}
 
+	public void removeElementsNotInList(ArrayList<String> itemsToKeepList) {
 
+		updateControl =false;
+		
+		for (BatchRodDataTransferObject b : brdtoList) {
+			boolean notGood = true;
+
+			for (String s : itemsToKeepList) {
+
+				if (s.equals(b.getRodName())) {
+					notGood = false;
+				}
+			}
+			if (notGood) {
+				brdtoList.remove(b);
+			}
+		}
+		
+		updateControl =true;
+		setPoke(!poke);
+
+	}
 
 	public void setPoke(boolean poke) {
-		firePropertyChange("poke", this.poke,
-				this.poke= poke);
+		firePropertyChange("poke", this.poke, this.poke = poke);
 
 	}
-	
+
+	public boolean isUpdateControl() {
+		return updateControl;
+	}
+
+	public void setUpdateControl(boolean updateControl) {
+		this.updateControl = updateControl;
+	}
+
+	public String getNxsFolderPath() {
+		return nxsFolderPath;
+	}
+
+	public void setNxsFolderPath(String nxsFolderPath) {
+		this.nxsFolderPath = nxsFolderPath;
+	}
+
 }

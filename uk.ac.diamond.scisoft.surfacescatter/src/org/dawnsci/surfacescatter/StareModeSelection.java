1c96dc5012011326bb7bcb5f406720952f1ff580
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-10-26 18:41:49 +0100

    batch check improved

---
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/StareModeSelection.java
@@ -0,0 +1,16 @@
+package org.dawnsci.surfacescatter;
+
+public class StareModeSelection {
+
+	private boolean accept = true;
+
+	public boolean getAccept() {
+		return accept;
+	}
+
+	public void setAccept(boolean accept) {
+		this.accept = accept;
+	}
+	
+	
+}

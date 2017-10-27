ac2fae970f8c66f895df09c7f54a664de9e74426
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-10-27 14:29:26 +0100

    fixed listeners on background boxes. Support.

--- uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/PlotSystem2DataSetter.java
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/PlotSystem2DataSetter.java
@@ -5,18 +5,6 @@ import org.eclipse.dawnsci.analysis.api.roi.IRectangularROI;
 import org.eclipse.january.dataset.IDataset;
 
 public class PlotSystem2DataSetter {
-
-//    public static IDataset PlotSystem2DataSetter1(ExampleModel model) {
-//
-//    	IROI box = model.getROI();
-//		IRectangularROI roi = box.getBounds();
-//		int[] newLen = roi.getIntLengths();
-//		int[] newPt = roi.getIntPoint();
-//		int[][] newLenPt = {newLen, newPt};
-//		IDataset j = ImageSlicerUtils.ImageSliceUpdate(model.getSliderPos(), model.getDatImages(),newLenPt);
-//
-//		return j;
-//    }
     
     public static IDataset PlotSystem2DataSetter1(IROI box, IDataset input) {
 
@@ -24,8 +12,6 @@ public class PlotSystem2DataSetter {
 		int[] newLen = roi.getIntLengths();
 		int[] newPt = roi.getIntPoint();
 		int[][] newLenPt = {newLen, newPt};
-		IDataset j = ImageSlicerUtils.ImageSliceUpdate(input, newLenPt);
-
-		return j;
+		return ImageSlicerUtils.ImageSliceUpdate(input, newLenPt);
     }
 }

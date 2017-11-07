53b6ef6dab9adc880cf6d9e7f9959999e3733997
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-11-07 18:10:19 +0000

    Moved series panel.

--- uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/DisplayLabelStrings.java
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/DisplayLabelStrings.java
@@ -5,8 +5,13 @@ public class DisplayLabelStrings {
 	private static final String TURN_TRACKER_ON = "Turn Tracker On";
 	private static final String TURN_TRACKER_OFF = "Turn Tracker Off";
 	private static final String BG_SUBTRACTED_IMAGE = "Bg Subtracted Image";
+	private static final String TRANSFER_SELECTED_TO_ROD = "Transfer Selected to Rod ->";
+	private static final String TRANSFER_SERIES_TO_ROD = "Transfer Series to Rod ->";
+		
 	
-	
+	public static String getTransferSelectedToRod() {
+		return TRANSFER_SELECTED_TO_ROD;
+	}
 	public static String getTurnTrackerOn() {
 		return TURN_TRACKER_ON;
 	}
@@ -15,6 +20,9 @@ public class DisplayLabelStrings {
 	}
 	public static String getBgSubtractedImage() {
 		return BG_SUBTRACTED_IMAGE;
+	}
+	public static String getTransferSeriesToRod() {
+		return TRANSFER_SERIES_TO_ROD;
 	}
 	
 	

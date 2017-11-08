a8e6c3e869ab3273577f5b9f8a5b3f11c0019510
Ed Nicklin <Ed.Nicklin@diamond.ac.uk>
2017-11-08 16:49:56 +0000

    overlap display tidying

--- uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/OverlapDisplayObjects.java
+++ uk.ac.diamond.scisoft.surfacescatter/src/org/dawnsci/surfacescatter/OverlapDisplayObjects.java
@@ -2,13 +2,11 @@ package org.dawnsci.surfacescatter;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 
@@ -28,151 +26,134 @@ public class OverlapDisplayObjects {
 	private boolean modified = false;
 	private boolean buttonPushed = false;
 	private Button resetOverlap;
-	
-	
-	public OverlapDisplayObjects generateFromOdmAndTable(OverlapDataModel odm,
-														int i,
-														Table table){
-		
-		 this.odoNumber = i;
-		 
-		 OverlapDisplayObjects odo = new OverlapDisplayObjects();
-		 
-		 label = "Overlap: " + i;
-	 
-		 textCorrectedContent = (odm.getAttenuationFactor());
-	 
-		 textRawContent = (odm.getAttenuationFactorRaw());
-		 
-		 textFhklContent = (odm.getAttenuationFactorFhkl());
 
-	    
+	public OverlapDisplayObjects generateFromOdmAndTable(OverlapDataModel odm, int i) {
+
+		this.odoNumber = i;
+
+		OverlapDisplayObjects odo = new OverlapDisplayObjects();
+
+		label = "Overlap: " + i;
+
+		textCorrectedContent = (odm.getAttenuationFactor());
+
+		textRawContent = (odm.getAttenuationFactorRaw());
+
+		textFhklContent = (odm.getAttenuationFactorFhkl());
+
 		return odo;
 	}
-	
-	public void addResetListener(Button button){
-		
+
+	public void addResetListener(Button button) {
+
 		resetOverlap = button;
-		  
-	    resetOverlap.addSelectionListener(new SelectionListener() {
-			
+
+		resetOverlap.addSelectionListener(new SelectionAdapter() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+
 				modified = false;
-				buttonPushed =true;
-				firePropertyChange("modified", 
-						OverlapDisplayObjects.this.modified, 
+				buttonPushed = true;
+				firePropertyChange("modified", OverlapDisplayObjects.this.modified,
 						OverlapDisplayObjects.this.modified = true);
 			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				
-			}
+
 		});
-	
+
 	}
-	
-	
-	public void buildTextCorrected(Text text){
-		
+
+	public void buildTextCorrected(Text text) {
+
 		this.textCorrected = text;
-		
-	    textCorrected.setText(String.valueOf(textCorrectedContent));
-		
-	    textCorrected.addModifyListener(new ModifyListener() {
-			
+
+		textCorrected.setText(String.valueOf(textCorrectedContent));
+
+		textCorrected.addModifyListener(new ModifyListener() {
+
 			@Override
 			public void modifyText(ModifyEvent e) {
 				modified = true;
 				textCorrectedContent = Double.valueOf(textCorrected.getText());
-				
+
 			}
 		});
-		
+
 	}
-	
-	
-	public void buildTextRaw(Text text){
-		
+
+	public void buildTextRaw(Text text) {
+
 		this.textRaw = text;
-		
-	    textRaw.setText(String.valueOf(textRawContent));
-		
-	    textRaw.addModifyListener(new ModifyListener() {
-			
+
+		textRaw.setText(String.valueOf(textRawContent));
+
+		textRaw.addModifyListener(new ModifyListener() {
+
 			@Override
 			public void modifyText(ModifyEvent e) {
-				
+
 				textRawContent = Double.valueOf(textRaw.getText());
 				modified = true;
-					
+
 			}
 		});
-		
+
 	}
-	
-	public void buildTextFhkl(Text text){
-		
+
+	public void buildTextFhkl(Text text) {
+
 		this.textFhkl = text;
-		
-	    textFhkl.setText(String.valueOf(textFhklContent));
-		
-	    textFhkl.addModifyListener(new ModifyListener() {
-			
+
+		textFhkl.setText(String.valueOf(textFhklContent));
+
+		textFhkl.addModifyListener(new ModifyListener() {
+
 			@Override
 			public void modifyText(ModifyEvent e) {
-				
+
 				textFhklContent = Double.valueOf(textFhkl.getText());
 				modified = true;
-					
+
 			}
 		});
-		
-	}	
-	
-	public double getTextCorrectedContentAsDoubleForBuilding(){
-		double w =  (textCorrectedContent);
-		return w;
-	}
-	
-	public double getTextRawContentAsDoubleForBuilding(){
-		double w =  (textRawContent);
-		return w;
-	}
-	
-	public double getTextFhklContentAsDoubleForBuilding(){
-		double w =  (textFhklContent);
-		return w;
-	}
-	
 
-	public double getTextCorrectedContentAsDoubleForUpdating(){
-		double w =  Double.valueOf(textCorrected.getText());
-		return w;
 	}
-	
-	public double getTextRawContentAsDoubleForUpdating(){
-		double w =  Double.valueOf(textRaw.getText());
-		return w;
+
+	public double getTextCorrectedContentAsDoubleForBuilding() {
+		return textCorrectedContent;
+
 	}
-	
-	public double getTextFhklContentAsDoubleForUpdating(){
-		double w =  Double.valueOf(textFhkl.getText());
-		return w;
+
+	public double getTextRawContentAsDoubleForBuilding() {
+		return textRawContent;
 	}
-	
-	
+
+	public double getTextFhklContentAsDoubleForBuilding() {
+		return textFhklContent;
+	}
+
+	public double getTextCorrectedContentAsDoubleForUpdating() {
+		return Double.valueOf(textCorrected.getText());
+
+	}
+
+	public double getTextRawContentAsDoubleForUpdating() {
+		return Double.valueOf(textRaw.getText());
+
+	}
+
+	public double getTextFhklContentAsDoubleForUpdating() {
+		return Double.valueOf(textFhkl.getText());
+
+	}
+
 	public TableItem getTb() {
 		return tb;
 	}
 
-
 	public void setTb(TableItem tb) {
 		this.tb = tb;
 	}
-
 
 	public String getLabel() {
 		return label;
@@ -185,49 +166,42 @@ public class OverlapDisplayObjects {
 	public Text getTextCorrected() {
 		return textCorrected;
 	}
-	
-	public OverlapAttenuationObject getOAo(){
-		if(oAo == null){
+
+	public OverlapAttenuationObject getOAo() {
+		if (oAo == null) {
 			oAo = new OverlapAttenuationObject();
-			
-		
+
 			oAo.setAttenuationFactorCorrected(getTextCorrectedContentAsDoubleForBuilding());
 			oAo.setAttenuationFactorRaw(getTextRawContentAsDoubleForBuilding());
 			oAo.setAttenuationFactorFhkl(getTextFhklContentAsDoubleForBuilding());
-				
+
 			oAo.setOdoNumber(odoNumber);
 			oAo.setModified(modified);
-		}
-		else{
+		} else {
 			oAo.setAttenuationFactorCorrected(getTextCorrectedContentAsDoubleForUpdating());
 			oAo.setAttenuationFactorRaw(getTextRawContentAsDoubleForUpdating());
 			oAo.setAttenuationFactorFhkl(getTextFhklContentAsDoubleForUpdating());
-			
+
 			oAo.setModified(modified);
 		}
-		
+
 		return oAo;
 	}
-
 
 	public void setTextCorrected(Text text) {
 		this.textCorrected = text;
 	}
-	
+
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		propertyChangeSupport.addPropertyChangeListener(listener);
 	}
 
-	public void removePropertyChangeListener(String propertyName,
-			PropertyChangeListener listener) {
-		propertyChangeSupport.removePropertyChangeListener(propertyName,
-				listener);
+	public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+		propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
 	}
 
-	protected void firePropertyChange(String propertyName, Object oldValue,
-			Object newValue) {
-		propertyChangeSupport.firePropertyChange(propertyName, oldValue,
-				newValue);
+	protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+		propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
 	}
 
 	public int getOdoNumber() {
@@ -253,7 +227,7 @@ public class OverlapDisplayObjects {
 	public void setResetOverlap(Button resetOverlap) {
 		this.resetOverlap = resetOverlap;
 	}
-	
+
 	public Text getTextRaw() {
 		return textRaw;
 	}
@@ -277,6 +251,7 @@ public class OverlapDisplayObjects {
 	public void setButtonPushed(boolean buttonPushed) {
 		this.buttonPushed = buttonPushed;
 	}
+
 	public double getTextCorrectedContent() {
 		return textCorrectedContent;
 	}

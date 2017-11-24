369bbbd5bc3b9d8ad2a17065bb56974f44c9ceab
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-11-24 15:19:21 +0000

    SCI-6336: AbstractFramesOperation: new abstract class
    Serves as base class for frame-wise operations like SumFramesOperation, which was rewritten in order to extend from this new class.

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/SumFramesOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/SumFramesOperation.java
@@ -9,73 +9,17 @@
 
 package uk.ac.diamond.scisoft.analysis.processing.operations;
 
-import org.eclipse.dawnsci.analysis.api.processing.IExportOperation;
-import org.eclipse.dawnsci.analysis.api.processing.OperationData;
-import org.eclipse.dawnsci.analysis.api.processing.OperationException;
-import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.api.processing.model.EmptyModel;
-import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
-import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
-import org.eclipse.january.IMonitor;
-import org.eclipse.january.dataset.Dataset;
-import org.eclipse.january.dataset.DatasetUtils;
-import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.Maths;
 
-public class SumFramesOperation extends AbstractOperation<EmptyModel, OperationData> implements IExportOperation {
+public class SumFramesOperation extends AbstractFramesOperation {
 
-	private Dataset sum;
-	private int count;
-	
 	@Override
 	public String getId() {
 		return "uk.ac.diamond.scisoft.analysis.processing.operations.SumFramesOperation";
 	}
 
-	@Override
-	public OperationRank getInputRank() {
-		return OperationRank.ANY;
+	// do not use the method reference -> see https://jira.diamond.ac.uk/browse/SCI-6339
+	public SumFramesOperation() {
+		super((oldDataset, newDataset) -> Maths.add(oldDataset, newDataset));
 	}
-
-	@Override
-	public OperationRank getOutputRank() {
-		return OperationRank.SAME;
-	}
-	
-	@Override
-	public void init(){
-		sum = null;
-		count = 0;
-	}
-
-	@Override
-	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
-		SliceFromSeriesMetadata ssm = getSliceSeriesMetadata(input);
-		
-		if (sum == null) {
-			sum = DatasetUtils.convertToDataset(input.getSlice());
-		} else {
-			sum.iadd(input);
-		}
-		
-		count++;
-		
-		if (count == ssm.getTotalSlices()) {
-			IDataset out = sum.getSliceView();
-			copyMetadata(input, out);
-			out.clearMetadata(SliceFromSeriesMetadata.class);
-			sum = null;
-			SliceFromSeriesMetadata outsmm = ssm.clone();
-			for (int i = 0; i < ssm.getParent().getRank(); i++) {
-				
-				if (!outsmm.isDataDimension(i)) outsmm.reducedDimensionToSingular(i);
-				
-			}
-			out.setMetadata(outsmm);
-			
-			return new OperationData(out);
-		}
-		
-		return null;
-	}
-
 }

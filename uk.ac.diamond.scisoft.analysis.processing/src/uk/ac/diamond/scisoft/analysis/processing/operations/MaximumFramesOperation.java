e73c934952c92856ee8d9a85df40002598a8198c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-11-27 10:39:55 +0000

    SCI-6336: new operation MaximumFramesOperation

---
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/MaximumFramesOperation.java
@@ -0,0 +1,25 @@
+/*-
+ * Copyright 2017 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package uk.ac.diamond.scisoft.analysis.processing.operations;
+
+import org.eclipse.january.dataset.Maths;
+
+public class MaximumFramesOperation extends AbstractFramesOperation {
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.operations.MaximumFramesOperation";
+	}
+
+	// do not use the method reference -> see https://jira.diamond.ac.uk/browse/SCI-6339
+	public MaximumFramesOperation() {
+		super((oldDataset, newDataset) -> Maths.maximum(oldDataset, newDataset));
+	}
+}

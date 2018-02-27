a6bd13b91b96862bdda9f0ba5635b2c32cd37bb8
Peter Chang <peter.chang@diamond.ac.uk>
2018-02-27 10:54:33 +0000

    Add dark frame support to background subtraction operation

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractFittedBackgroundModel.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractFittedBackgroundModel.java
@@ -10,6 +10,7 @@
 package uk.ac.diamond.scisoft.analysis.processing.operations.backgroundsubtraction;
 
 import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+import org.eclipse.dawnsci.analysis.api.processing.model.FileType;
 import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
 
 /**
@@ -21,17 +22,29 @@ public class SubtractFittedBackgroundModel extends AbstractOperationModel {
 		Gaussian;
 	}
 
-	@OperationModelField(label = "Positive Only (ignore negative values in data)")
-	private boolean positiveOnly = true;
-
-	@OperationModelField(label = "Background PDF")
+	@OperationModelField(fieldPosition = 0, label = "Background PDF")
 	private BackgroundPixelPDF backgroundPDF = BackgroundPixelPDF.Gaussian;
 
-	@OperationModelField(label = "Signal to background ratio", min=1)
+	@OperationModelField(fieldPosition = 1, label = "Positive Only (ignore negative values in data)")
+	private boolean positiveOnly = true;
+
+	@OperationModelField(fieldPosition = 2, label = "Signal to background ratio", min = 1)
 	private double ratio = 3.0;
+
+	@OperationModelField(fieldPosition = 3, label = "Dark image file", file = FileType.EXISTING_FILE, hint = "Can be empty then uniform background is assumed")
+	private String darkImageFile = null;
+
+//	@OperationModelField(fieldPosition = 4, label = "Rectangle to fit profile", enableif = "darkImageFile != null")
+//	private IRectangularROI roi = null;
+
+	@OperationModelField(fieldPosition = 5, label = "Gaussian smoothing length parameter", enableif = "darkImageFile != null")
+	private double gaussianSmoothingLength = 10;
 
 	public static final int HISTOGRAM_MAX_BINS = 1024*1024;
 
+	/**
+	 * @return if true, ignore negative values in background
+	 */
 	public boolean isPositiveOnly() {
 		return positiveOnly;
 	}
@@ -40,6 +53,9 @@ public class SubtractFittedBackgroundModel extends AbstractOperationModel {
 		firePropertyChange("setPositiveOnly", this.positiveOnly, this.positiveOnly = positiveOnly);
 	}
 
+	/**
+	 * @return probability distribution function used to fit background
+	 */
 	public BackgroundPixelPDF getBackgroundPDF() {
 		return backgroundPDF;
 	}
@@ -48,11 +64,49 @@ public class SubtractFittedBackgroundModel extends AbstractOperationModel {
 		firePropertyChange("setBackgroundPDF", this.backgroundPDF, this.backgroundPDF = pdf);
 	}
 
+	/**
+	 * @return signal to noise ratio that determines the threshold value at which to cut-off the background
+	 */
 	public double getRatio() {
 		return ratio;
 	}
 
 	public void setRatio(double ratio) {
 		firePropertyChange("setRatio", this.ratio, this.ratio = ratio);
+	}
+
+	/**
+	 * @return path to file that contains dark image(s)
+	 */
+	public String getDarkImageFile() {
+		return darkImageFile;
+	}
+
+	public void setDarkImageFile(String darkImageFile) {
+		firePropertyChange("setDarkImageFile", this.darkImageFile, this.darkImageFile = darkImageFile);
+	}
+
+//	/**
+//	 * @return get region of interest (can be null to signify the entire image)
+//	 */
+//	public IRectangularROI getRoi() {
+//		return roi;
+//	}
+//
+//	public void setRoi(IRectangularROI roi) {
+//		firePropertyChange("setRoi", this.roi, this.roi = roi);
+//	}
+//
+	/**
+	 * @return length parameter used for Gaussian smoothing filter
+	 */
+	public double getGaussianSmoothingLength() {
+		return gaussianSmoothingLength;
+	}
+
+	public static final String GAUSSIAN_PROPERTY = "setGaussianSmoothingLength";
+
+	public void setGaussianSmoothingLength(double gaussianSmoothingLength) {
+		firePropertyChange(GAUSSIAN_PROPERTY, this.gaussianSmoothingLength, this.gaussianSmoothingLength = gaussianSmoothingLength);
 	}
 }
--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractFittedBackgroundOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/backgroundsubtraction/SubtractFittedBackgroundOperation.java
@@ -9,41 +9,63 @@
 
 package uk.ac.diamond.scisoft.analysis.processing.operations.backgroundsubtraction;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.fitting.functions.IFunction;
 import org.eclipse.dawnsci.analysis.api.fitting.functions.IParameter;
-import org.eclipse.dawnsci.analysis.api.processing.IOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationDataForDisplay;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationLog;
+import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
+import org.eclipse.dawnsci.analysis.api.tree.Tree;
+import org.eclipse.dawnsci.analysis.dataset.impl.Signal;
+import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
+import org.eclipse.dawnsci.analysis.dataset.slicer.SliceInformation;
+import org.eclipse.january.DatasetException;
 import org.eclipse.january.IMonitor;
+import org.eclipse.january.dataset.BroadcastSelfIterator;
+import org.eclipse.january.dataset.BroadcastSingleIterator;
 import org.eclipse.january.dataset.Comparisons;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.DatasetUtils;
 import org.eclipse.january.dataset.DoubleDataset;
 import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.dataset.ILazyDataset;
 import org.eclipse.january.dataset.IndexIterator;
 import org.eclipse.january.dataset.IntegerDataset;
+import org.eclipse.january.dataset.LazyMaths;
+import org.eclipse.january.dataset.LongDataset;
 import org.eclipse.january.dataset.Maths;
+import org.eclipse.january.dataset.Operations;
 import org.eclipse.january.dataset.Slice;
 import org.eclipse.january.dataset.SliceND;
+import org.eclipse.january.dataset.UnaryOperation;
 
 import uk.ac.diamond.scisoft.analysis.dataset.function.Histogram;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.AFunction;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.CoordinatesIterator;
 import uk.ac.diamond.scisoft.analysis.fitting.functions.Gaussian;
+import uk.ac.diamond.scisoft.analysis.io.NexusTreeUtils;
 import uk.ac.diamond.scisoft.analysis.optimize.ApacheOptimizer;
 import uk.ac.diamond.scisoft.analysis.optimize.ApacheOptimizer.Optimizer;
+import uk.ac.diamond.scisoft.analysis.processing.LocalServiceManager;
 import uk.ac.diamond.scisoft.analysis.processing.metadata.FitMetadataImpl;
 import uk.ac.diamond.scisoft.analysis.processing.operations.MetadataUtils;
 import uk.ac.diamond.scisoft.analysis.processing.operations.backgroundsubtraction.SubtractFittedBackgroundModel.BackgroundPixelPDF;
+import uk.ac.diamond.scisoft.analysis.processing.operations.rixs.ElasticLineReduction;
 import uk.ac.diamond.scisoft.analysis.processing.operations.utils.ProcessingUtils;
 
 /**
  * Subtract a background from the input dataset. The background is determined by fitting to a function
  */
-public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionOperation<SubtractFittedBackgroundModel> {
+public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionOperation<SubtractFittedBackgroundModel> implements PropertyChangeListener {
 
 	private IFunction pdf;
 	private double residual;
@@ -52,6 +74,13 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 	private Dataset fit;
 	private double threshold;
 	private OperationLog log = new OperationLog();
+	private String darkImageFile = null;
+	private Dataset smoothedDarkProfile;
+	private Dataset darkProfile;
+
+	protected List<IDataset> displayData = new ArrayList<>();
+	protected List<IDataset> auxData = new ArrayList<>();
+	private double darkImageCountTime;
 
 	@Override
 	public String getFilenameSuffix() {
@@ -61,6 +90,19 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 	@Override
 	public String getId() {
 		return getClass().getName();
+	}
+
+	@Override
+	public void setModel(SubtractFittedBackgroundModel model) {
+		super.setModel(model);
+		model.addPropertyChangeListener(this);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (SubtractFittedBackgroundModel.GAUSSIAN_PROPERTY.equals(evt.getPropertyName())) {
+			smoothedDarkProfile = null;
+		}
 	}
 
 	public static double findPositiveMin(Dataset d) {
@@ -87,12 +129,12 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		}
 
 		Dataset bins;
-		if (in.hasFloatingPointElements() || nbin > SubtractFittedBackgroundModel.HISTOGRAM_MAX_BINS) {
-			bins = DatasetFactory.createLinearSpace(DoubleDataset.class, min, max, nbin + 1);
+		if (in.hasFloatingPointElements() || nbin >= SubtractFittedBackgroundModel.HISTOGRAM_MAX_BINS) {
+			bins = DatasetFactory.createLinearSpace(DoubleDataset.class, min, max, Math.min(nbin + 1, SubtractFittedBackgroundModel.HISTOGRAM_MAX_BINS));
 		} else {
 			int imin = (int) min;
 			int imax = (int) max;
-			if (delta > 1) {
+			if (delta > 1) { // align bins with zero
 				imin = ((imin-delta+1)/delta) * delta;
 				imax = ((imax+delta-1)/delta) * delta;
 			}
@@ -120,12 +162,11 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		int e = (int) p.getUpperLimit();
 		int c = (int) p.getValue();
 		p.setLimits(x.getDouble(c - 1) - 0.5, x.getDouble(c + 1)); // set narrow range for fitting pdf position
-		
 		double cx = x.getDouble(c);
 		p.setValue(cx);
 		SliceND slice = new SliceND(h.getShapeRef(), new Slice(b, e + 1, 1));
 
-		residual = fitFunction(this, pdf, x.getSliceView(slice), h.getSliceView(slice));
+		residual = fitFunction(pdf, x.getSliceView(slice), h.getSliceView(slice));
 		log.append("\nFitted PDF in %s: residual = %g\n%s", slice, residual, pdf);
 
 		fit = DatasetUtils.convertToDataset(pdf.calculateValues(x));
@@ -148,11 +189,57 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		return thr;
 	}
 
-	// this calculates a background to subtract from input
 	@Override
 	protected Dataset getImage(IDataset input) throws OperationException {
+		// this calculates a background to subtract from input
 		Dataset in = DatasetUtils.convertToDataset(input);
-		double thr = calculateThreshold(in, model.isPositiveOnly());
+//		IRectangularROI r = model.getRoi(); // cannot do this as superclass expects result to have input's shape 
+//		Slice[] s = null;
+//		if (r != null) {
+//			s = getSlice(in.getShapeRef(), r);
+//			if (s[0] != null || s[1] != null) {
+//				in = in.getSliceView(s);
+//			}
+//		}
+
+		if (smoothedDarkProfile != null) {
+			// fit dark to current??? (scale and offset)
+//			displayData.add(dark);
+			Dataset profile = in.sum(1, true);
+			if (profile.getDouble() == 0) {
+				profile.set(0.5*(profile.getDouble(1) + profile.getDouble(2)), 0); // ensure 1st entry is non-zero
+			}
+
+			profile.setName("profile");
+			auxData.add(profile);
+			IFunction so = scaleAndOffset(profile);
+			Dataset darkFit = Maths.multiply(smoothedDarkProfile, so.getParameterValue(0)).iadd(so.getParameterValue(1));
+//			displayData.add(smoothedDarkProfile);
+//			displayData.add(profile);
+//			displayData.add(darkFit);
+
+			auxData.add(ProcessingUtils.createNamedDataset(darkFit, "profile_dark"));
+			Dataset diff = Maths.subtract(profile, darkFit);
+			auxData.add(ProcessingUtils.createNamedDataset(diff, "profile_diff"));
+//			displayData.add(diff);
+			Dataset scaledFit = Maths.multiply(darkFit.reshape(darkFit.getSize(), 1), 1./in.getShapeRef()[1]).cast(in.getClass());
+
+			diff = Maths.subtract(in, scaledFit);
+
+			double thr = calculateThreshold(diff, smoothedDarkProfile == null ? model.isPositiveOnly() : false);
+			threshold = thr;
+			if (Double.isNaN(thr)) {
+				double min = in.min(true).doubleValue();
+				log.append("No range in data. All finite values are %g", min);
+				return DatasetFactory.createFromObject(min);
+			}
+
+			scaledFit.iadd(thr);
+			log.append("Threshold = %g", threshold);
+			return DatasetUtils.select(Comparisons.lessThan(in, scaledFit), in, scaledFit);
+		}
+
+		double thr = calculateThreshold(in, smoothedDarkProfile == null ? model.isPositiveOnly() : false);
 		threshold = thr;
 		if (Double.isNaN(thr)) {
 			double min = in.min(true).doubleValue();
@@ -170,23 +257,225 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		return DatasetUtils.select(Comparisons.lessThan(in, thr), in, thr);
 	}
 
-	static double fitFunction(IOperation<?, ?> op, IFunction fun, Dataset xf, IDataset hf) {
-		double residual = Double.POSITIVE_INFINITY;
-		try {
-			ApacheOptimizer opt = new ApacheOptimizer(Optimizer.LEVENBERG_MARQUARDT);
-			opt.optimize(new Dataset[] {xf}, hf, fun);
-			residual = opt.calculateResidual();
-		} catch (Exception fittingError) {
-			throw new OperationException(op, "Exception performing fit", fittingError);
+	private IFunction scaleAndOffset(Dataset in) {
+		ScaleAndOffset so = new ScaleAndOffset(smoothedDarkProfile);
+		so.setParameterValues(1, 0);
+		double res = fitFunction(so, DatasetFactory.zeros(in.getShapeRef()), in);
+		System.err.printf("Scale and offset results: res=%g\n%s\n", res, so);
+		return so;
+	}
+
+	class ScaleAndOffset extends AFunction {
+		private static final long serialVersionUID = -5789700891337976334L;
+		private Dataset in;
+		private BroadcastSelfIterator bit;
+		private int[] shape;
+
+		public ScaleAndOffset(Dataset in) {
+			super(2);
+			this.in = in;
 		}
-		return residual;
+
+		@Override
+		public double val(double... values) {
+			return 0;
+		}
+
+		@Override
+		protected void setNames() {
+		}
+
+		@Override
+		public void fillWithValues(DoubleDataset data, CoordinatesIterator it) {
+			double a = getParameterValue(0);
+			double b = getParameterValue(1);
+
+			int[] dshape = data.getShapeRef();
+			if (bit == null || !Arrays.equals(shape, dshape)) {
+				shape = dshape;
+				bit = BroadcastSingleIterator.createIterator(data, in);
+			} else {
+				bit.reset();
+			}
+			bit.setOutputDouble(true);
+			while (bit.hasNext()) {
+				data.setAbs(bit.aIndex, a * bit.bDouble + b);
+			}
+		}
+	}
+
+	class GaussianOperation implements UnaryOperation {
+
+		private double length;
+
+		public GaussianOperation(double length) {
+			this.length = length;
+		}
+
+		@Override
+		public boolean booleanOperate(long a) {
+			return false;
+		}
+
+		@Override
+		public long longOperate(long a) {
+			return (long) doubleOperate(a);
+		}
+
+		@Override
+		public double doubleOperate(double a) {
+			double x = a/length;
+			return Math.exp(-x*x);
+		}
+
+		@Override
+		public void complexOperate(double[] out, double ra, double ia) {
+		}
+
+		@Override
+		public String toString(String a) {
+			return "Gaussian";
+		}
+	}
+
+	private Dataset smoothFilter(Dataset darkProfile) {
+		double l = model.getGaussianSmoothingLength();
+		int n = (int) Math.ceil(3*l);
+		int m = 2*n + 1;
+		Dataset x = DatasetFactory.createRange(m);
+		x.isubtract(n);
+		Dataset g = Operations.operate(new GaussianOperation(l), x, null);
+		g.imultiply(1./((Number) g.sum(true)).doubleValue());
+		Dataset smoothed = Signal.convolveToSameShape(darkProfile.cast(DoubleDataset.class), g, null);
+		smoothed.setSlice(smoothed.getDouble(n), new Slice(n));
+		smoothed.setSlice(smoothed.getDouble(-n), new Slice(-n-1, null));
+		return smoothed;
+	}
+
+	private void createDarkProfile(SliceFromSeriesMetadata ssm) {
+		String file = model.getDarkImageFile();
+		if (file == null) {
+			smoothedDarkProfile = null;
+			darkImageFile = null;
+			return;
+		}
+		if (file.equals(darkImageFile) && smoothedDarkProfile != null) {
+			return;
+		}
+
+		if (!file.equals(darkImageFile)) {
+			darkImageFile = file;
+	
+			String data = ssm.getDatasetName();
+	
+			ILazyDataset dark;
+			try {
+				dark = LocalServiceManager.getLoaderService().getData(file, null).getLazyDataset(data);
+			} catch (Exception e) {
+				throw new OperationException(this, "Could not load dark image file", e);
+			}
+	
+			darkImageCountTime = getCountTime(file).getDouble();
+	
+			try {
+				// sum all so that only profile along row is left
+				darkProfile = LazyMaths.sum(dark, true, 1).cast(LongDataset.class);
+				if (darkProfile.getDouble() == 0) {
+					darkProfile.set(0.5*(darkProfile.getDouble(1) + darkProfile.getDouble(2)), 0); // ensure 1st entry is non-zero
+				}
+				darkProfile.imultiply(1./dark.getShape()[0]); // assume 3D
+				darkProfile.setName("dark_image_profile");
+			} catch (DatasetException e) {
+				throw new OperationException(this, "Could not generate dark image profile", e);
+			}
+	
+			// blip removal...
+			Dataset diffs = Maths.difference(darkProfile, 1, 0);
+//			diffs.setName("differences");
+//			displayData.add(diffs);
+	
+			// get FWHM
+			IFunction dpdf = fitHistogram(diffs, false);
+			double f = dpdf.getParameterValue(1);
+			System.err.println("Differences has FWHM of " + f);
+			IndexIterator it = diffs.getIterator();
+			it.hasNext();
+			double yp = diffs.getElementDoubleAbs(it.index);
+			int i = 1;
+			while (it.hasNext()) {
+				double yc = diffs.getElementDoubleAbs(it.index);
+				if (Math.abs(yp) > f && Math.abs(yc) > f && Math.signum(yp) != Math.signum(yc)) {
+					darkProfile.set(0.5*(darkProfile.getDouble(i-1) + darkProfile.getDouble(i+1)), i);
+				}
+				i++;
+				yp = yc;
+			}
+		}
+
+		smoothedDarkProfile = smoothFilter(darkProfile);
+		smoothedDarkProfile.setName("dark_image_smoothed_profile");
+//		displayData.add(darkProfile);
+//		displayData.add(smoothedDarkProfile);
+	}
+
+	private IFunction fitHistogram(Dataset d, boolean plot) {
+		List<Dataset> hs = createHistogram(d, false, 200);
+		Dataset hd = hs.get(0);
+		hd.setName("diff histo");
+		Dataset dx = hs.get(1).getSliceView(new Slice(-1));
+		IFunction dpdf = prepareBackgroundPDF(dx, hd, model.getBackgroundPDF());
+		IParameter p = dpdf.getParameter(0);
+		int b = (int) p.getLowerLimit();
+		int e = (int) p.getUpperLimit();
+		int c = (int) p.getValue();
+		p.setLimits(dx.getDouble(Math.max(0, c - 5)) - 0.5, dx.getDouble(Math.min(dx.getSize()-1, c + 5))); // set narrow range for fitting pdf position
+		p.setValue(dx.getDouble(c)); 
+		SliceND slice = new SliceND(hd.getShapeRef(), new Slice(b, e + 1, 1));
+		System.err.println("Initial histo peak: " + dpdf);
+
+		double res = fitFunction(dpdf, dx.getSliceView(slice), hd.getSliceView(slice));
+		log.append("\nFitted PDF in %s: residual = %g\n%s", slice, res, dpdf);
+		System.err.println("Fitted histo peak: " + dpdf + " in " + slice);
+		if (plot) {
+			IDataset t = dpdf.calculateValues(dx);
+			MetadataUtils.setAxes(t, dx);
+			displayData.add(t);
+		}
+		return dpdf;
+	}
+
+//	private Slice[] getSlice(int[] shape, IRectangularROI r) {
+//		Slice s0 = RixsBaseOperation.createSlice(r.getPointY(), r.getLength(1), shape[0]);
+//		Slice s1 = RixsBaseOperation.createSlice(r.getPointX(), r.getLength(0), shape[1]);
+//
+//		return new Slice[] {s0, s1};
+//	}
+
+	protected double fitFunction(IFunction f, Dataset x, Dataset v) {
+		return ElasticLineReduction.fitFunction(this, new ApacheOptimizer(Optimizer.LEVENBERG_MARQUARDT), log, f, x, v, null);
 	}
 
 	@Override
 	public OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		auxData.clear();
+//		displayData.clear();
 		log.clear();
 		log.append("Subtract Fitted Background");
 		log.append("==========================");
+
+		SliceFromSeriesMetadata ssm = getSliceSeriesMetadata(input);
+		SliceInformation si = ssm.getSliceInfo();
+		if (si.isFirstSlice()) {
+			createDarkProfile(ssm);
+
+			if (smoothedDarkProfile != null) {
+				String filePath = ssm.getFilePath();
+				double countTime = getCountTime(filePath).getDouble();
+				if (countTime != darkImageCountTime) {
+					throw new OperationException(this, "Count time in " + filePath + ": " + countTime + " != " + darkImageCountTime + " for dark image");
+				}
+			}
+		}
 
 		OperationData op = super.process(input, monitor);
 
@@ -194,19 +483,25 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		OperationDataForDisplay od = new OperationDataForDisplay(op.getData());
 		od.setShowSeparately(true);
 		od.setLog(log);
+		od.setDisplayData(displayData.toArray(new IDataset[displayData.size()]));
 
 		if (h != null) {
 			setDisplayData(od, x, h, fit);
-	
+
 			// switch to use display data and record fit parameters as [1]-shape datasets
 			// need two plots...
-			od.setAuxData(ProcessingUtils.createNamedDataset(threshold, "threshold"),
-					ProcessingUtils.createNamedDataset(pdf.getParameterValue(0), "pdf_posn"),
-					ProcessingUtils.createNamedDataset(pdf.getParameterValue(1), "pdf_fwhm"),
-					ProcessingUtils.createNamedDataset(pdf.getParameterValue(2), "pdf_area"),
-					ProcessingUtils.createNamedDataset(residual, "residual"));
-	
+			auxData.add(ProcessingUtils.createNamedDataset(threshold, "threshold"));
+			auxData.add(ProcessingUtils.createNamedDataset(pdf.getParameterValue(0), "pdf_posn"));
+			auxData.add(ProcessingUtils.createNamedDataset(pdf.getParameterValue(1), "pdf_fwhm"));
+			auxData.add(ProcessingUtils.createNamedDataset(pdf.getParameterValue(2), "pdf_area"));
+			auxData.add(ProcessingUtils.createNamedDataset(residual, "residual"));
+			od.setAuxData(auxData.toArray(new Serializable[auxData.size()]));
+
 			od.getData().addMetadata(new FitMetadataImpl().setFitFunction(pdf));
+		}
+
+		if (si.isLastSlice() && smoothedDarkProfile != null) {
+			od.setSummaryData(darkProfile, smoothedDarkProfile);
 		}
 
 		return od;
@@ -279,4 +574,29 @@ public class SubtractFittedBackgroundOperation extends AbstractImageSubtractionO
 		List<Double> cs = DatasetUtils.crossings(x.getSliceView(slice), y.getSliceView(slice), (ym + base)*0.5);
 		return cs.size() > 0 ? (cs.get(0) - x.getDouble(pos)) * 2 : Double.NaN;
 	}
+
+	/**
+	 * Parse NeXus file to set various fields
+	 * @param llog 
+	 * @param filePath
+	 */
+	protected Dataset getCountTime(String filePath) {
+		try {
+			Tree t = LocalServiceManager.getLoaderService().getData(filePath, null).getTree();
+
+			GroupNode root = t.getGroupNode();
+			// entry1:NXentry
+			//     before_scan:NXcollection
+			//         andorPreampGain:NXcollection/andorPreampGain [1, 2, 4]
+			//         pgmEnergy:NXcollection/ [energy in eV, always single value, even for an energy scan]
+
+			GroupNode entry = (GroupNode) NexusTreeUtils.findFirstNode(root, "NXentry").getDestination();
+			GroupNode instrument = (GroupNode) NexusTreeUtils.findFirstNode(entry, "NXinstrument").getDestination();
+			GroupNode detector = (GroupNode) NexusTreeUtils.findFirstNode(instrument, "NXdetector").getDestination();
+			return DatasetUtils.sliceAndConvertLazyDataset(detector.getDataNode("count_time").getDataset());
+		} catch (Exception e) {
+			throw new OperationException(this, "Could not parse Nexus file " + filePath + " for count time", e);
+		}
+	}
+
 }

d4bcd88aa1a96759e5f76bef55cbd17b6f6e596b
Tim Spain <timothy.spain@diamond.ac.uk>
2018-05-14 17:38:26 +0100

    Interpolate from the XRMC detector to the real detector in two Operations.

---
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/InterpolateXRMCToGammaDelta.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package uk.ac.diamond.scisoft.xpdf.operations;
+
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.api.processing.model.EmptyModel;
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
+import org.eclipse.january.IMonitor;
+import org.eclipse.january.dataset.IDataset;
+
+public class InterpolateXRMCToGammaDelta extends AbstractOperation<EmptyModel, OperationData> {
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.xpdf.operations.InterpolateXRMCToGammaDelta";
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		// FIXME: Implement more than a pass-through
+		return new OperationData(input);
+	}
+
+}

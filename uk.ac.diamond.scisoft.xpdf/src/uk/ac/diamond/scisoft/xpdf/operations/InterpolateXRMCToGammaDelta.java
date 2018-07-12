387315b6c9af3baa8caa0484b9a3da282ccee883
Tim Spain <timothy.spain@diamond.ac.uk>
2018-07-12 15:59:53 +0100

    Add user definable limits to the interpolation to (γ,δ) coordinates., Also, optionally output the coordinate values.
    Change-Id: I8a6c8683ac6dac2e3aac22aba858dd3b246db1cb

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/InterpolateXRMCToGammaDelta.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/InterpolateXRMCToGammaDelta.java
@@ -11,12 +11,10 @@ package uk.ac.diamond.scisoft.xpdf.operations;
 import java.util.List;
 
 import javax.vecmath.Vector2d;
-import javax.vecmath.Vector3d;
 
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.api.processing.model.EmptyModel;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 import org.eclipse.january.IMonitor;
 import org.eclipse.january.MetadataException;
@@ -26,14 +24,13 @@ import org.eclipse.january.dataset.DatasetUtils;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.metadata.AxesMetadata;
 import org.eclipse.january.metadata.MetadataFactory;
-import org.eclipse.january.metadata.internal.AxesMetadataImpl;
 
 import uk.ac.diamond.scisoft.analysis.diffraction.powder.GenericPixelIntegrationCache;
 import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegration;
 import uk.ac.diamond.scisoft.xpdf.metadata.XRMCMetadata;
 import uk.ac.diamond.scisoft.xpdf.xrmc.XRMCDetector;
 
-public class InterpolateXRMCToGammaDelta extends AbstractOperation<EmptyModel, OperationData> {
+public class InterpolateXRMCToGammaDelta extends AbstractOperation<InterpolateXRMCToGDModel, OperationData> {
 
 	@Override
 	public String getId() {
@@ -60,11 +57,6 @@ public class InterpolateXRMCToGammaDelta extends AbstractOperation<EmptyModel, O
 		
 		Dataset gamma = DatasetFactory.zeros(shape);
 		Dataset delta = DatasetFactory.zeros(shape);
-		Dataset x = DatasetFactory.zeros(shape);
-		Dataset y = DatasetFactory.zeros(shape);
-		Dataset z = DatasetFactory.zeros(shape);
-		Dataset	phi = DatasetFactory.zeros(shape);
-		Dataset tth = DatasetFactory.zeros(shape);
 
 		// Get the angles
 		for (int i = 0; i < shape[0]; i++) {
@@ -73,38 +65,40 @@ public class InterpolateXRMCToGammaDelta extends AbstractOperation<EmptyModel, O
 				Vector2d angles = xdet.anglesFromPixel(pxPos);
 				gamma.set(angles.x, i, j);
 				delta.set(angles.y, i, j);
-				Vector2d phtth = new Vector2d(xdet.polarAnglesFromPixel(pxPos));
-				phi.set(phtth.x, i, j);
-				tth.set(phtth.y, i, j);
-				Vector3d position = xdet.labFromPixel(pxPos);
-				x.set(position.x, i, j);
-				y.set(position.y, i, j);
-				z.set(position.z, i, j);
 			}
 		}
 
-		// Interpolate to a (γ,δ) grid
-		double gamma00 = gamma.getDouble(0, 0);
-		double gammaNx = gamma.getDouble(0, shape[1]-1);
-		double nGamma = shape[0];
-		double gammaStep = (gammaNx - gamma00)/(nGamma-1);
-
-		double delta0 = 0;
-		// Limits of delta
-		for (int i = 0; i < shape[1]; i++) {
-			if (Math.abs(delta.getDouble(0, i)) >= Math.abs(gamma.getDouble(0, i)))
-				delta0 = Math.abs(delta.getDouble(0, i));
+		// Here we bail if either of the 'Show $coordinate' boxes have been checked
+		if (model.isShowGamma()) {
+			return new OperationData(gamma);
 		}
-		double delta00 = -delta0;
-		double deltaNx = delta0;
-		double nDelta = shape[0];
-		double deltaStep = (deltaNx - delta00)/(nDelta-1);
+		
+		if (model.isShowDelta()) {
+			return new OperationData(delta);
+		}
+		
+		// Interpolate to a (γ,δ) grid
 
-		// Make the axes isotropic at the origin
-		double isoStep = Math.min(gammaStep, deltaStep);
-		Dataset gammaRange;
-		Dataset deltaRange = DatasetFactory.createRange(delta00, deltaNx+isoStep, isoStep);
-		gammaRange = deltaRange.clone();
+		AngularLimits angularLimits = new AngularLimits();
+
+		if (model.getGammaRange() == null || model.getDeltaRange() == null)
+			angularLimits.calculate(gamma, delta);
+		
+		if (model.getStepSize() != null) {
+			angularLimits.gammaStep = angularLimits.deltaStep = model.getStepSize();
+		}
+		
+		if (model.getGammaRange() != null) {
+			angularLimits.gammaMin = model.getGammaRange()[0];
+			angularLimits.gammaMax = model.getGammaRange()[1];
+		}
+		if (model.getDeltaRange() != null) {
+			angularLimits.deltaMin = model.getDeltaRange()[0];
+			angularLimits.deltaMax = model.getDeltaRange()[1];
+		}
+		
+		Dataset deltaRange = DatasetFactory.createRange(angularLimits.deltaMin, angularLimits.deltaMax, angularLimits.deltaStep);
+		Dataset gammaRange = DatasetFactory.createRange(angularLimits.gammaMin, angularLimits.gammaMax, angularLimits.gammaStep);
 		
 		GenericPixelIntegrationCache gdpic = new GenericPixelIntegrationCache(gamma, delta, gammaRange, deltaRange);
 		List<Dataset> piResults = PixelIntegration.integrate(DatasetUtils.convertToDataset(input), null, gdpic);
@@ -127,4 +121,58 @@ public class InterpolateXRMCToGammaDelta extends AbstractOperation<EmptyModel, O
 		return new OperationData(gdData);
 	}
 
+	
+	private class AngularLimits {
+		double gammaMin;
+		double gammaMax;
+		double deltaMin;
+		double deltaMax;
+		double gammaStep;
+		double deltaStep;
+	
+		public AngularLimits() {
+			gammaMin = Double.MAX_VALUE;
+			gammaMax = Double.MIN_VALUE;
+			deltaMin = Double.MAX_VALUE;
+			deltaMax = Double.MIN_VALUE;
+			gammaStep = 0.0;
+			deltaStep = 0.0;
+		}
+
+		public void calculate(Dataset gamma, Dataset delta) {
+
+			int[] shape = gamma.getShape();
+
+			double gamma00 = gamma.getDouble(0, 0);
+			double gammaNx = gamma.getDouble(0, shape[1]-1);
+			double nGamma = shape[0];
+			gammaStep = (gammaNx - gamma00)/(nGamma-1);
+			
+			double delta0 = 0;
+			// Limits of delta
+			for (int i = 0; i < shape[1]; i++) {
+				if (Math.abs(delta.getDouble(0, i)) >= Math.abs(gamma.getDouble(0, i)))
+					delta0 = Math.abs(delta.getDouble(0, i));
+			}
+			double delta00 = -delta0;
+			double deltaNx = delta0;
+			double nDelta = shape[0];
+			deltaStep = (deltaNx - delta00)/(nDelta-1);
+
+			
+			// Make the axes isotropic at the origin
+			double isoStep = Math.min(gammaStep, deltaStep);
+			gammaStep = isoStep;
+			deltaStep = isoStep;
+			
+			// Delta is the more restrictive coordinate
+			deltaMin = delta00;
+			deltaMax = deltaNx + deltaStep; 
+
+			// Make a square array
+			gammaMin = deltaMin;
+			gammaMax = deltaMax;
+		
+		}
+	}
 }

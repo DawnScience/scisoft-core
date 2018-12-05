669ffdccd4584fd555473a499afd621191f13596
Peter Chang <peter.chang@diamond.ac.uk>
2018-12-05 10:26:14 +0000

    Create new constants class
    Change-Id: Iddbf7a5f8e62a35faba24f74067c9d0121f33349

---
+++ org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/PlottingEventConstants.java
@@ -0,0 +1,19 @@
+/*-
+ * Copyright 2018 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.plotting.api;
+
+public class PlottingEventConstants {
+
+	/**
+	 * Topic name for file open event
+	 */
+	public static final String FILE_OPEN_EVENT = "org/dawnsci/events/file/OPEN";
+
+}

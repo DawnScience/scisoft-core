27622a7be3cd9ab685d24f940cc19af2a6cc9a99
Tim Spain <timothy.spain@diamond.ac.uk>
2019-01-15 15:42:57 +0000

    Use smoother interpolation to take the data from projective coordinates to the target detector.
    Change-Id: I93fe60388847a7f34052a7aaa85a2adf3ff8f0ff

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/InterpolateXRMCFromProjectiveOperation.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/operations/InterpolateXRMCFromProjectiveOperation.java
@@ -10,6 +10,7 @@ package uk.ac.diamond.scisoft.xpdf.operations;
 
 import javax.vecmath.Vector3d;
 
+import org.apache.commons.math3.analysis.interpolation.PiecewiseBicubicSplineInterpolator;
 import org.eclipse.dawnsci.analysis.api.diffraction.DetectorProperties;
 import org.eclipse.dawnsci.analysis.api.metadata.IDiffractionMetadata;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
@@ -25,8 +26,7 @@ import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.ILazyDataset;
 import org.eclipse.january.metadata.AxesMetadata;
 
-import uk.ac.diamond.scisoft.analysis.diffraction.powder.GenericPixelIntegrationCache;
-import uk.ac.diamond.scisoft.analysis.diffraction.powder.PixelIntegration;
+import uk.ac.diamond.scisoft.analysis.dataset.function.Interpolation2D;
 import uk.ac.diamond.scisoft.analysis.io.NexusDiffractionCalibrationReader;
 
 public class InterpolateXRMCFromProjectiveOperation extends AbstractOperation<InterpolateXRMCFromGDModel, OperationData> {
@@ -80,43 +80,25 @@ public class InterpolateXRMCFromProjectiveOperation extends AbstractOperation<In
 			throw new OperationException(this, "Error obtaining projective coordinate axes.");
 		}
 		
-		// TODO: Need output coordinate values on the input grid.
 		// 1. Output coordinates on the output grid
 		int[] size = new int[] {detProp.getPx(), detProp.getPy()};
-		int nBorder = 400;
-		int[] borderedSize = new int[] {size[0] + 2*nBorder, size[1] + 2*nBorder};
-		Dataset iArr = DatasetFactory.zeros(borderedSize);
-		Dataset jArr = DatasetFactory.zeros(borderedSize);
 		
 		// 2. Input coordinates on the output grid
-		Dataset xpArr = DatasetFactory.zeros(borderedSize);
-		Dataset ypArr = DatasetFactory.zeros(borderedSize);
+		Dataset xpArr = DatasetFactory.zeros(size);
+		Dataset ypArr = DatasetFactory.zeros(size);
 		
-		for (int i = 0; i < borderedSize[0]; i++) {
-			for (int j = 0; j < borderedSize[1]; j++) {
-				iArr.set(i-nBorder, i, j);
-				jArr.set(j-nBorder, i, j);
-				Vector3d r = detProp.pixelPosition(i+0.5-nBorder, j+0.5-nBorder);
+		for (int i = 0; i < size[0]; i++) {
+			for (int j = 0; j < size[1]; j++) {
+				Vector3d r = detProp.pixelPosition(j+0.5, i+0.5);
 				xpArr.set(r.x/r.z, i, j);
 				ypArr.set(r.y/r.z, i, j);
 			}
 		}
 		
-		// 3. Input coordinate axes (xp & yp)
+		// 3. Perform the interpolation
+		Dataset outputIntensity = Interpolation2D.interpolate2d(xp, yp, input, xpArr, ypArr, new PiecewiseBicubicSplineInterpolator());
 		
-		// 4. PixelIntegrationCache to go from the output grid to the input
-		GenericPixelIntegrationCache inFromOutPic = new GenericPixelIntegrationCache(ypArr, xpArr, xp, yp);
-
-		// 5. i,j coordinates of the output detector on the input grid
-		Dataset iOutOnIn = PixelIntegration.integrate(iArr, null, inFromOutPic).get(1);
-		Dataset jOutOnIn = PixelIntegration.integrate(jArr, null, inFromOutPic).get(1);
-		
-		// 6. PixelIntegrationCache to go from input to output grid
-		GenericPixelIntegrationCache outFromInPic = new GenericPixelIntegrationCache(iOutOnIn, jOutOnIn, DatasetFactory.createRange(size[0]),  DatasetFactory.createRange(size[1]));
-		
-		// 7. Interpolate the intensity from input to output
-		Dataset outputIntensity = PixelIntegration.integrate(input, null, outFromInPic).get(1);
-		
+		// 4. Create the operation data and away!
 		return new OperationData(outputIntensity);
 	}
 

708a5f19fd059b7a0bc832dddd4a1689663df0df
Tim Spain <timothy.spain@diamond.ac.uk>
2019-01-16 10:52:37 +0000

    Add a unit test for the 2D interpolation with 2D indices.
    Change-Id: I2389078aaf805b609174fc6b3743ddc3a1dee4c2

--- uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/dataset/function/Interpolation2DTest.java
+++ uk.ac.diamond.scisoft.analysis.test/src/uk/ac/diamond/scisoft/analysis/dataset/function/Interpolation2DTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
+import org.apache.commons.math3.analysis.interpolation.PiecewiseBicubicSplineInterpolator;
 import org.eclipse.january.asserts.TestUtils;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
@@ -98,5 +99,105 @@ public class Interpolation2DTest {
 		}
 		
 		return rv;
+	}
+
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void test2DIndices() {
+		
+		Dataset bigData = makeBigData();
+		Dataset smallData = makeSmallData();
+		Dataset oldAxis = DatasetFactory.createRange(smallnx);
+		
+		Dataset oldXOnNew = DatasetFactory.zeros(bigData);
+		Dataset oldYOnNew = DatasetFactory.zeros(bigData);
+		for (int i = 0; i < bignx; i++) {
+			for (int j = 0; j < bigny; j++) {
+				double[] oldCoords = oldCoordsFromNew(new double[] {i, j});
+				oldXOnNew.set(oldCoords[0], i, j);
+				oldYOnNew.set(oldCoords[1], i, j);
+			}
+		}
+		
+		Dataset interpolated = Interpolation2D.interpolate2d(oldAxis, oldAxis, smallData, oldXOnNew, oldYOnNew, new PiecewiseBicubicSplineInterpolator());
+
+		Dataset difference = Maths.subtract(interpolated, bigData);
+
+		double rmsDifference = Math.sqrt((double) Maths.square(difference).sum(true) / (bignx * bigny));
+		
+		assertTrue("RMS difference between directly calculated and interpolated values is too large", rmsDifference < 1e-2);
+	}
+	
+	private Dataset makeSmallData( ) {
+		Dataset smallData = DatasetFactory.zeros(DoubleDataset.class, new int[] {smallnx, smallny});
+		
+		for (int i = 0; i < smallnx; i++) {
+			for (int j = 0; j < smallny; j++) {
+				smallData.set(dataFromOldCoords(new double[] {i, j}), i, j);
+			}
+		}
+		
+		return smallData;
+	}
+	
+	private Dataset makeBigData( ) {
+		Dataset bigData = DatasetFactory.zeros(DoubleDataset.class, new int[] {bignx, bigny});
+		
+		for (int i = 0; i < bignx; i++) {
+			for (int j = 0; j < bigny; j++) {
+				bigData.set(dataFromOldCoords(oldCoordsFromNew(new double[] {i, j})), i, j);
+			}
+		}
+		
+		return bigData;
+	}
+	
+	private double[] oldCoordsFromNew(double[] newCoords) {
+		double theta = Math.toRadians(34.5);
+		double c = Math.cos(theta);
+		double s = Math.sin(theta);
+		double x0 = bignx/2;
+		double y0 = bigny/2;
+		// Rotate about the centre
+		double rotX = c*(newCoords[0]-x0) - s*(newCoords[1]-y0);
+		double rotY = s*(newCoords[0]-x0) + c*(newCoords[1]-y0);
+		
+		// Scale
+		double scale = Math.sqrt(s*c) * smallnx/bignx;
+		double scaleX = rotX*scale;
+		double scaleY = rotY*scale;
+		
+		// Shift the origin back
+		double oldX = scaleX + 50.5;
+		double oldY = scaleY + 50.5;
+		
+		return new double[] {oldX, oldY};
+	}
+	
+	private double dataFromOldCoords(double[] oldCoords) {
+		double wavenumberX = 2*Math.PI/30.;
+		double offsetX = 51;
+		double wavenumberY = 2*Math.PI/12.;
+		double offsetY = 51;
+		double wavenumberR = 2*Math.PI/50.;
+		double offsetR = 14;
+		
+		double cartesianPhase = wavenumberX * (oldCoords[0] - offsetX) + wavenumberY * (oldCoords[1] - offsetY);
+		double radialPhase = wavenumberR * Math.sqrt(square(oldCoords[0] - offsetX) + square(oldCoords[1] - offsetY) - offsetR);
+		
+		return Math.sin(cartesianPhase + radialPhase);
+	}
+	
+	private static final int bignx = 288;
+	private static final int bigny = 288;
+	
+	private static final int smallnx = 101;
+	private static final int smallny = 101;
+
+	private double square(double x) {
+		return x*x;
 	}
 }

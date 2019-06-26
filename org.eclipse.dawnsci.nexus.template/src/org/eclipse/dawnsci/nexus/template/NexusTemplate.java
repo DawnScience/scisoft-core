e13afaabac2c930e1a2a794953b654bcda674981
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2019-06-26 11:50:45 +0100

    [DAQ-2188] add logging to OnDiskNexusContext and InMemoryNexusContext
    Change-Id: Ib81a35e86fc17bd6b6d83e7ed4cc83de6244f24d

--- org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/NexusTemplate.java
+++ org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/NexusTemplate.java
@@ -1,18 +1,36 @@
 package org.eclipse.dawnsci.nexus.template;
 
-import org.eclipse.dawnsci.nexus.NXroot;
+import org.eclipse.dawnsci.analysis.api.tree.Tree;
 import org.eclipse.dawnsci.nexus.NexusException;
 
 /**
- * TODO javadoc
+ * A nexus template that can be applied to a nexus file one of two ways:<ul>
+ * <li>by calling {@link #apply(String)} with a the file path of the nexus file</li>
+ * <li>by calling {@link #apply(Tree)} with the {@link Tree} object at the root of an
+ *   in-memory nexus tree.</li>
+ * </ul>
  * 
  * @author wgp76868
  *
  */
 public interface NexusTemplate {
 	
-	public void apply(NXroot nexusRoot) throws NexusException;
+	/**
+	 * Applies the nexus template to the given in-memory NeXus {@link Tree}.
+	 * @param nexusTree the nexus tree to apply the template to
+	 * @throws NexusException if the template could not be fully applied for any reason,
+	 *    note the template may have been partially applied, so the tree may have some
+	 *    modifications
+	 */
+	public void apply(Tree nexusTree) throws NexusException;
 	
+	/**
+	 * Applies the nexus template to the NeXus file at the given file path.
+	 * @param nexusTree the path of the NeXus file to apply the template to
+	 * @throws NexusException if the template could not be fully applied for any reason,
+	 *    note the template may have been partially applied, so the file may have some
+	 *    modifications
+	 */
 	public void apply(String nexusFilePath) throws NexusException;
 	
 }
--- org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/impl/tree/InMemoryNexusContext.java
+++ org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/impl/tree/InMemoryNexusContext.java
@@ -7,38 +7,58 @@ import org.eclipse.dawnsci.analysis.api.tree.DataNode;
 import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
 import org.eclipse.dawnsci.analysis.api.tree.Node;
 import org.eclipse.dawnsci.analysis.api.tree.SymbolicNode;
+import org.eclipse.dawnsci.analysis.api.tree.Tree;
+import org.eclipse.dawnsci.analysis.api.tree.TreeUtils;
 import org.eclipse.dawnsci.analysis.tree.TreeFactory;
 import org.eclipse.dawnsci.nexus.NXobject;
 import org.eclipse.dawnsci.nexus.NXroot;
 import org.eclipse.dawnsci.nexus.NexusBaseClass;
 import org.eclipse.dawnsci.nexus.NexusException;
 import org.eclipse.dawnsci.nexus.NexusNodeFactory;
+import org.eclipse.dawnsci.nexus.NexusUtils;
 import org.eclipse.dawnsci.nexus.template.NexusTemplateConstants.ApplicationMode;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.IDataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A {@link NexusContext} to abstract adding nodes to an in-memory nexus tree.
  */
 public class InMemoryNexusContext implements NexusContext {
 	
-	private final NXroot root;
+	private static final Logger logger = LoggerFactory.getLogger(InMemoryNexusContext.class);
 	
-	public InMemoryNexusContext(NXroot root) {
-		this.root = root;
+	private final Tree tree;
+	
+	public InMemoryNexusContext(Tree tree) {
+		this.tree = tree;
 	}
 	
 	@Override
 	public ApplicationMode getApplicationMode() {
 		return ApplicationMode.IN_MEMORY;
 	}
-
+	
+	private void logDebug(String pattern, Object... arguments) {
+		if (logger.isDebugEnabled()) {
+			// replace any nodes with their paths within the nexus file
+			// getting the path uses a breadth-first search, so we only do it if we have to
+			for (int i = 0; i < arguments.length; i++) {
+				if (arguments[i] instanceof Node) {
+					arguments[i] = TreeUtils.getPath(tree, (Node) arguments[i]);
+				}
+			}
+			logger.debug(pattern, arguments);
+		}
+	}
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.dawnsci.nexus.template.impl.tree.NexusContext#getNexusRoot()
 	 */
 	@Override
 	public GroupNode getNexusRoot() {
-		return root;
+		return tree.getGroupNode();
 	}
 
 	/* (non-Javadoc)
@@ -46,6 +66,7 @@ public class InMemoryNexusContext implements NexusContext {
 	 */
 	@Override
 	public GroupNode addGroupNode(GroupNode parent, String name, NexusBaseClass nexusBaseClass) {
+		logDebug("Creating new group node with name '{}' and nexus class '{}' to parent group '{}'", name, nexusBaseClass, parent);
 		NXobject group = NexusNodeFactory.createNXobjectForClass(nexusBaseClass);
 		parent.addGroupNode(name, group);
 		return group;
@@ -56,6 +77,7 @@ public class InMemoryNexusContext implements NexusContext {
 	 */
 	@Override
 	public DataNode addDataNode(GroupNode parent, String name, Object value) {
+		logDebug("Creating new data node with name '{}' and value '{}' to parent group '{}'", name, value, parent);
 		final DataNode dataNode = NexusNodeFactory.createDataNode();
 		parent.addDataNode(name, dataNode);
 		dataNode.setDataset(DatasetFactory.createFromObject(value));
@@ -67,19 +89,23 @@ public class InMemoryNexusContext implements NexusContext {
 	 */
 	@Override
 	public void addLink(GroupNode parent, String name, String linkPath) throws NexusException {
+		logDebug("Linking node or attribute at path '{}' to parent '{}' with name '{}'", linkPath, parent, name); 
 		final Object nodeOrAttr = getNodeOrAttribute(linkPath);
 		if (nodeOrAttr == null) {
 			throw new NexusException("Invalid link path, no such node: " + linkPath);
 		} else if (nodeOrAttr instanceof Attribute) {
+			logDebug("Copying attribute '{}' with name '{}' to parent '{}'", linkPath, name, parent); 
 			final IDataset data = ((Attribute) nodeOrAttr).getValue();
 			final Attribute newAttr = TreeFactory.createAttribute(name, data.clone());
 			parent.addAttribute(newAttr);
 		} else if (((Node) nodeOrAttr).isSymbolicNode()) { // must a Node if not an Attribute
+			logDebug("Linking node '{}' with name '{}' to parent '{}'", linkPath, name, parent); 
 			final SymbolicNode existingLinkNode = (SymbolicNode) nodeOrAttr;
 			final SymbolicNode newLinkNode = NexusNodeFactory.createSymbolicNode(
 					existingLinkNode.getSourceURI(), existingLinkNode.getPath());
 			parent.addSymbolicNode(name, newLinkNode);
 		} else {
+			logDebug("Linking node at path '{}' to parent '{}' with name '{}'", linkPath, parent, name); 
 			parent.addNode(name, (Node) nodeOrAttr);
 		}
 	}
@@ -92,7 +118,7 @@ public class InMemoryNexusContext implements NexusContext {
 			throw new NexusException("Invalid link path: " + linkPath);
 		}
 		
-		Node node = root;
+		Node node = tree.getGroupNode();
 		for (int i = 1; i < pathSegments.length; i++) {
 			final String pathSegment = pathSegments[i];
 			if (i == pathSegments.length - 1 && pathSegment.endsWith(String.valueOf(ATTRIBUTE_SUFFIX))) {

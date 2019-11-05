30918948f131defa861e01157417053005acf151
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-11-05 11:34:51 +0000

    Bump version: 2.16.0 → 2.17.0
    Change-Id: I8eac77450cb34746a4b2da036742713b089440eb

--- uk.ac.diamond.scisoft.python/src/setup.py
+++ uk.ac.diamond.scisoft.python/src/setup.py
@@ -12,7 +12,7 @@ if py2 and os.name == 'posix':
 
 setup(
     name="scisoftpy",
-    version="2.16.0",
+    version="2.17.0",
     description="DAWN Python Extensions",
     author="Peter Chang",
     author_email="scientificsoftware@diamond.ac.uk",

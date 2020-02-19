66a4f2a6356a98899b7c70abe31784ef01730e6f
Chris Kelley <chris.kelley@diamond.ac.uk>
2020-02-19 10:41:26 +0000

    Remove unused imports
    Change-Id: I01a963868042fd82a3b3ef2b0d63513e18548a8d

--- org.eclipse.dawnsci.analysis.dataset.test/src/org/eclipse/dawnsci/analysis/dataset/operations/TestOperation1to1.java
+++ org.eclipse.dawnsci.analysis.dataset.test/src/org/eclipse/dawnsci/analysis/dataset/operations/TestOperation1to1.java
@@ -3,9 +3,7 @@ package org.eclipse.dawnsci.analysis.dataset.operations;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.api.processing.model.EmptyModel;
 import org.eclipse.january.IMonitor;
-import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.IDataset;
 
 public class TestOperation1to1 extends AbstractTestOperation {

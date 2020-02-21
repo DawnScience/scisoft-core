0320c029b13db66812751a6830b204c8767fdb51
Peter Chang <peter.chang@diamond.ac.uk>
2020-02-21 13:34:51 +0000

    Add elastic line specific cutoff options to combine RIXS operation for I21-794
    Change-Id: Iee427cda79157eff03c8d0f45721db0f8a52fb76

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/rixs/RixsImageCombinedReductionModel.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/rixs/RixsImageCombinedReductionModel.java
@@ -23,6 +23,15 @@ public class RixsImageCombinedReductionModel extends RixsImageReductionBaseModel
 	@OperationModelField(label = "Elastic line scan", description = "Used to work out which scan file to find an elastic line", hint = "Use next (or same or previous) scan")
 	private SCAN_OPTION scanOption = SCAN_OPTION.NEXT_SCAN;
 
+	@OperationModelField(label = "Elastic line cutoff enable", description = "Use cutoff to remove pixels with high counts", expertOnly = true)
+	private boolean elUseCutoff = false;
+
+	@OperationModelField(label = "Elastic line cutoff for pixels", description = "Cutoff as multiple of single photon count", hint = "Check if peaks are clipped when cutoff is too low", enableif = "elUseCutoff == true", min = 1.0, expertOnly = true)
+	private double elCutoff = 5.0;
+
+	@OperationModelField(label = "Elastic line cutoff window size", description = "Size of window to use for cutoff", hint = "Cuts out a (size, size) window", enableif = "elUseCutoff == true", min = 1, expertOnly = true)
+	private int elCutoffSize = 1;
+
 	/**
 	 * @return option to work out which scan file to use
 	 */
@@ -47,4 +56,38 @@ public class RixsImageCombinedReductionModel extends RixsImageReductionBaseModel
 	public void setDelta(int delta) {
 		firePropertyChange("setRatio", this.delta, this.delta = delta);
 	}
+
+	/**
+	 * @return true if cutoff will be used for elastic line reduction
+	 */
+	public boolean isElUseCutoff() {
+		return elUseCutoff;
+	}
+
+	public void setElUseCutoff(boolean useCutoff) {
+		firePropertyChange("setUseCutoff", this.elUseCutoff, this.elUseCutoff = useCutoff);
+	}
+
+	/**
+	 * @return upper threshold multiple of single photon (in detector count units) used to ignore pixels for elastic line reduction
+	 */
+	public double getElCutoff() {
+		return elCutoff;
+	}
+
+	public void setElCutoff(double cutoff) {
+		firePropertyChange("setCutoff", this.elCutoff, this.elCutoff = cutoff);
+	}
+
+	/**
+	 * @return size of window to use for cutoff of pixels that exceed threshold for elastic line reduction
+	 */
+	public int getElCutoffSize() {
+		return elCutoffSize;
+	}
+
+	public void setElCutoffSize(int cutoffSize) {
+		firePropertyChange("setCutoff", this.elCutoffSize, this.elCutoffSize = cutoffSize);
+	}
+
 }

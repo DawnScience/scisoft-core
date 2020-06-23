de911867e6dc2aa87276bc07734813ac2c2ebca7
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2020-06-23 15:10:17 +0100

    [DAQ-2991] add method to set template as string to template device and appender
    Change-Id: Idc61a8210f28031afd65a3f97fe34d46bdb4b581

--- org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/device/NexusTemplateAppender.java
+++ org.eclipse.dawnsci.nexus.template/src/org/eclipse/dawnsci/nexus/template/device/NexusTemplateAppender.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
+import org.eclipse.dawnsci.nexus.INexusDevice;
 import org.eclipse.dawnsci.nexus.NXobject;
 import org.eclipse.dawnsci.nexus.NexusException;
 import org.eclipse.dawnsci.nexus.appender.AbstractNexusContextAppender;
@@ -15,6 +16,14 @@ import org.eclipse.dawnsci.nexus.template.impl.NexusTemplateImpl;
 /**
  * An appender that appends to a nexus object according to a template. This can be used
  * to append to nexus objects both in-memory and on-disk.
+ * <p>
+ * The template can be specified in one of three ways:
+ * <ul>
+ *   <li>by directly setting a template map using {@link #setTemplateMap(Map)};</li>
+ *   <li>by specifying the path to the template file by using {@link #setTemplateFilePath(String)};</li>
+ *   <li>or as a string by using {@link #setTemplateString(String)}.</li>
+ * </ul>
+ * This can be done easily in the spring configuration. <em>Note: only call one of these set methods should be called</em>.
  * 
  * @author Matthew Dickie
  *
@@ -25,6 +34,8 @@ public class NexusTemplateAppender<N extends NXobject> extends AbstractNexusCont
 	private NexusTemplate nexusTemplate;
 	
 	private String templateFilePath;
+	
+	private String templateString;
 	
 	public NexusTemplateAppender() {
 		// no-arg constructor for spring configuration
@@ -43,10 +54,20 @@ public class NexusTemplateAppender<N extends NXobject> extends AbstractNexusCont
 		this.templateFilePath = templateFilePath; 
 	}
 	
+	public void setTemplateString(String templateString) {
+		this.templateString = templateString;	
+	}
+
 	private void checkInitialized() throws NexusException {
 		if (nexusTemplate == null) {
-			Objects.requireNonNull(templateFilePath);
-			nexusTemplate = TemplateServiceHolder.getNexusTemplateService().loadTemplate(templateFilePath);
+			if (templateFilePath != null) {
+				nexusTemplate = TemplateServiceHolder.getNexusTemplateService().loadTemplate(templateFilePath);
+			} else if (templateString != null) {
+				nexusTemplate = TemplateServiceHolder.getNexusTemplateService().createTemplateFromString(templateString);
+			} else {
+				throw new NexusException("The appender has not been initialized. "
+						+ "One of the set methods should be called before using this class");
+			}
 		}
 	}
 	

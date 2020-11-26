9aed600642141e562d2dc1eaa40e40bcbde2e9b3
Peter Chang <peter.chang@diamond.ac.uk>
2020-11-26 23:59:35 +0000

    Clean up OSUtils
    Add macOS checker, remove unused methods and use of private properties
    
    Change-Id: I2e006184b514c5e0cb6d03ec510a254c76660d0e

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/utils/OSUtils.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/utils/OSUtils.java
@@ -13,47 +13,33 @@ package uk.ac.diamond.scisoft.analysis.utils;
  */
 public class OSUtils {
 	/**
-	 * @return true if windows
+	 * @return true if Windows
 	 */
 	static public boolean isWindowsOS() {
-		return (System.getProperty("os.name").indexOf("Windows") == 0);
+		return System.getProperty("os.name").startsWith("Windows");
 	}
+
 	/**
-	 * @return true if XP
-	 */
-	static public boolean isWindowsXP() {
-		if (!isWindowsOS()) return false;
-		return (System.getProperty("os.name").indexOf("Windows XP") == 0);
-	}
-	/**
-	 * @return true if vista
-	 */
-	static public boolean isWindowsVista() {
-		if (!isWindowsOS()) return false;
-		return "Windows Vista".equalsIgnoreCase(System.getProperty("os.name"));
-	}
-	/**
-	 * @return true if linux
+	 * @return true if Linux
 	 */
 	public static boolean isLinuxOS() {
 		String os = System.getProperty("os.name");
 		return os != null && os.startsWith("Linux");
 	}
+
 	/**
-	 * @return true if 32-bit JVM
-	 * CAUTION: this does not seem to be particularly portable (the system property is specific to the Sun (now Oracle) JVM)
+	 * @return true if macOS
 	 */
-	public static boolean is32bitJVM() {
-		String os = System.getProperty("sun.arch.data.model");
-		return os != null && os.equals("32");
-	}
-	/**
-	 * @return true if 64-bit JVM
-	 * CAUTION: this does not seem to be particularly portable (the system property is specific to the Sun (now Oracle) JVM)
-	 */
-	public static boolean is64bitJVM() {
-		String os = System.getProperty("sun.arch.data.model");
-		return os != null && os.equals("64");
+	public static boolean isMacOS() {
+		String os = System.getProperty("os.name");
+		return os != null && os.startsWith("Mac OS X");
 	}
 
+	/**
+	 * @return false
+	 */
+	@Deprecated
+	public static boolean is32bitJVM() {
+		return false; // don't run on 32-bit anymore
+	}
 }

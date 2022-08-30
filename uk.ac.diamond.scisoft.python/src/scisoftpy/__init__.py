bfa353325f8668a803e0cbb85b401cbe80caa02d
Peter Chang <peter.chang@diamond.ac.uk>
2022-08-30 13:16:41 +0100

    Bump version: 2.25.0 → 2.27.0
    Change-Id: I6b21194254e92f209318cb0af9fe3bbf44f2dc47

--- uk.ac.diamond.scisoft.python/src/scisoftpy/__init__.py
+++ uk.ac.diamond.scisoft.python/src/scisoftpy/__init__.py
@@ -130,7 +130,7 @@ else:
     from .python.pycomparisons import *
     from .python.pyscisoft import *
 
-__version__ = "2.25.0"
+__version__ = "2.27.0"
 
 '''
 Imports should work with python+numpy only agreed with MB 11 Nov 2011

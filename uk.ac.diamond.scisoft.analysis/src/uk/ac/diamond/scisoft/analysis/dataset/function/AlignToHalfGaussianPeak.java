3f5b62213e6a1054260d64ed13202082e013bd5c
Peter Chang <peter.chang@diamond.ac.uk>
2024-04-10 16:30:15 +0100

    Switch to aligning in x values
    This fixes issue with using indexes for spectra that have decreasing x
    values and different sizes
    
    Change-Id: Ib1db8cb11ee016459ccc234ed7735bbec91386cc

--- uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/dataset/function/AlignToHalfGaussianPeak.java
+++ uk.ac.diamond.scisoft.analysis/src/uk/ac/diamond/scisoft/analysis/dataset/function/AlignToHalfGaussianPeak.java
@@ -10,15 +10,18 @@
 package uk.ac.diamond.scisoft.analysis.dataset.function;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.fitting.functions.IParameter;
 import org.eclipse.dawnsci.analysis.dataset.impl.function.DatasetToNumberFunction;
+import org.eclipse.january.dataset.AbstractDataset;
 import org.eclipse.january.dataset.Comparisons;
 import org.eclipse.january.dataset.Comparisons.Monotonicity;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.DatasetUtils;
+import org.eclipse.january.dataset.DoubleDataset;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.Maths;
 import org.eclipse.january.dataset.Slice;
@@ -36,7 +39,8 @@ public class AlignToHalfGaussianPeak implements DatasetToNumberFunction {
 
 	private static final Logger logger = LoggerFactory.getLogger(AlignToHalfGaussianPeak.class);
 	private boolean useRisingSide = false;
-	private double lx = -Double.MAX_VALUE, hx = Double.MAX_VALUE;
+	private double lx = -Double.MAX_VALUE;
+	private double hx = Double.MAX_VALUE;
 
 	private DiffGaussian dg = new DiffGaussian();
 
@@ -62,227 +66,161 @@ public class AlignToHalfGaussianPeak implements DatasetToNumberFunction {
 
 	/**
 	 * @param datasets x/y pairs of datasets
-	 * @return fitted positions of derivative of Gaussian (in index values)
+	 * @return fitted positions of derivative of Gaussian (in x values)
 	 */
 	@Override
 	public List<Double> value(IDataset... datasets) {
-		List<Double> iPositions = new ArrayList<>(); // positions of derivative of Gaussian in index values
+		List<Double> xPositions = new ArrayList<>(); // positions of derivative of Gaussian in x values
 
 		final int imax = datasets.length;
 		for (int i = 0; i < imax; i += 2) {
 			Dataset x = DatasetUtils.convertToDataset(datasets[i]);
 			Dataset y = DatasetUtils.convertToDataset(datasets[i + 1]);
 			Monotonicity m = Comparisons.findMonotonicity(x);
-			Slice cs = findCroppingSlice(m, x, Math.min(x.getSize(), y.getSize()), lx, hx);
-			if (cs == null) {
-				logger.warn("Trace {} ignored as it is not strictly monotonic", i); //t.getName());
-				iPositions.add(null);
+			if (m != Monotonicity.STRICTLY_DECREASING && m != Monotonicity.STRICTLY_INCREASING) {
+				logger.warn("Trace {} ignored as it is not strictly monotonic", i);
+				xPositions.add(null);
 				continue;
 			}
+			boolean isXInc = m == Monotonicity.STRICTLY_INCREASING;
+			Slice cs = findCroppingSlice(isXInc, x, Math.min(x.getSize(), y.getSize()) - 1, lx, hx);
 
-			logger.debug("Cropping to {}", cs);
+			logger.info("Cropping to {}", cs);
+			Dataset cx = x.getSliceView(cs);
 			Dataset cy = y.getSliceView(cs);
+			if (!isXInc) {
+				Slice sf = new Slice(null, null, -1);
+				cx = cx.getSliceView(sf);
+				cy = cy.getSliceView(sf);
+			}
 
 			// look for 1st peak in derivative
-			Dataset dy = Maths.centralDifference(cy, 0);
+			Dataset dy = Maths.derivative(cx, cy, 1);
 			int pos = useRisingSide ? dy.argMax(true) : dy.argMin(true);
 
 			// fit to derivative of Gaussian
-			double c = fitDiffGaussian(dg, dy, cy.max(true).doubleValue()/5., pos);
-			iPositions.add(cs.getStart() + c);
+			double c = fitDiffGaussian(dg, cx, dy, cy.max(true).doubleValue()/5., pos);
+			xPositions.add(c);
 		}
 
-		logger.debug("Positions are {}", iPositions);
-		return iPositions;
+		logger.debug("Positions are {}", xPositions);
+		return xPositions;
 	}
 
 	/**
-	 * Align data to given positions
+	 * Align data to given x positions
 	 * @param resample
-	 * @param forceToPosition
-	 * @param position
-	 * @param iPositions
+	 * @param firstXPos if NaN, then do not align to given value
+	 * @param xPositions
 	 * @param datasets
 	 * @return x/y pairs of datasets where x and/or y have been shifted to align data
 	 */
-	public static Dataset[] alignToPositions(boolean resample, boolean forceToPosition, double position, List<Double> iPositions, Dataset... datasets) {
-		List<Double> shifts = calculateShifts(resample, forceToPosition, position, iPositions, datasets);
-
-		return shiftData(shifts, datasets);
+	public static Dataset[] alignToPositions(boolean resample, double firstXPos, List<Double> xPositions, Dataset[] datasets) {
+		if (xPositions.size() < 2) {
+			return datasets;
+		}
+		return resample ? resampleToPositions(firstXPos, xPositions, datasets) : shiftToPositions(firstXPos, xPositions, datasets);
 	}
 
-	/**
-	 * Calculate shifts for aligning datasets
-	 * @param resample
-	 * @param forceToPosition
-	 * @param position
-	 * @param iPositions
-	 * @param datasets
-	 * @return x/y pairs of shifts (y shifts are in index space)
-	 */
-	public static List<Double> calculateShifts(boolean resample, boolean forceToPosition, double position, List<Double> iPositions, Dataset... datasets) {
-		List<Double> shifts = new ArrayList<>();
-		if (resample) {
-			int firstI = 0;
-			if (forceToPosition) {
-				firstI = calcFirstXShift(shifts, datasets, iPositions);
-			} else {
-				firstI = calcXRelativeToFirstX(shifts, datasets, iPositions, Double.NaN, true);
+	private static Dataset[] resampleToPositions(double alignXPos, List<Double> posn, Dataset[] in) {
+		double firstXPos = Double.NaN;
+		int ip = 0;
+		int imax = posn.size() - 1;
+		for (; ip <= imax; ip++) {
+			Double p = posn.get(ip);
+			if (p != null && Double.isFinite(p)) {
+				firstXPos = p;
+				break;
 			}
-			calcYShifts(shifts, iPositions, firstI);
+		}
+		if (Double.isNaN(firstXPos) || ip  >= imax) {
+			return in;
+		}
+
+		Dataset xo = in[2 * ip];
+		double firstStep = xo.getDouble(1) - xo.getDouble();
+
+		Dataset[] result = Arrays.copyOf(in, in.length);
+		resampleToPosn(ip++, firstXPos, firstXPos, firstStep, result);
+
+		// shift first
+		while (ip <= imax) {
+			Double x = posn.get(ip);
+			if (x != null && Double.isFinite(x)) {
+				resampleToPosn(ip, x, firstXPos, firstStep, result);
+			}
+			ip++;
+		}
+
+		double delta = alignXPos - firstXPos;
+		if (Double.isFinite(delta) && delta != 0) {
+			for (ip = 0; ip <= imax; ip++) {
+				result[2 * ip] = Maths.add(result[2 * ip], delta);
+			}
+		}
+
+		return result;
+	}
+
+	private static void resampleToPosn(int ip, double x, double firstX, double firstStep, Dataset[] result) {
+		int ci = 2 * ip;
+		Dataset xo = result[ci];
+		double xShift = firstX - x;
+		double xSteps = xShift / firstStep;
+		if (xSteps > 1) {
+			xShift -= Math.floor(xSteps) * firstStep;
+		}
+		double xb = xo.getDouble();
+		double s = (x - xb) / firstStep;
+		int si = (int) Math.floor(s);
+		if (s - si > 1e-15) { // not on sample point
+			int size = (int) Math.floor((xo.getDouble(-1) - xb)/ firstStep);
+			double sx = x - si * firstStep;
+			DoubleDataset xn = DatasetFactory.createRange(size).imultiply(firstStep).iadd(sx);
+			Dataset yn = Maths.interpolate(xo, result[ci + 1], xn, null, null);
+			xn.iadd(xShift);
+			result[ci] = xn;
+			result[++ci] = yn;
 		} else {
-			calcXRelativeToFirstX(shifts, datasets, iPositions, forceToPosition ? position : Double.NaN, false);
-		}
-
-		System.err.println("Shifts are " + shifts);
-		return shifts;
-	}
-
-	/**
-	 * Shift first X and set all other X to same
-	 * @param shifts (should be empty)
-	 * @param data
-	 * @param iPositions
-	 * @return index of first X with valid position
-	 */
-	private static int calcFirstXShift(List<Double> shifts, Dataset[] data, List<Double> iPositions) {
-		Dataset firstX = null;
-		int i = 0;
-		int firstI = 0;
-		for (Double p : iPositions) {
-			if (p != null && Double.isFinite(p) && firstX == null) {
-				firstX = data[i];
-				double xPos = Maths.interpolate(firstX, p);
-				logger.debug("Shifting 1st x {} by {}", i, xPos);
-				shifts.add(-xPos);
-				firstI = i;
-			} else {
-				shifts.add(null);
-			}
-			shifts.add(null); // placeholder for y shift
-			i += 2;
-		}
-
-		return firstI;
-	}
-
-	/**
-	 * Shift rest (if leaveRest is false) relative to first X position
-	 * @param shifts empty list to populate
-	 * @param data x/y pairs of 
-	 * @param iPositions
-	 * @param firstXPos can be null to automatically find first X with valid position
-	 * @param leaveRest if true, then do not shift any
-	 * @return index of first X with valid position
-	 */
-	private static int calcXRelativeToFirstX(List<Double> shifts, Dataset[] data, List<Double> iPositions, double firstXPos, boolean leaveRest) {
-		int i = 0;
-		int firstI = 0;
-		for (Double p : iPositions) {
-			if (p != null && Double.isFinite(p)) {
-				Dataset x = data[i];
-				if (Double.isNaN(firstXPos)) {
-					firstXPos = Maths.interpolate(x, p);
-					shifts.add(null); // leave first unshifted
-					firstI = i;
-				} else {
-					if (leaveRest) {
-						shifts.add(null);
-					} else {
-						double delta = Maths.interpolate(x, p) - firstXPos;
-						logger.debug("Shifting x {} by {}", i, delta);
-						shifts.add(-delta);
-					}
-				}
-			} else {
-				shifts.add(null);
-			}
-			shifts.add(null); // placeholder for y shift
-			i += 2;
-		}
-
-		return firstI;
-	}
-
-	/**
-	 * Calculate shift to resample Y data
-	 * @param shifts
-	 * @param iPositions
-	 * @param firstI
-	 */
-	private static void calcYShifts(List<Double> shifts, List<Double> iPositions, int firstI) {
-		double firstIShift = iPositions.get(firstI);
-		for (int i = firstI, imax = shifts.size(); i < imax; i += 2) {
-			Double p = iPositions.get(i / 2);
-			if (p != null && Double.isFinite(p)) {
-				double delta = p - firstIShift;
-				if (delta != 0) {
-					logger.debug("Resampling {} by {}", i + 1, delta);
-					shifts.set(i + 1, delta);
-				}
-			}
+			result[ci] = Maths.add(xo, xShift);
 		}
 	}
 
 	/**
-	 * Shift data by given shifts
-	 * @param shifts x/y pairs of shifts
-	 * @param datasets
+	 * Shift data by given shifts relative to first X position
+	 * @param firstXPos can be NaN to automatically find first X with valid position
+	 * @param positions
+	 * @param in
 	 * @return x/y pairs of datasets where x and/or y have been shifted to align data
 	 */
-	public static Dataset[] shiftData(List<Double> shifts, Dataset... datasets) {
-		final int imax = datasets.length;
+	private static Dataset[] shiftToPositions(double firstXPos, List<Double> positions, Dataset[] in) {
+		int i = 0;
+		final int imax = in.length;
 		Dataset[] result = new Dataset[imax];
-		double[] firstXShift = {Double.NaN};
-		for (int i = 0; i < imax; i += 2) {
-			Dataset[] shifted = shiftData(firstXShift, shifts.get(i), shifts.get(i + 1), datasets[i], datasets[i+1]);
-			result[i] = shifted[0];
-			result[i + 1] = shifted[1];
+		for (Double p : positions) {
+			double delta = 0;
+			if (p != null && Double.isFinite(p)) {
+				if (Double.isNaN(firstXPos)) {
+					firstXPos = p;
+				} else {
+					delta = firstXPos - p;
+				}
+			}
+			if (delta != 0) {
+				result[i] = Maths.add(in[i], delta);
+			} else {
+				result[i] = in[i];
+			}
+			result[i + 1] = in[i + 1];
+			i += 2;
 		}
 		return result;
 	}
 
-	/**
-	 * Shift data by given shifts
-	 * @param shiftX x shift
-	 * @param shiftY y shift
-	 * @param x
-	 * @param y
-	 * @return x/y pair of datasets where x and/or y have been shifted to align data
-	 */
-	public static Dataset[] shiftData(double[] firstXShift, Double shiftX, Double shiftY, Dataset x, Dataset y) {
-		String yName = null;
-		if (shiftX != null) {
-			if (shiftX != 0) {
-				x = Maths.add(x, shiftX);
-				if (Double.isNaN(firstXShift[0])) {
-					firstXShift[0] = shiftX;
-				}
-			}
-		} else {
-			if (shiftY != null) { // resample case
-				if (shiftY != 0) {
-					// assume x is uniformly spaced (otherwise we need to interpolate by new x values)
-					yName = y.getName();
-					y = RegisterData1D.shiftData(y, shiftY);
-					if (Double.isFinite(firstXShift[0])) {
-						x = Maths.add(x, firstXShift[0]);
-					}
-				}
-			}
-		}
-		if (yName != null) {
-			y.setName(yName);
-		}
-		return new Dataset[] {x, y};
-	}
-
-	private static Slice findCroppingSlice(Monotonicity m, Dataset x, int l, double lx, double hx) {
-		if (m == Monotonicity.STRICTLY_DECREASING) {
-			Slice s = new Slice(l - 1, null, -1);
+	private static Slice findCroppingSlice(boolean isXInc, Dataset x, int max, double lx, double hx) {
+		if (!isXInc) {
+			Slice s = new Slice(max, null, -1);
 			x = x.getSliceView(s);
-		} else if (m != Monotonicity.STRICTLY_INCREASING) {
-			return null;
 		}
 	
 		// slice plot according to x interval
@@ -290,95 +228,98 @@ public class AlignToHalfGaussianPeak implements DatasetToNumberFunction {
 		int li = 0;
 		if (lx > x.getDouble(li)) {
 			List<Double> c = DatasetUtils.crossings(x, lx);
-			if (c.size() > 0) {
+			if (!c.isEmpty()) {
 				li = (int) Math.ceil(c.get(0));
 			}
 		}
 	
-		int hi = l - 1;
+		int hi = max;
 		if (hx < x.getDouble(hi)) {
 			List<Double> c = DatasetUtils.crossings(x, hx);
-			if (c.size() > 0) {
+			if (!c.isEmpty()) {
 				hi = (int) Math.floor(c.get(0));
 			}
 		}
 		assert hi > li;
 	
-		return m == Monotonicity.STRICTLY_INCREASING ? new Slice(li, hi) : new Slice(l - 1 - hi, l - 1 - li);
+		return isXInc ? new Slice(li, hi) : new Slice(max - hi, max - li);
 	}
 
 	/**
-	 * TODO allow x dataset too
 	 * @param dg
+	 * @param x
 	 * @param dy
 	 * @param peak
 	 * @param pos
 	 * @return position of differentiated Gaussian
 	 */
-	private static double fitDiffGaussian(DiffGaussian dg, Dataset dy, double peak, int pos) {
+	private static double fitDiffGaussian(DiffGaussian dg, Dataset x, Dataset dy, double peak, int pos) {
 		double hpy = 0.5 * dy.getDouble(pos);
-		boolean neg = hpy < 0;
-		int max = dy.getSize();
+		int max = dy.getSize() - 1;
 
 		int hwhm;
-		int beg, end;
-		if (neg) {
-			int pw = pos + 1;
-			// find range to fit from peak to half-peak distance
-			while (pw < max && dy.getDouble(pw) < hpy) {
-				pw++;
-			}
-			if (pw >= max) {
+		int beg;
+		int end;
+		int pw = pos;
+		// find range to fit from peak to half-peak distance
+		if (hpy < 0) {
+			while (++pw <= max && dy.getDouble(pw) <= hpy);
+			if (pw > max) {
 				logger.warn("Could not find closest mid-height point");
 				return Double.NaN;
 			}
-			hwhm = Math.max(2, pw - pos);
+			hwhm = Math.max(2, pos - pw);
 			// work out where zero crossing ends
-			if (pos <= 0) {
-				beg = 0;
-			} else {
-				beg = pos - 1;
-				while (dy.getDouble(beg) <= 0 && --beg >= 0) {
-				}
-				beg++;
-			}
-			end = Math.min(max, pos + 4*hwhm);
+			beg = pos;
+			while (--beg >= 0 && dy.getDouble(beg) <= 0);
+			beg = Math.max(beg, 0);
+			end = Math.min(max, pos + 3*hwhm);
 		} else {
-			int pw = pos - 1;
-			while (dy.getDouble(pw) > hpy && --pw >= 0) {
-			}
+			while (--pw >= 0 && dy.getDouble(pw) >= hpy);
 			if (pw < 0) {
 				logger.warn("Could not find closest mid-height point");
 				return Double.NaN;
 			}
 			hwhm = Math.max(2, pos - pw);
-			beg = Math.max(0, pos - 4*hwhm);
-			end = pos + 1;
-			while (dy.getDouble(end) >= 0 && ++end < max) {
+			beg = Math.max(0, pos - 3*hwhm);
+			end = pos;
+			while (++end <= max && dy.getDouble(end) >= 0);
+			end = Math.min(end, max);
+			if (end < max - 1) { // include crossing
+				end++;
 			}
 		}
+
 		Dataset cdy = dy.getSliceView(new Slice(beg, end));
 		logger.info("Using hw of {} in {}:{}", hwhm, beg, end);
-		logger.info(cdy.toString(true));
-		Dataset cdx = DatasetFactory.createRange(beg, end, 1.0);
+		Dataset cdx = x.getSliceView(new Slice(beg, end));
 
 		IParameter p = dg.getParameter(0);
-		double cen = neg ? beg : end;
-		dg.setParameterValues(cen - 0.5, peak, 1e-1/hwhm);
-		p.setLimits(cen - 1, cen);
+		double step = cdx.getDouble(1) - cdx.getDouble();
+		double cx = x.getDouble(pos);
+		double width = hwhm*step;
+		dg.setParameterValues(cx, peak, 1/(width*width));
+		p.setLimits(cdx.getDouble(), x.getDouble(end));
 		ApacheOptimizer opt = new ApacheOptimizer(Optimizer.SIMPLEX_NM);
 		double residual = Double.POSITIVE_INFINITY;
-//		double[] errors = null;
 		try {
+			if (logger.isInfoEnabled()) {
+				AbstractDataset.setMaxLineLength(250);
+				logger.info("Fitting {} over {}", dg, cdx.toString(true));
+			}
 			opt.optimize(new IDataset[] {cdx}, cdy, dg);
 			residual = opt.calculateResidual();
-			logger.info("Residual is {}", residual);
-//			errors = opt.guessParametersErrors();
+			Dataset fdy = dg.calculateValues(cdx);
+			if (logger.isInfoEnabled()) {
+				logger.info("Fitted {} to {}", fdy.toString(true), cdy.toString(true));
+				logger.info("Parameters are {}", Arrays.toString(dg.getParameterValues()));
+				logger.info("Residual is {}", residual);
+			}
 		} catch (Exception e) {
 			return Double.NaN;
 		}
 
-		return dg.getParameterValue(0);
+		return p.getValue();
 	}
 
 }
